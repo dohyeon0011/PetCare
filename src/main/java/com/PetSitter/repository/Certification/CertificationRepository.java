76b4c9ed4bc163b1fcc6d9ba849357b2ab63cb6b
idohyeon <ehgus5026@naver.com>
2025-02-05 01:17:12 +0900

    -Certification Repository(돌봄사 자격증)

---
+++ src/main/java/com/PetSitter/repository/Certification/CertificationRepository.java
@@ -0,0 +1,18 @@
+package com.PetSitter.repository.Certification;
+
+import com.PetSitter.domain.Certification.Certification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CertificationRepository extends JpaRepository<Certification, Long> {
+
+    // 특정 회원의 자격증 목록 조회
+    List<Certification> findBySitterId(long sitterId);
+
+    Optional<Certification> findBySitterIdAndId(long sitterId, long id);
+
+    // 특정 회원의 특정 자격증 목록 조회
+    List<Certification> findBySitterIdAndIdIn(long sitterId, List<Long> ids);
+}
