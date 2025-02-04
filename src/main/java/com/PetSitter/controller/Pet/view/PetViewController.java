065a3218344e0016ed9d136c14fc7378529e32f9
idohyeon <ehgus5026@naver.com>
2025-02-05 00:56:36 +0900

    -Pet VIEW Controller(반려견)

---
+++ src/main/java/com/PetSitter/controller/Pet/view/PetViewController.java
@@ -0,0 +1,45 @@
+package com.PetSitter.controller.Pet.view;
+
+import com.PetSitter.dto.Pet.response.PetResponse;
+import com.PetSitter.service.Pet.PetService;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.Comment;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/pets-care")
+public class PetViewController {
+
+    private final PetService petService;
+
+    @Comment("반려견 등록 || 수정")
+    @GetMapping("/members/{customerId}/pets/new")
+    public String newPet(@PathVariable("customerId") long customerId, Model model) {
+        List<PetResponse.GetList> pets = petService.findById(customerId);
+
+        if (pets.isEmpty()) {
+            model.addAttribute("pets", new PetResponse.GetList());
+        } else {
+            model.addAttribute("pets", pets);
+        }
+
+        return "pet/newPet";
+    }
+
+    @Comment("특정 회원의 모든 반려견 조회")
+    @GetMapping("/members/{customerId}/pets")
+    public String getAllPet(@PathVariable("customerId") long customerId, Model model) {
+        List<PetResponse.GetList> pets = petService.findById(customerId);
+        model.addAttribute("pets", pets);
+
+        return "pet/petList";
+    }
+
+}
