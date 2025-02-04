5cbf839825687cfe95159b5603198ff1b6728f66
idohyeon <ehgus5026@naver.com>
2025-02-05 01:14:13 +0900

    -Add CustomerReservation DTO(예약->고객 시점)

---
+++ src/main/java/com/PetSitter/dto/Reservation/CustomerReservation/request/AddCustomerReservationRequest.java
@@ -0,0 +1,28 @@
+package com.PetSitter.dto.Reservation.CustomerReservation.request;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class AddCustomerReservationRequest {
+    private long customerId; // 고객
+
+    @NotNull(message = "돌봄을 맡길 돌봄사 선택은 필수입니다.")
+    private long sitterId; // 돌봄사
+
+    @NotNull(message = "날짜 선택은 필수입니다.")
+    private long careAvailableId;
+
+    @NotNull(message = "돌봄 예약 비용은 필수 값입니다.")
+    private int price;
+
+    @NotEmpty(message = "돌봄을 맡길 반려견 선택은 필수입니다.")
+    private List<Long> petIds;
+}
