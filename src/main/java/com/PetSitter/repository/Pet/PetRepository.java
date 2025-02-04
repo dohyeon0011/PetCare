71621bd90d884c3b97d32681e23866b1d99ec564
idohyeon <ehgus5026@naver.com>
2025-02-05 01:17:34 +0900

    -Pet Repository(반려견)

---
+++ src/main/java/com/PetSitter/repository/Pet/PetRepository.java
@@ -0,0 +1,18 @@
+package com.PetSitter.repository.Pet;
+
+import com.PetSitter.domain.Pet.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PetRepository extends JpaRepository<Pet, Long> {
+
+    // 특정 회원의 반려견 목록 조회
+    List<Pet> findByCustomerId(long customerId);
+
+    Optional<Pet> findByCustomerIdAndId(long customerId, long id);
+
+    // 특정 회원이 보유중인 특정 반려견 목록만 조회
+    List<Pet> findByCustomerIdAndIdIn(long customerId, List<Long> ids);
+}
