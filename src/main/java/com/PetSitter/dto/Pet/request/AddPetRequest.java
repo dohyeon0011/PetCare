76be399c2e41a18793a24581fa03b7f969208734
idohyeon <ehgus5026@naver.com>
2025-02-05 01:12:10 +0900

    -Add Pet DTO(반려견 등록)

---
+++ src/main/java/com/PetSitter/dto/Pet/request/AddPetRequest.java
@@ -0,0 +1,36 @@
+package com.PetSitter.dto.Pet.request;
+
+import com.PetSitter.domain.Pet.Pet;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class AddPetRequest {
+    private String name;
+
+    @NotNull(message = "반려견 나이 입력은 필수입니다.")
+    private int age;
+
+    @NotBlank(message = "반려견 품종 입력은 필수입니다.")
+    private String breed;
+
+    private String medicalConditions;
+
+    private String profileImgPath;
+
+    public Pet toEntity() {
+        return Pet.builder()
+                .name(name)
+                .age(age)
+                .breed(breed)
+                .medicalConditions(medicalConditions)
+                .profileImgPath(profileImgPath)
+                .build();
+    }
+
+}
