899287ca4e1feebedeee130513aff95d1b548bd4
idohyeon <ehgus5026@naver.com>
2025-02-05 01:13:45 +0900

    -Reservation Response DTO(예약 시)

---
+++ src/main/java/com/PetSitter/dto/Reservation/ReservationResponse.java
@@ -0,0 +1,40 @@
+package com.PetSitter.dto.Reservation;
+
+import com.PetSitter.domain.CareAvailableDate.CareAvailableDate;
+import com.PetSitter.domain.Member.Member;
+import com.PetSitter.domain.Pet.Pet;
+import com.PetSitter.dto.CareAvailableDate.response.CareAvailableDateResponse;
+import com.PetSitter.dto.Pet.response.PetResponse;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+public class ReservationResponse { // 고객이 예약할 때 보여줄 정보
+    private long customerId;
+    private long sitterId;
+    private String sitterName;
+    private List<CareAvailableDateResponse.GetReservation> careAvailableDates;
+    private String zipcode;
+    private String address;
+    private List<PetResponse.GetReservation> pets;
+
+    public ReservationResponse(Member customer, Member sitter, List<CareAvailableDate> careAvailableDates, List<Pet> pets) {
+        this.customerId = customer.getId();
+        this.sitterId = sitter.getId();
+        this.sitterName = sitter.getName();
+        this.zipcode = sitter.getZipcode();
+        this.address = sitter.getAddress();
+        this.careAvailableDates = careAvailableDates
+                .stream()
+                .map(CareAvailableDateResponse.GetReservation::new)
+                .toList();
+        this.pets = pets
+                .stream()
+                .map(PetResponse.GetReservation::new)
+                .toList();
+    }
+
+}
