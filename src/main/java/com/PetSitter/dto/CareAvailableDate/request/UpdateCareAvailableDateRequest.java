b92fdd3834b77519327a115a31568330d2f76a1b
idohyeon <ehgus5026@naver.com>
2025-02-05 01:05:40 +0900

    -Update CareAvailableDate DTO(돌봄사 -> 돌봄 가능 날짜)

---
+++ src/main/java/com/PetSitter/dto/CareAvailableDate/request/UpdateCareAvailableDateRequest.java
@@ -0,0 +1,24 @@
+package com.PetSitter.dto.CareAvailableDate.request;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UpdateCareAvailableDateRequest {
+
+    @NotNull(message = "날짜 입력은 필수입니다.")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate availabilityAt;
+
+    @Min(value = 1, message = "돌봄 비용 입력은 필수입니다.")
+    private int price;
+
+}
