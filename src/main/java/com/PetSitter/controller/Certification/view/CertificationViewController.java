81c3ad93c2b939e2fc3009869522c47dbe473bd1
idohyeon <ehgus5026@naver.com>
2025-02-05 00:55:24 +0900

    -Certification VIEW Controller(돌봄사 자격증)

---
+++ src/main/java/com/PetSitter/controller/Certification/view/CertificationViewController.java
@@ -0,0 +1,44 @@
+package com.PetSitter.controller.Certification.view;
+
+import com.PetSitter.dto.Certification.response.CertificationResponse;
+import com.PetSitter.service.Certification.CertificationService;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.Comment;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/pets-care")
+public class CertificationViewController {
+
+    private final CertificationService certificationService;
+
+    @Comment("회원의 자격증 추가")
+    @GetMapping("/members/{sitterId}/certifications/new")
+    public String newCertification(@PathVariable("sitterId") long sitterId, Model model) {
+        List<CertificationResponse.GetList> certifications = certificationService.findById(sitterId);
+
+        if (certifications.isEmpty()) {
+            model.addAttribute("certifications", new CertificationResponse.GetList());
+        } else {
+            model.addAttribute("certifications", certifications);
+        }
+
+        return "certification/newCertification";
+    }
+
+    @Comment("회원의 모든 자격증 조회")
+    @GetMapping("/members/{sitterId}/certifications")
+    public String getAllCertification(@PathVariable("sitterId") long sitterId, Model model) {
+        List<CertificationResponse.GetList> certifications = certificationService.findById(sitterId);
+        model.addAttribute("certifications", certifications);
+
+        return "certification/certificationList";
+    }
+}
