fb225079baec40eea93b7619bf71ffb7ae61f455
idohyeon <ehgus5026@naver.com>
2025-02-05 01:20:18 +0900

    -돌봄 예약금 적립 구현체

---
+++ src/main/java/com/PetSitter/service/Reservation/Reward/RewardServiceImpl.java
@@ -0,0 +1,31 @@
+package com.PetSitter.service.Reservation.Reward;
+
+import com.PetSitter.domain.Member.Member;
+import com.PetSitter.repository.Member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class RewardServiceImpl implements RewardService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public void addReward(long customerId, int amount) {
+        Member customer = memberRepository.findById(customerId)
+                .orElseThrow(() -> new NoSuchElementException("고객 정보 조회에 실패했습니다."));
+
+        int reservationAmount = calculatorReward(amount);
+
+        customer.addRewardPoints(reservationAmount);
+    }
+
+    @Override
+    public int calculatorReward(int reservationAmount) { // 적립금 계산 로직 (예약금의 2% 적립)
+        return (int) (reservationAmount * 0.02);
+    }
+
+}
