b6277a4500be84e471b1753a67a14a1331781ec9
idohyeon <ehgus5026@naver.com>
2025-02-05 01:13:59 +0900

    -Reservation Sitter Response DTO(예약 전 돌봄사의 정보 조회)

---
+++ src/main/java/com/PetSitter/dto/Reservation/ReservationSitterResponse.java
@@ -0,0 +1,73 @@
+package com.PetSitter.dto.Reservation;
+
+import com.PetSitter.domain.Member.Member;
+import com.PetSitter.domain.Review.Review;
+import com.PetSitter.dto.Certification.response.CertificationResponse;
+import com.PetSitter.dto.Review.response.ReviewResponse;
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+public class ReservationSitterResponse { // 고객이 예약하기 전 보여줄 정보
+
+    @NoArgsConstructor
+    @Getter
+    public static class GetList { // 예약 가능 페이지에 있는 돌봄사들의 정보
+        private long sitterId;
+        private String sitterName;
+        private String introduction;
+
+        /*public GetList(Member sitter) {
+            this.sitterId = sitter.getId();
+            this.sitterName = sitter.getName();
+            this.introduction = sitter.getIntroduction();
+        }*/
+
+        @QueryProjection
+        public GetList(long sitterId, String sitterName, String introduction) {
+            this.sitterId = sitterId;
+            this.sitterName = sitterName;
+            this.introduction = introduction;
+        }
+    }
+
+    @NoArgsConstructor
+    @Getter
+    public static class GetDetail { // 예약 가능 페이지 목록 중 특정 돌봄사의 자세한 정보 + 해당 돌봄사에 대한 리뷰도 보여줄 것
+        private long sitterId;
+        private String sitterName;
+        private String introduction;
+        private Integer careerYear;
+        private List<CertificationResponse.GetReservation> certifications;
+        private String zipcode;
+        private String address;
+        private List<ReviewResponse.GetDetail> reviews;
+
+        public GetDetail(Member sitter, List<Review> reviews) {
+            this.sitterId = sitter.getId();
+            this.sitterName = sitter.getName();
+            this.introduction = sitter.getIntroduction();
+            this.careerYear = sitter.getCareerYear();
+            this.certifications = sitter.getCertifications()
+                    .stream()
+                    .map(CertificationResponse.GetReservation::new)
+                    .toList();
+            this.zipcode = sitter.getZipcode();
+            this.address = sitter.getAddress();
+            /*this.reviews = reviews
+                    .stream()
+                    .map(ReviewResponse.GetDetail::new)
+                    .toList();*/
+            this.reviews = reviews
+                    .stream()
+                    .map(review -> {
+                        return new ReviewResponse.GetDetail(review.getId(), review.getCustomerReservation().getId(), review.getCustomerReservation().getCustomer().getNickName(),
+                                review.getCustomerReservation().getSitter().getName(), review.getRating(), review.getComment());
+                    })
+                    .toList();
+        }
+    }
+
+}
