109d34c3b399975a4ab4cead9dc5446c4cfffec1
idohyeon <ehgus5026@naver.com>
2025-02-05 01:09:32 +0900

    -Certification Response DTO(돌봄사 돌봄 자격증)

---
+++ src/main/java/com/PetSitter/dto/Certification/response/CertificationResponse.java
@@ -0,0 +1,32 @@
+package com.PetSitter.dto.Certification.response;
+
+import com.PetSitter.domain.Certification.Certification;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+public class CertificationResponse {
+
+    @NoArgsConstructor
+    @Getter
+    public static class GetList {
+        private long id;
+        private String name;
+
+        public GetList(Certification certification) {
+            this.id = certification.getId();
+            this.name = certification.getName();
+        }
+    }
+
+    @NoArgsConstructor
+    @Getter
+    public static class GetReservation {
+        private String name;
+
+        public GetReservation(Certification certification) {
+            this.name = certification.getName();
+        }
+    }
+
+}
