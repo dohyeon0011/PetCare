e69a1231dbd0ee3705a628f73e8f098ed317b419
idohyeon <ehgus5026@naver.com>
2025-02-07 21:12:53 +0900

    -CareAvailableDate API Controller(돌봄사 -> 돌봄 가능 날짜)

--- src/main/java/com/PetSitter/controller/CareAvailableDate/api/CareAvailableDateApiController.java
+++ src/main/java/com/PetSitter/controller/CareAvailableDate/api/CareAvailableDateApiController.java
@@ -1,6 +1,5 @@
 package com.PetSitter.controller.CareAvailableDate.api;
 
-import com.PetSitter.domain.CareAvailableDate.CareAvailableDate;
 import com.PetSitter.dto.CareAvailableDate.request.AddCareAvailableDateRequest;
 import com.PetSitter.dto.CareAvailableDate.request.UpdateCareAvailableDateRequest;
 import com.PetSitter.dto.CareAvailableDate.response.CareAvailableDateResponse;
@@ -12,9 +11,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.ObjectError;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,9 +30,23 @@ public class CareAvailableDateApiController {
 
     @Operation(description = "돌봄 가능 일정 등록 API")
     @PostMapping("/{sitterId}/care-available-dates/new")
-    public ResponseEntity<CareAvailableDateResponse.GetDetail> saveCareAvailability(@PathVariable("sitterId") long id,
-                                                                  @RequestBody @Valid AddCareAvailableDateRequest request) {
+    public ResponseEntity<?> saveCareAvailability(@PathVariable("sitterId") long id,
+                                                                                    @RequestBody @Valid AddCareAvailableDateRequest request,
+                                                                                    BindingResult result) {
 //        CareAvailableDate careAvailableDate = careAvailableDateService.save(id, request);
+
+        // 검증 오류가 있을 경우 에러 메시지를 리턴
+        if (result.hasErrors()) {
+            Map<String, String> errors = new HashMap<>();
+
+            result.getFieldErrors().forEach(error -> {
+                errors.put(error.getField(), error.getDefaultMessage()); // 필드와 메시지를 매핑
+            });
+
+            return ResponseEntity.badRequest()
+                    .body(errors);
+        }
+
         CareAvailableDateResponse.GetDetail careAvailableDate = careAvailableDateService.save(id, request);
 
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -74,9 +93,21 @@ public class CareAvailableDateApiController {
 
     @Operation(description = "회원의 등록한 특정 돌봄 일정 수정 API")
     @PutMapping("/{sitterId}/care-available-dates/{careAvailableDateId}")
-    public ResponseEntity<CareAvailableDateResponse.GetDetail> updateCareAvailableDate(@PathVariable("sitterId") long id,
+    public ResponseEntity<?> updateCareAvailableDate(@PathVariable("sitterId") long id,
                                                                             @PathVariable("careAvailableDateId") long careAvailableDateId,
-                                                                            @RequestBody @Valid UpdateCareAvailableDateRequest request) {
+                                                                            @RequestBody @Valid UpdateCareAvailableDateRequest request,
+                                                                            BindingResult result) {
+        if (result.hasErrors()) {
+            Map<String, String> errorMessages = new HashMap<>();
+
+            result.getFieldErrors().forEach(error -> {
+                errorMessages.put(error.getField(), error.getDefaultMessage());
+            });
+
+            return ResponseEntity.badRequest()
+                    .body(errorMessages);
+        }
+
         CareAvailableDateResponse.GetDetail updateSitterAvailableDate = careAvailableDateService.update(id, careAvailableDateId, request);
 
         return ResponseEntity.ok()
