00e8c89c09acb706920d1687f54ed489c1651739
idohyeon <ehgus5026@naver.com>
2025-02-12 20:43:39 +0900

    -GlobalExceptionHandler Controller(API 예외 공통 처리 클래스)

---
+++ src/main/java/com/PetSitter/config/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.PetSitter.config.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> handleIllegalArgumentException(IllegalArgumentException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", ex.getMessage()); // 에러 메시지를 JSON으로 반환
+
+        return ResponseEntity.badRequest()
+                .body(errorResponse);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<Map<String, String>> handleNoSuchElementException(NoSuchElementException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", ex.getMessage());
+
+        return ResponseEntity.badRequest()
+                .body(errorResponse);
+    }
+}
