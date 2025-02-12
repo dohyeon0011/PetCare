a7d83496e118ff59fbbacfeced31ee2961d3f7c8
idohyeon <ehgus5026@naver.com>
2025-02-12 20:41:16 +0900

    -Reservation Response DTO(예약 시)

--- src/main/java/com/PetSitter/dto/Reservation/ReservationResponse.java
+++ src/main/java/com/PetSitter/dto/Reservation/ReservationResponse.java
@@ -15,18 +15,22 @@ import java.util.List;
 public class ReservationResponse { // 고객이 예약할 때 보여줄 정보
     private long customerId;
     private long sitterId;
+    private String customerNickName;
     private String sitterName;
     private List<CareAvailableDateResponse.GetReservation> careAvailableDates;
     private String zipcode;
     private String address;
+    private int amount;
     private List<PetResponse.GetReservation> pets;
 
     public ReservationResponse(Member customer, Member sitter, List<CareAvailableDate> careAvailableDates, List<Pet> pets) {
         this.customerId = customer.getId();
         this.sitterId = sitter.getId();
+        this.customerNickName = customer.getNickName();
         this.sitterName = sitter.getName();
         this.zipcode = sitter.getZipcode();
         this.address = sitter.getAddress();
+        this.amount = customer.getAmount();
         this.careAvailableDates = careAvailableDates
                 .stream()
                 .map(CareAvailableDateResponse.GetReservation::new)
