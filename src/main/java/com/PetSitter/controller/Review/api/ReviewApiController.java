57e561724bd0dacaa7679ae65e8affb32a5f3fc4
idohyeon <ehgus5026@naver.com>
2025-02-24 22:21:36 +0900

    -Review API Controller(돌봄 리뷰)

--- src/main/java/com/PetSitter/controller/Review/api/ReviewApiController.java
+++ src/main/java/com/PetSitter/controller/Review/api/ReviewApiController.java
@@ -13,6 +13,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/pets-care")
@@ -79,5 +81,49 @@ public class ReviewApiController {
         return ResponseEntity.ok()
                 .body(newReview);
     }
+
+    /**
+     * V2: 검색 조건을 검색 전용 클래스 객체로 받기
+     * 1. 검색 조건이 추가되더라도 컨트롤러 메서드의 변경 없이 유지 가능
+     * 2. 파라미터 개수가 많아져도 코드가 깔끔해짐
+     * 3. 스프링이 자동으로 요청 파라미터를 객체 필드에 매핑해줌(@ModelAttribute를 사용하면 쿼리 파라미터를 자동으로 객체 필드에 매핑)
+     */
+    @Operation(description = "이용 후기 페이지에서 전체 리뷰 및 특정 돌봄사의 리뷰 조회 API: V2")
+    @GetMapping("/reviews")
+    public ResponseEntity<List<ReviewResponse.GetDetail>> getAllReviewsBySitterV2(@ModelAttribute ReviewSearch reviewSearch,
+                                                                                  @RequestParam(defaultValue = "0") int page) {
+        List<ReviewResponse.GetDetail> reviews = reviewService.getAllReviewsBySitterV2(reviewSearch, page);
+
+        return ResponseEntity.ok()
+                .body(reviews);
+    }
+
+    /**
+     * V2에 통합
+     */
+    /*@Operation(description = "이용 후기 페이지에서 전체 리뷰 조회 API")
+    @GetMapping("/reviews-all")
+    public ResponseEntity<List<ReviewResponse.GetDetail>> getAllReviews(@RequestParam(defaultValue = "0") int page) {
+        List<ReviewResponse.GetDetail> reviews = reviewService.getAllReviews(page);
+
+        return ResponseEntity.ok()
+                .body(reviews);
+    }*/
+
+    /**
+     * V1: 검색 조건 @RequestParam 으로 받기
+     * 1. 검색 조건이 추가될 때마다 @RequestParam을 계속 추가해야 함
+     * 2. 검색 조건이 많아지면 컨트롤러 메서드의 파라미터가 길어짐
+     * 3. 유지보수가 어려워짐
+     */
+    /*@Operation(description = "이용 후기 페이지에서 특정 돌봄사의 리뷰 조회 API: V1")
+    @GetMapping("/reviews-v1")
+    public ResponseEntity<List<ReviewResponse.GetDetail>> getAllReviewsBySitterV1(@RequestParam("sitter") String sitterName,
+                                                                                @RequestParam(defaultValue = "0") int page) {
+        List<ReviewResponse.GetDetail> reviews = reviewService.getAllReviewsBySitterV1(sitterName, page);
+
+        return ResponseEntity.ok()
+                .body(reviews);
+    }*/
 
 }
