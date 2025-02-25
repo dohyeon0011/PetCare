7ab70158924453f1299e98c0066c4de0f5b95324
idohyeon <ehgus5026@naver.com>
2025-02-25 14:55:17 +0900

    -Main VIEW Controller

---
+++ src/main/java/com/PetSitter/controller/MainViewController.java
@@ -0,0 +1,36 @@
+package com.PetSitter.controller;
+
+import com.PetSitter.dto.Review.response.ReviewResponse;
+import com.PetSitter.service.Review.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.Comment;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/pets-care")
+public class MainViewController {
+
+    private final ReviewService reviewService;
+
+    @GetMapping("/main")
+    public String home(Model model) {
+
+        List<ReviewResponse.GetDetail> reviews = reviewService.getAllReview();
+
+        model.addAttribute("reviews", reviews);
+        return "main";
+    }
+
+    @Comment("로그인")
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "member/login";
+    }
+
+}
