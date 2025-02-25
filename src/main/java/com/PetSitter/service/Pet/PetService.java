34f3e9c59d6fd59bdaa7d1338499958b1b18ed22
idohyeon <ehgus5026@naver.com>
2025-02-25 20:17:59 +0900

    -Pet Service(반려견)

--- src/main/java/com/PetSitter/service/Pet/PetService.java
+++ src/main/java/com/PetSitter/service/Pet/PetService.java
@@ -45,7 +45,7 @@ public class PetService {
     @Comment("특정 회원의 반려견 조회")
     @Transactional(readOnly = true)
     public List<PetResponse.GetList> findById(long customerId) {
-        List<PetResponse.GetList> pets = petRepository.findByCustomerId(customerId)
+        List<PetResponse.GetList> pets = petRepository.findByCustomerIdAndIsDeletedFalse(customerId)
                 .stream()
                 .map(PetResponse.GetList::new)
                 .toList();
@@ -68,9 +68,11 @@ public class PetService {
         // 여러 개의 엔티티를 한 번의 쿼리로 삭제하는 방식으로 성능을 개선
         // 이 방식은 영속성 컨텍스트(Persistence Context)에서 해당 엔티티들을 관리하지 않으므로, 엔티티 상태가 영속성 컨텍스트와 동기화되지 않음
         // 즉, 삭제 후 해당 엔티티는 더 이상 영속성 컨텍스트에서 관리되지 않으며, 이후에 해당 엔티티에 접근하려면 다시 조회해야 함
-        petRepository.delete(pet);
+//        petRepository.delete(pet);
 //        petRepository.deleteAllInBatch(pets);
 //        petRepository.deleteAll(pets); // 얘는 여러번 쿼리 나감
+
+        pet.changeIsDeleted(true);
     }
 
     @Comment("특정 회원의 반려견 수정")
