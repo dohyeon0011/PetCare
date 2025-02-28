3a743875fe6dbdf3dfd1ac3f0293e1392a15c971
idohyeon <ehgus5026@naver.com>
2025-02-28 20:52:02 +0900

    -Admin Reservation API Controller(관리자 - 돌봄 예약 관리)

--- src/main/java/com/PetSitter/controller/Admin/Reservation/api/AdminReservationApiController.java
+++ src/main/java/com/PetSitter/controller/Admin/Reservation/api/AdminReservationApiController.java
@@ -1,7 +1,9 @@
 package com.PetSitter.controller.Admin.Reservation.api;
 
+import com.PetSitter.domain.Member.Member;
 import com.PetSitter.domain.Reservation.ReservationSearch;
 import com.PetSitter.dto.Reservation.CustomerReservation.response.AdminReservationResponse;
+import com.PetSitter.service.Reservation.CustomerReservation.AdminReservationService;
 import com.PetSitter.service.Reservation.CustomerReservation.CustomerReservationService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -16,12 +18,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/pets-care/admin")
 public class AdminReservationApiController {
 
-    private final CustomerReservationService customerReservationService;
+    private final AdminReservationService adminReservationService;
 
     @Operation(description = "관리자 페이지 모든 예약 목록 조회 API")
     @GetMapping("/reservations")
-    public ResponseEntity<Page<AdminReservationResponse.ReservationListResponse>> findAllForAdmin(@ModelAttribute ReservationSearch reservationSearch, @PageableDefault Pageable pageable) {
-        Page<AdminReservationResponse.ReservationListResponse> reservations = customerReservationService.findAllForAdmin(reservationSearch, pageable);
+    public ResponseEntity<Page<AdminReservationResponse.ReservationListResponse>> findAllForAdmin(@ModelAttribute ReservationSearch reservationSearch,
+                                                                                                  @SessionAttribute("member") Member member,
+                                                                                                  @PageableDefault Pageable pageable) {
+        Page<AdminReservationResponse.ReservationListResponse> reservations = adminReservationService.findAllForAdmin(reservationSearch, member, pageable);
 
         return ResponseEntity.ok()
                 .body(reservations);
@@ -29,8 +33,20 @@ public class AdminReservationApiController {
 
     @Operation(description = "관리자 페이지 예약 상세 정보 조회 API")
     @GetMapping("/reservations/{reservationId}")
-    public ResponseEntity<AdminReservationResponse.ReservationDetailResponse> findForAdmin(@PathVariable("reservationId") long id) {
+    public ResponseEntity<AdminReservationResponse.ReservationDetailResponse> findForAdmin(@PathVariable("reservationId") long id,
+                                                                                           @SessionAttribute("member") Member member) {
+        AdminReservationResponse.ReservationDetailResponse reservation = adminReservationService.findForAdmin(id, member);
 
+        return ResponseEntity.ok()
+                .body(reservation);
     }
 
+    @Operation(description = "관리자 권한 예약 취소 API")
+    @DeleteMapping("reservations/{reservationId}")
+    public ResponseEntity<Void> deleteForAdmin(@PathVariable("reservationId") long id, @SessionAttribute("member") Member member) {
+        adminReservationService.deleteForAdmin(id, member);
+
+        return ResponseEntity.noContent()
+                .build();
+    }
 }
