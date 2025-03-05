8343312472a6afb5d8cd1f0c5fa65a13e4946542
idohyeon <ehgus5026@naver.com>
2025-03-05 18:38:34 +0900

    -PointsHistory Repository(고객 포인트 내역(+적립, 사용))

---
+++ src/main/java/com/PetSitter/repository/PointHistory/PointHistoryRepository.java
@@ -0,0 +1,14 @@
+package com.PetSitter.repository.PointHistory;
+
+import com.PetSitter.domain.PointHistory.PointsHistory;
+import com.PetSitter.domain.PointHistory.PointsStatus;
+import com.PetSitter.domain.Reservation.CustomerReservation.CustomerReservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PointHistoryRepository extends JpaRepository<PointsHistory, Long> {
+
+    // 예약(고객 시점)으로 포인트 내역 조회('USING')
+    Optional<PointsHistory> findByCustomerReservationAndPointsStatus(CustomerReservation customerReservation, PointsStatus pointsStatus);
+}
