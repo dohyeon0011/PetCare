0d090ac4921998b5a214ec87034d0d922418e105
idohyeon <ehgus5026@naver.com>
2025-03-07 18:25:31 +0900

    -UserDetailsService 구현체(시큐리티)

---
+++ src/main/java/com/PetSitter/service/Member/MemberDetailsService.java
@@ -0,0 +1,23 @@
+package com.PetSitter.service.Member;
+
+import com.PetSitter.domain.Member.MemberDetails;
+import com.PetSitter.repository.Member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
+        return memberRepository.findByLoginId(loginId)
+                .map(MemberDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + loginId));
+    }
+}
