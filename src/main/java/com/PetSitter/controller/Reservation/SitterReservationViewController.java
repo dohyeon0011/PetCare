191651399c886226d593550fe7bb5d7ad3ee8de9
idohyeon <ehgus5026@naver.com>
2025-03-10 20:06:22 +0900

    -로그인 사용자 정보 읽기 수정(시큐리티), (고객 시점 -> 예약하기 전 조회될 정보 VIEW Controller)

--- src/main/java/com/PetSitter/controller/Reservation/SitterReservationViewController.java
+++ src/main/java/com/PetSitter/controller/Reservation/SitterReservationViewController.java
@@ -1,11 +1,13 @@
 package com.PetSitter.controller.Reservation;
 
+import com.PetSitter.domain.Member.MemberDetails;
 import com.PetSitter.dto.Reservation.ReservationSitterResponse;
 import com.PetSitter.service.Reservation.SitterReservationService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,19 +24,25 @@ public class SitterReservationViewController {
 
     @Operation(description = "고객에게 돌봄 예약 가능한 돌봄사들의 정보 조회")
     @GetMapping("/reservable-list")
-    public String getAllReservable(Pageable pageable, Model model) {
+    public String getAllReservable(@AuthenticationPrincipal MemberDetails memberDetails, Pageable pageable, Model model) {
 //        List<ReservationSitterResponse.GetList> reservableSitters = sitterReservationService.findReservableSitters();
-        Page<ReservationSitterResponse.GetList> reservableSitters = sitterReservationService.findReservableSitters(pageable);
-        model.addAttribute("reservableSitters", reservableSitters);
+        if (memberDetails != null) {
+            Page<ReservationSitterResponse.GetList> reservableSitters = sitterReservationService.findReservableSitters(pageable);
+            model.addAttribute("reservableSitters", reservableSitters);
+            model.addAttribute("currentUser", memberDetails.getMember());
+        }
 
         return "reservable/reservable-list";
     }
 
     @Operation(description = "돌봄 예약 가능 목록 중 선택한 돌봄사의 자세한 정보 조회")
     @GetMapping("/reservable/members/{sitterId}")
-    public String getReservableSitter(@PathVariable("sitterId") long sitterId, @RequestParam(defaultValue = "0") int page, Model model) {
-        ReservationSitterResponse.GetDetail reservableSitter = sitterReservationService.findReservableSitter(sitterId, page);
-        model.addAttribute("reservableSitter", reservableSitter);
+    public String getReservableSitter(@PathVariable("sitterId") long sitterId, @RequestParam(defaultValue = "0") int page, @AuthenticationPrincipal MemberDetails memberDetails, Model model) {
+        if (memberDetails != null) {
+            ReservationSitterResponse.GetDetail reservableSitter = sitterReservationService.findReservableSitter(sitterId, page);
+            model.addAttribute("reservableSitter", reservableSitter);
+            model.addAttribute("currentUser", memberDetails.getMember());
+        }
 
         return "reservable/reservable-detail";
     }
