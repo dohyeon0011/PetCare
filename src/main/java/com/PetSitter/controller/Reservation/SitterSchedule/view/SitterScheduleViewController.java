16f001d1c355c3a62fd3d3b76de82caa6ecfd00c
idohyeon <ehgus5026@naver.com>
2025-03-10 20:06:55 +0900

    -로그인 사용자 정보 읽기 수정(시큐리티), (돌봄사 돌봄 일정 VIEW Controller)

--- src/main/java/com/PetSitter/controller/Reservation/SitterSchedule/view/SitterScheduleViewController.java
+++ src/main/java/com/PetSitter/controller/Reservation/SitterSchedule/view/SitterScheduleViewController.java
@@ -1,11 +1,14 @@
 package com.PetSitter.controller.Reservation.SitterSchedule.view;
 
+import com.PetSitter.domain.Member.MemberDetails;
+import com.PetSitter.dto.Reservation.CustomerReservation.response.CustomerReservationResponse;
 import com.PetSitter.dto.Reservation.SitterSchedule.response.SitterScheduleResponse;
 import com.PetSitter.service.Reservation.SitterSchedule.SitterScheduleService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,10 +24,13 @@ public class SitterScheduleViewController {
 
     @Operation(description = "돌봄사의 전체 돌봄 예약 목록 조회")
     @GetMapping("/members/{sitterId}/schedules")
-    public String getAllSitterSchedule(@PathVariable("sitterId") long sitterId, Pageable pageable, Model model) {
+    public String getAllSitterSchedule(@PathVariable("sitterId") long sitterId, Pageable pageable, @AuthenticationPrincipal MemberDetails memberDetails, Model model) {
 //        List<SitterScheduleResponse.GetList> schedules = sitterScheduleService.findAllById(sitterId);
-        Page<SitterScheduleResponse.GetList> schedules = sitterScheduleService.findAllById(sitterId, pageable);
-        model.addAttribute("schedules", schedules);
+        if (memberDetails.getMember().getId() == sitterId) {
+            Page<SitterScheduleResponse.GetList> schedules = sitterScheduleService.findAllById(sitterId, pageable);
+            model.addAttribute("schedules", schedules);
+            model.addAttribute("currentUser", memberDetails.getMember());
+        }
 
         return "schedule/schedule-list";
     }
@@ -32,9 +38,13 @@ public class SitterScheduleViewController {
     @Operation(description = "특정 돌봄사의 특정 돌봄 예약 상세 정보 조회")
     @GetMapping("/members/{sitterId}/schedules/{sitterScheduleId}")
     public String getSitterSchedule(@PathVariable("sitterId") long sitterId,
-                                    @PathVariable("sitterScheduleId") long sitterScheduleId, Model model) {
-        SitterScheduleResponse.GetDetail schedule = sitterScheduleService.findById(sitterId, sitterScheduleId);
-        model.addAttribute("schedule", schedule);
+                                    @PathVariable("sitterScheduleId") long sitterScheduleId,
+                                    @AuthenticationPrincipal MemberDetails memberDetails, Model model) {
+        if (memberDetails.getMember().getId() == sitterId) {
+            SitterScheduleResponse.GetDetail schedule = sitterScheduleService.findById(sitterId, sitterScheduleId);
+            model.addAttribute("schedule", schedule);
+            model.addAttribute("currentUser", memberDetails.getMember());
+        }
 
         return "schedule/schedule-detail";
     }
