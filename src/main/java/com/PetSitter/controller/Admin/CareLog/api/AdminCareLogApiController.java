28622426e68c2289fb42687212f135fe05f73f25
idohyeon <ehgus5026@naver.com>
2025-03-10 19:55:46 +0900

    -로그인 사용자 정보 수정(시큐리티) (관리자 - 돌봄 케어 로그 관리)

--- src/main/java/com/PetSitter/controller/Admin/CareLog/api/AdminCareLogApiController.java
+++ src/main/java/com/PetSitter/controller/Admin/CareLog/api/AdminCareLogApiController.java
@@ -1,10 +1,12 @@
 package com.PetSitter.controller.Admin.CareLog.api;
 
 import com.PetSitter.domain.Member.Member;
+import com.PetSitter.domain.Member.MemberDetails;
 import com.PetSitter.service.Admin.CareLog.AdminCareLogService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,8 +18,8 @@ public class AdminCareLogApiController {
 
     @Operation(description = "관리자 권한 돌봄 케어 로그 삭제 API")
     @DeleteMapping("/care-logs/{careLogId}")
-    public ResponseEntity<Void> deleteForAdmin(@PathVariable("careLogId") long id, @SessionAttribute("member") Member member) {
-        adminCareLogService.deleteForAdmin(id, member);
+    public ResponseEntity<Void> deleteForAdmin(@PathVariable("careLogId") long id, @AuthenticationPrincipal MemberDetails memberDetails) {
+        adminCareLogService.deleteForAdmin(id, memberDetails.getMember());
 
         return ResponseEntity.noContent()
                 .build();
