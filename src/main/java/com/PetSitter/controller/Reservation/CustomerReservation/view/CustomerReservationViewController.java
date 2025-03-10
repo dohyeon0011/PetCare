6a002d304bc1beb0e2a70dda1416cc21db8d8feb
idohyeon <ehgus5026@naver.com>
2025-03-10 20:05:51 +0900

    -로그인 사용자 정보 읽기 수정(시큐리티), (고객 시점 -> 돌봄 예약)

--- src/main/java/com/PetSitter/controller/Reservation/CustomerReservation/view/CustomerReservationViewController.java
+++ src/main/java/com/PetSitter/controller/Reservation/CustomerReservation/view/CustomerReservationViewController.java
@@ -1,5 +1,6 @@
 package com.PetSitter.controller.Reservation.CustomerReservation.view;
 
+import com.PetSitter.domain.Member.MemberDetails;
 import com.PetSitter.dto.Reservation.CustomerReservation.response.CustomerReservationResponse;
 import com.PetSitter.dto.Reservation.ReservationResponse;
 import com.PetSitter.service.Reservation.CustomerReservation.CustomerReservationService;
@@ -8,6 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,19 +26,26 @@ public class CustomerReservationViewController {
 
     @Operation(description = "회원 돌봄 예약 생성")
     @GetMapping("/members/{customerId}/sitters/{sitterId}/reservations/new")
-    public String newReservation(@PathVariable("customerId") long customerId, @PathVariable("sitterId") long sitterId, Model model) {
-        ReservationResponse reservationInfo = sitterReservationService.getReservationDetails(customerId, sitterId);
-        model.addAttribute("reservationInfo", reservationInfo);
+    public String newReservation(@PathVariable("customerId") long customerId, @PathVariable("sitterId") long sitterId, Model model, @AuthenticationPrincipal MemberDetails memberDetails) {
+        if (memberDetails.getMember().getId() == customerId) {
+            ReservationResponse reservationInfo = sitterReservationService.getReservationDetails(customerId, sitterId);
+            model.addAttribute("reservationInfo", reservationInfo);
+            model.addAttribute("currentUser", memberDetails.getMember());
+        }
 
         return "reservation/new-reservation";
     }
 
     @Operation(description = "회원의 모든 돌봄 예약 내역 조회")
     @GetMapping("/members/{customerId}/reservations")
-    public String getAllReservation(@PathVariable("customerId") long customerId, Pageable pageable, Model model) {
+    public String getAllReservation(@PathVariable("customerId") long customerId, Pageable pageable, @AuthenticationPrincipal MemberDetails memberDetails, Model model) {
 //        List<CustomerReservationResponse.GetList> reservations = customerReservationService.findAllById(customerId);
-        Page<CustomerReservationResponse.GetList> reservations = customerReservationService.findAllById(customerId, pageable);
-        model.addAttribute("reservations", reservations);
+
+        if (memberDetails.getMember().getId() == customerId) {
+            Page<CustomerReservationResponse.GetList> reservations = customerReservationService.findAllById(customerId, pageable);
+            model.addAttribute("reservations", reservations);
+            model.addAttribute("currentUser", memberDetails.getMember());
+        }
 
         return "reservation/reservation-list";
     }
@@ -44,9 +53,13 @@ public class CustomerReservationViewController {
     @Operation(description = "회원의 특정 돌봄 예약 상세 조회")
     @GetMapping("/members/{customerId}/reservations/{customerReservationId}")
     public String getReservation(@PathVariable("customerId") long customerId,
-                                 @PathVariable("customerReservationId") long customerReservationId, Model model) {
-        CustomerReservationResponse.GetDetail reservation = customerReservationService.findById(customerId, customerReservationId);
-        model.addAttribute("reservation", reservation);
+                                 @PathVariable("customerReservationId") long customerReservationId,
+                                 @AuthenticationPrincipal MemberDetails memberDetails, Model model) {
+        if (memberDetails.getMember().getId() == customerId) {
+            CustomerReservationResponse.GetDetail reservation = customerReservationService.findById(customerId, customerReservationId);
+            model.addAttribute("reservation", reservation);
+            model.addAttribute("currentUser", memberDetails.getMember());
+        }
 
         return "reservation/reservation-detail";
     }
