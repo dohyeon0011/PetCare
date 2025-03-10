bb75f1ae75c501751ef097ef9dad544fa38e45ea
idohyeon <ehgus5026@naver.com>
2025-03-10 19:58:05 +0900

    -로그인 사용자 정보 읽기 수정(시큐리티), (관리자 페이지 - 돌봄 예약 관리)

--- src/main/java/com/PetSitter/controller/Admin/Reservation/api/AdminReservationApiController.java
+++ src/main/java/com/PetSitter/controller/Admin/Reservation/api/AdminReservationApiController.java
@@ -1,6 +1,7 @@
 package com.PetSitter.controller.Admin.Reservation.api;
 
 import com.PetSitter.domain.Member.Member;
+import com.PetSitter.domain.Member.MemberDetails;
 import com.PetSitter.domain.Reservation.ReservationSearch;
 import com.PetSitter.dto.Reservation.CustomerReservation.response.AdminReservationResponse;
 import com.PetSitter.service.Admin.Reservation.AdminReservationService;
@@ -10,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,9 +24,9 @@ public class AdminReservationApiController {
     @Operation(description = "관리자 페이지 모든 예약 목록 조회 API")
     @GetMapping("/reservations")
     public ResponseEntity<Page<AdminReservationResponse.ReservationListResponse>> findAllForAdmin(@ModelAttribute ReservationSearch reservationSearch,
-                                                                                                  @SessionAttribute("member") Member member,
+                                                                                                  @AuthenticationPrincipal MemberDetails memberDetails,
                                                                                                   @PageableDefault Pageable pageable) {
-        Page<AdminReservationResponse.ReservationListResponse> reservations = adminReservationService.findAllForAdmin(reservationSearch, member, pageable);
+        Page<AdminReservationResponse.ReservationListResponse> reservations = adminReservationService.findAllForAdmin(reservationSearch, memberDetails.getMember(), pageable);
 
         return ResponseEntity.ok()
                 .body(reservations);
@@ -33,8 +35,8 @@ public class AdminReservationApiController {
     @Operation(description = "관리자 페이지 예약 상세 정보 조회 API")
     @GetMapping("/reservations/{reservationId}")
     public ResponseEntity<AdminReservationResponse.ReservationDetailResponse> findForAdmin(@PathVariable("reservationId") long id,
-                                                                                           @SessionAttribute("member") Member member) {
-        AdminReservationResponse.ReservationDetailResponse reservation = adminReservationService.findForAdmin(id, member);
+                                                                                           @AuthenticationPrincipal MemberDetails memberDetails) {
+        AdminReservationResponse.ReservationDetailResponse reservation = adminReservationService.findForAdmin(id, memberDetails.getMember());
 
         return ResponseEntity.ok()
                 .body(reservation);
@@ -42,8 +44,8 @@ public class AdminReservationApiController {
 
     @Operation(description = "관리자 권한 예약 취소 API")
     @DeleteMapping("reservations/{reservationId}")
-    public ResponseEntity<Void> deleteForAdmin(@PathVariable("reservationId") long id, @SessionAttribute("member") Member member) {
-        adminReservationService.deleteForAdmin(id, member);
+    public ResponseEntity<Void> deleteForAdmin(@PathVariable("reservationId") long id, @AuthenticationPrincipal MemberDetails memberDetails) {
+        adminReservationService.deleteForAdmin(id, memberDetails.getMember());
 
         return ResponseEntity.noContent()
                 .build();
--- src/main/java/com/PetSitter/controller/Admin/Reservation/view/AdminReservationViewController.java
+++ src/main/java/com/PetSitter/controller/Admin/Reservation/view/AdminReservationViewController.java
@@ -1,6 +1,7 @@
 package com.PetSitter.controller.Admin.Reservation.view;
 
 import com.PetSitter.domain.Member.Member;
+import com.PetSitter.domain.Member.MemberDetails;
 import com.PetSitter.domain.Reservation.ReservationSearch;
 import com.PetSitter.dto.Reservation.CustomerReservation.response.AdminReservationResponse;
 import com.PetSitter.service.Admin.Reservation.AdminReservationService;
@@ -9,6 +10,7 @@ import org.hibernate.annotations.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -22,19 +24,25 @@ public class AdminReservationViewController {
 
     @Comment("관리자 페이지 모든 예약 목록 조회")
     @GetMapping("/reservations")
-    public String getAllForAdmin(@ModelAttribute ReservationSearch reservationSearch, @SessionAttribute("member") Member member,
+    public String getAllForAdmin(@ModelAttribute ReservationSearch reservationSearch, @AuthenticationPrincipal MemberDetails memberDetails,
                                  @PageableDefault Pageable pageable, Model model) {
-        Page<AdminReservationResponse.ReservationListResponse> reservations = adminReservationService.findAllForAdmin(reservationSearch, member, pageable);
-        model.addAttribute("reservations", reservations);
+        if (memberDetails.getMember() != null) {
+            Page<AdminReservationResponse.ReservationListResponse> reservations = adminReservationService.findAllForAdmin(reservationSearch, memberDetails.getMember(), pageable);
+            model.addAttribute("reservations", reservations);
+            model.addAttribute("currentUser", memberDetails.getMember());
+        }
 
         return "admin/reservation/reservation-list";
     }
 
     @Comment("관리자 페이지 예약 상세 정보 조회")
     @GetMapping("/reservations/{reservationId}")
-    public String getForAdmin(@PathVariable("reservationId") long id, @SessionAttribute("member") Member member, Model model) {
-        AdminReservationResponse.ReservationDetailResponse reservation = adminReservationService.findForAdmin(id, member);
-        model.addAttribute("reservation", reservation);
+    public String getForAdmin(@PathVariable("reservationId") long id, @AuthenticationPrincipal MemberDetails memberDetails, Model model) {
+        if (memberDetails.getMember() != null) {
+            AdminReservationResponse.ReservationDetailResponse reservation = adminReservationService.findForAdmin(id, memberDetails.getMember());
+            model.addAttribute("reservation", reservation);
+            model.addAttribute("currentUser", memberDetails.getMember());
+        }
 
         return "admin/reservation/reservation-detail";
     }
