8c8f7418b417f2bd2c59da7af259f743f1e89956
idohyeon <ehgus5026@naver.com>
2025-03-11 18:48:27 +0900

    -스웨거 수정 (관리자 페이지 - 회원 포인트 내역)

--- src/main/java/com/PetSitter/controller/Admin/PointHistory/api/AdminPointHistoryApiController.java
+++ src/main/java/com/PetSitter/controller/Admin/PointHistory/api/AdminPointHistoryApiController.java
@@ -6,6 +6,7 @@ import com.PetSitter.domain.PointHistory.PointSearch;
 import com.PetSitter.dto.PointHistory.response.AdminPointHistoryResponse;
 import com.PetSitter.service.Admin.PointHistory.AdminPointHistoryService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,9 +22,10 @@ public class AdminPointHistoryApiController {
 
     private final AdminPointHistoryService adminPointHistoryService;
 
-    @Operation(description = "관리자 페이지 - 회원 포인트 내역 전체 조회 API")
+    @Operation(summary = "관리자 페이지 - 회원 포인트 내역 전체 조회", description = "관리자 권한 - 회원 포인트 내역 전체 조회 API")
     @GetMapping("/amounts")
-    public ResponseEntity<Page<AdminPointHistoryResponse.PointListResponse>> findAllForAdmin(@ModelAttribute PointSearch pointSearch, @AuthenticationPrincipal MemberDetails memberDetails, @PageableDefault Pageable pageable) {
+    public ResponseEntity<Page<AdminPointHistoryResponse.PointListResponse>> findAllForAdmin(@ModelAttribute @Parameter(description = "검색 파라미터: 회원(고객) 이름 ex)\"\" 전체 조회") PointSearch pointSearch, @AuthenticationPrincipal MemberDetails memberDetails,
+                                                                                             @PageableDefault @Parameter(description = "페이징 파라미터, page: 페이지 번호 - 0부터 시작, size: 한 페이지의 데이터 개수") Pageable pageable) {
         Page<AdminPointHistoryResponse.PointListResponse> pointsHistory = adminPointHistoryService.findAllForAdmin(pointSearch, memberDetails.getMember(), pageable);
 
         return ResponseEntity.ok()
