2f3d798cfbb9cf38f73d41688b9300ab48ca1b9a
idohyeon <ehgus5026@naver.com>
2025-03-11 18:54:36 +0900

    -스웨거 수정 (돌봄사 -> 돌봄 일정 API Controller)

--- src/main/java/com/PetSitter/controller/Reservation/SitterSchedule/api/SitterScheduleApiController.java
+++ src/main/java/com/PetSitter/controller/Reservation/SitterSchedule/api/SitterScheduleApiController.java
@@ -1,9 +1,11 @@
 package com.PetSitter.controller.Reservation.SitterSchedule.api;
 
+import com.PetSitter.config.exception.BadRequestCustom;
 import com.PetSitter.domain.Member.MemberDetails;
 import com.PetSitter.dto.Reservation.SitterSchedule.response.SitterScheduleResponse;
 import com.PetSitter.service.Reservation.SitterSchedule.SitterScheduleService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,15 +20,15 @@ public class SitterScheduleApiController {
 
     private final SitterScheduleService sitterScheduleService;
 
-    @Operation(description = "돌봄사의 전체 돌봄 예약 목록 조회 API")
+    @Operation(summary = "돌봄사 - 전체 돌봄 예약 목록 조회", description = "전체 돌봄 예약 목록 조회 API")
     @GetMapping("/members/{sitterId}/schedules")
-    public ResponseEntity<Page<SitterScheduleResponse.GetList>> findAllSitterSchedule(@PathVariable("sitterId") long id, Pageable pageable,
+    public ResponseEntity<Page<SitterScheduleResponse.GetList>> findAllSitterSchedule(@PathVariable("sitterId") @Parameter(required = true, description = "회원(돌봄사) 고유 번호") long id,
+                                                                                      @Parameter(description = "페이징 파라미터, page: 페이지 번호 - 0부터 시작, size: 한 페이지의 데이터 개수") Pageable pageable,
                                                                                       @AuthenticationPrincipal MemberDetails memberDetails) {
 //        List<SitterScheduleResponse.GetList> sitterScheduleList = sitterScheduleService.findAllById(id);
 
-        if (memberDetails.getMember().getId() != id) {
-            return ResponseEntity.badRequest()
-                    .build();
+        if (memberDetails != null && memberDetails.getMember().getId() != id) {
+            throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
         }
 
         Page<SitterScheduleResponse.GetList> sitterScheduleList = sitterScheduleService.findAllById(id, pageable);
@@ -35,14 +37,13 @@ public class SitterScheduleApiController {
                 .body(sitterScheduleList);
     }
 
-    @Operation(description = "특정 돌봄사의 특정 돌봄 예약 정보 조회 API")
+    @Operation(summary = "돌봄사 - 특정 돌봄 예약 정보 조회", description = "특정 돌봄 예약 정보 조회 API")
     @GetMapping("/members/{sitterId}/schedules/{sitterScheduleId}")
-    public ResponseEntity<SitterScheduleResponse.GetDetail> findSitterSchedule(@PathVariable("sitterId") long id,
-                                                                               @PathVariable("sitterScheduleId") long sitterScheduleId,
+    public ResponseEntity<SitterScheduleResponse.GetDetail> findSitterSchedule(@PathVariable("sitterId") @Parameter(required = true, description = "회원(돌봄사) 고유 번호") long id,
+                                                                               @PathVariable("sitterScheduleId") @Parameter (required = true, description = "예약 고유 번호") long sitterScheduleId,
                                                                                @AuthenticationPrincipal MemberDetails memberDetails) {
         if (memberDetails.getMember().getId() != id) {
-            return ResponseEntity.badRequest()
-                    .build();
+            throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
         }
 
         SitterScheduleResponse.GetDetail sitterSchedule = sitterScheduleService.findById(id, sitterScheduleId);
@@ -51,13 +52,13 @@ public class SitterScheduleApiController {
                 .body(sitterSchedule);
     }
 
-    @Operation(description = "특정 돌봄사의 특정 돌봄 예약 취소 API")
+    @Operation(summary = "돌봄사 - 특정 돌봄 예약 취소", description = "특정 돌봄 예약 취소 API")
     @DeleteMapping("/members/{sitterId}/schedules/{sitterScheduleId}")
-    public ResponseEntity<Void> deleteSitterSchedule(@PathVariable("sitterId") long id, @PathVariable("sitterScheduleId") long sitterScheduleId,
+    public ResponseEntity<Void> deleteSitterSchedule(@PathVariable("sitterId") @Parameter(required = true, description = "회원(돌봄사) 고유 번호") long id,
+                                                     @PathVariable("sitterScheduleId") @Parameter(required = true, description = "예약 고유 번호") long sitterScheduleId,
                                                      @AuthenticationPrincipal MemberDetails memberDetails) {
         if (memberDetails.getMember().getId() != id) {
-            return ResponseEntity.badRequest()
-                    .build();
+            throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
         }
 
         sitterScheduleService.delete(id, sitterScheduleId);
