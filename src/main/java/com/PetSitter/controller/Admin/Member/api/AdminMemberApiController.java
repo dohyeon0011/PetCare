65876f5254284ce4b374dc1bf84c11379d8f79f6
idohyeon <ehgus5026@naver.com>
2025-03-11 18:45:33 +0900

    -스웨거 수정 (관리자 페이지 - 회원 관리)

--- src/main/java/com/PetSitter/controller/Admin/Member/api/AdminMemberApiController.java
+++ src/main/java/com/PetSitter/controller/Admin/Member/api/AdminMemberApiController.java
@@ -6,6 +6,7 @@ import com.PetSitter.domain.Member.MemberSearch;
 import com.PetSitter.dto.Member.response.AdminMemberResponse;
 import com.PetSitter.service.Admin.Member.AdminMemberService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,29 +22,29 @@ public class AdminMemberApiController {
 
     private final AdminMemberService adminMemberService;
 
-    @Operation(description = "관리자 페이지 모든 회원 목록 조회 API")
+    @Operation(summary = "관리자 페이지 - 모든 회원 목록 조회", description = "관리자 권한 - 모든 회원 목록 조회 API")
     @GetMapping("/members")
     public ResponseEntity<Page<AdminMemberResponse.MemberListResponse>> getAllMember(@AuthenticationPrincipal MemberDetails memberDetails,
-                                                                                     @ModelAttribute MemberSearch memberSearch,
-                                                                                     @PageableDefault Pageable pageable) {
+                                                                                     @ModelAttribute @Parameter(description = "검색 파라미터: 회원(고객) 이름 ex)\"\" 전체 조회") MemberSearch memberSearch,
+                                                                                     @PageableDefault @Parameter(description = "페이징 파라미터, page: 페이지 번호 - 0부터 시작, size: 한 페이지의 데이터 개수") Pageable pageable) {
         Page<AdminMemberResponse.MemberListResponse> members = adminMemberService.findAllForAdmin(memberDetails.getMember(), memberSearch, pageable);
 
         return ResponseEntity.ok()
                 .body(members);
     }
 
-    @Operation(description = "관리자 페이지 회원 상세 정보 조회 API")
+    @Operation(summary = "관리자 페이지 - 회원 상세 정보 조회", description = "관리자 권한 - 회원 상세 정보 조회 API")
     @GetMapping("/members/{memberId}")
-    public ResponseEntity<?> getMemberDetail(@PathVariable("memberId") long id, @AuthenticationPrincipal MemberDetails memberDetails) {
+    public ResponseEntity<?> getMemberDetail(@PathVariable("memberId") @Parameter(required = true, description = "회원 고유 번호") long id, @AuthenticationPrincipal MemberDetails memberDetails) {
         Object findMember = adminMemberService.findByIdForAdmin(id, memberDetails.getMember());
 
         return ResponseEntity.ok()
                 .body(findMember);
     }
 
-    @Operation(description = "관리자 권한 회원 탈퇴 API")
+    @Operation(summary = "관리자 페이지 - 회원 탈퇴", description = "관리자 권한 - 회원 탈퇴 API")
     @DeleteMapping("/members/{memberId}")
-    public ResponseEntity<Void> deleteMember(@PathVariable("memberId") long id, @AuthenticationPrincipal MemberDetails memberDetails) {
+    public ResponseEntity<Void> deleteMember(@PathVariable("memberId") @Parameter(required = true, description = "회원 고유 번호") long id, @AuthenticationPrincipal MemberDetails memberDetails) {
         adminMemberService.deleteForAdmin(id, memberDetails.getMember());
 
         return ResponseEntity.noContent()
