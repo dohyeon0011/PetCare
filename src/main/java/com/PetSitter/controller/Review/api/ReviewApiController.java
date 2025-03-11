61a270b61b0b02bf3851cf09567d00bdcefc3bb1
idohyeon <ehgus5026@naver.com>
2025-03-11 18:55:58 +0900

    -스웨거 수정 (돌봄 리뷰 API Controller)

--- src/main/java/com/PetSitter/controller/Review/api/ReviewApiController.java
+++ src/main/java/com/PetSitter/controller/Review/api/ReviewApiController.java
@@ -1,5 +1,6 @@
 package com.PetSitter.controller.Review.api;
 
+import com.PetSitter.config.exception.BadRequestCustom;
 import com.PetSitter.domain.Member.MemberDetails;
 import com.PetSitter.domain.Review.ReviewSearch;
 import com.PetSitter.dto.Review.request.AddReviewRequest;
@@ -7,6 +8,7 @@ import com.PetSitter.dto.Review.request.UpdateReviewRequest;
 import com.PetSitter.dto.Review.response.ReviewResponse;
 import com.PetSitter.service.Review.ReviewService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -25,14 +27,13 @@ public class ReviewApiController {
 
     private final ReviewService reviewService;
 
-    @Operation(description = "리뷰 작성 API")
+    @Operation(summary = "고객 - 리뷰 작성", description = "리뷰 작성 API")
     @PostMapping("/members/{customerId}/reservations/{customerReservationId}/reviews/new")
-    public ResponseEntity<ReviewResponse.GetDetail> saveReview(@PathVariable("customerId") long customerId,
-                                                               @PathVariable("customerReservationId") long reservationId,
+    public ResponseEntity<ReviewResponse.GetDetail> saveReview(@PathVariable("customerId") @Parameter(required = true, description = "회원(고객) 고유 번호") long customerId,
+                                                               @PathVariable("customerReservationId") @Parameter(required = true, description = "예약 고유 번호") long reservationId,
                                                                @RequestBody @Valid AddReviewRequest request, @AuthenticationPrincipal MemberDetails memberDetails) {
-        if (memberDetails.getMember().getId() != customerId) {
-            return ResponseEntity.badRequest()
-                    .build();
+        if (memberDetails != null && memberDetails.getMember().getId() != customerId) {
+            throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
         }
 
         ReviewResponse.GetDetail review = reviewService.save(customerId, reservationId, request);
@@ -41,14 +42,14 @@ public class ReviewApiController {
                 .body(review);
     }
 
-    @Operation(description = "특정 회원의 모든 리뷰 조회 API")
+    @Operation(summary = "모든 리뷰 조회", description = "모든 리뷰 조회 API")
     @GetMapping("/members/{memberId}/reviews")
-    public ResponseEntity<Page<ReviewResponse.GetList>> findAllReview(@PathVariable("memberId") long memberId, Pageable pageable, @AuthenticationPrincipal MemberDetails memberDetails) {
+    public ResponseEntity<Page<ReviewResponse.GetList>> findAllReview(@PathVariable("memberId") @Parameter(required = true, description = "회원 고유 번호") long memberId,
+                                                                      @Parameter(description = "페이징 파라미터, page: 페이지 번호 - 0부터 시작, size: 한 페이지의 데이터 개수") Pageable pageable, @AuthenticationPrincipal MemberDetails memberDetails) {
 //        List<ReviewResponse.GetList> reviews = reviewService.findAllById(memberId);
 
-        if (memberDetails.getMember().getId() != memberId) {
-            return ResponseEntity.badRequest()
-                    .build();
+        if (memberDetails != null && memberDetails.getMember().getId() != memberId) {
+            throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
         }
 
         Page<ReviewResponse.GetList> reviews = reviewService.findAllById(memberId, pageable);
@@ -57,21 +58,21 @@ public class ReviewApiController {
                 .body(reviews);
     }
 
-    @Operation(description = "특정 리뷰 조회 API")
+    @Operation(summary = "특정 리뷰 조회", description = "특정 리뷰 조회 API")
     @GetMapping("/reviews/{reviewId}")
-    public ResponseEntity<ReviewResponse.GetDetail> findReview(@PathVariable("reviewId") long reviewId) {
+    public ResponseEntity<ReviewResponse.GetDetail> findReview(@PathVariable("reviewId") @Parameter(required = true, description = "리뷰 고유 번호") long reviewId) {
         ReviewResponse.GetDetail review = reviewService.findById(reviewId);
 
         return ResponseEntity.ok()
                 .body(review);
     }
 
-    @Operation(description = "특정 리뷰 삭제 API")
+    @Operation(summary = "고객 - 특정 리뷰 삭제", description = "특정 리뷰 삭제 API")
     @DeleteMapping("/members/{customerId}/reviews/{reviewId}")
-    public ResponseEntity<Void> deleteReview(@PathVariable("customerId") long customerId, @PathVariable("reviewId") long reviewId, @AuthenticationPrincipal MemberDetails memberDetails) {
+    public ResponseEntity<Void> deleteReview(@PathVariable("customerId") @Parameter(required = true, description = "회원(고객) 고유 번호") long customerId,
+                                             @PathVariable("reviewId") @Parameter(required = true, description = "리뷰 고유 번호") long reviewId, @AuthenticationPrincipal MemberDetails memberDetails) {
         if (memberDetails.getMember().getId() != customerId) {
-            return ResponseEntity.badRequest()
-                    .build();
+            throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
         }
 
         reviewService.delete(customerId, reviewId);
@@ -80,15 +81,14 @@ public class ReviewApiController {
                 .build();
     }
 
-    @Operation(description = "특정 리뷰 수정 API")
+    @Operation(summary = "고객 - 특정 리뷰 수정", description = "특정 리뷰 수정 API")
     @PutMapping("/members/{customerId}/reviews/{reviewId}")
-    public ResponseEntity<ReviewResponse.GetDetail> updateReview(@PathVariable("customerId") long customerId,
-                                                                 @PathVariable("reviewId") long reviewId,
+    public ResponseEntity<ReviewResponse.GetDetail> updateReview(@PathVariable("customerId") @Parameter(required = true, description = "회원(고객) 고유 번호") long customerId,
+                                                                 @PathVariable("reviewId") @Parameter(required = true, description = "리뷰 고유 번호") long reviewId,
                                                                  @RequestBody @Valid UpdateReviewRequest request,
                                                                  @AuthenticationPrincipal MemberDetails memberDetails) {
-        if (memberDetails.getMember().getId() != customerId) {
-            return ResponseEntity.badRequest()
-                    .build();
+        if (memberDetails != null && memberDetails.getMember().getId() != customerId) {
+            throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
         }
 
         ReviewResponse.GetDetail updateReview = reviewService.update(customerId, reviewId, request);
@@ -97,14 +97,13 @@ public class ReviewApiController {
                 .body(updateReview);
     }
 
-    @Operation(description = "리뷰 작성 시 보여질 폼 데이터")
+    @Operation(summary = "리뷰 작성 시 보여질 폼 데이터", description = "리뷰 작성 시 보여질 폼 데이터")
     @GetMapping("/members/{customerId}/reservations/{customerReservationId}/reviews/new")
-    public ResponseEntity<ReviewResponse.GetNewReview> getReview(@PathVariable("customerId") long customerId,
-                                                                 @PathVariable("customerReservationId") long customerReservationId,
+    public ResponseEntity<ReviewResponse.GetNewReview> getReview(@PathVariable("customerId") @Parameter(required = true, description = "회원(고객) 고유 번호") long customerId,
+                                                                 @PathVariable("customerReservationId") @Parameter(required = true, description = "예약 고유 번호") long customerReservationId,
                                                                  @AuthenticationPrincipal MemberDetails memberDetails) {
-        if (memberDetails.getMember().getId() != customerId) {
-            return ResponseEntity.badRequest()
-                    .build();
+        if (memberDetails != null && memberDetails.getMember().getId() != customerId) {
+            throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
         }
 
         ReviewResponse.GetNewReview newReview = reviewService.getNewReview(customerId, customerReservationId);
@@ -119,10 +118,10 @@ public class ReviewApiController {
      * 2. 파라미터 개수가 많아져도 코드가 깔끔해짐
      * 3. 스프링이 자동으로 요청 파라미터를 객체 필드에 매핑해줌(@ModelAttribute를 사용하면 쿼리 파라미터를 자동으로 객체 필드에 매핑)
      */
-    @Operation(description = "이용 후기 페이지에서 전체 리뷰 및 특정 돌봄사의 리뷰 조회 API: V2")
+    @Operation(summary = "이용 후기 페이지 - 전체 리뷰 및 특정 돌봄사의 리뷰 조회", description = "이용 후기 페이지에서 전체 리뷰 및 특정 돌봄사의 리뷰 조회 API: V2")
     @GetMapping("/reviews")
-    public ResponseEntity<List<ReviewResponse.GetDetail>> getAllReviewsBySitterV2(@ModelAttribute ReviewSearch reviewSearch,
-                                                                                  @RequestParam(defaultValue = "0") int page) {
+    public ResponseEntity<List<ReviewResponse.GetDetail>> getAllReviewsBySitterV2(@ModelAttribute @Parameter(description = "검색 파라미터: 회원(돌봄사) 이름") ReviewSearch reviewSearch,
+                                                                                  @RequestParam(defaultValue = "0") @Parameter(description = "페이징 파라미터, page: 페이지 번호 - 0부터 시작, size: 한 페이지의 데이터 개수") int page) {
         List<ReviewResponse.GetDetail> reviews = reviewService.getAllReviewsBySitterV2(reviewSearch, page);
 
         return ResponseEntity.ok()
