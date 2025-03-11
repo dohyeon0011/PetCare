aa345f4e5693b95a0de868eab2da7ea55e2ac5c5
idohyeon <ehgus5026@naver.com>
2025-03-11 18:44:36 +0900

    -스웨거 수정 (관리자 - 돌봄 케어 로그 관리)

--- src/main/java/com/PetSitter/controller/Admin/CareLog/api/AdminCareLogApiController.java
+++ src/main/java/com/PetSitter/controller/Admin/CareLog/api/AdminCareLogApiController.java
@@ -4,6 +4,7 @@ import com.PetSitter.domain.Member.Member;
 import com.PetSitter.domain.Member.MemberDetails;
 import com.PetSitter.service.Admin.CareLog.AdminCareLogService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -16,9 +17,9 @@ public class AdminCareLogApiController {
 
     private final AdminCareLogService adminCareLogService;
 
-    @Operation(description = "관리자 권한 돌봄 케어 로그 삭제 API")
+    @Operation(summary = "관리자 페이지 - 돌봄 케어 로그 삭제", description = "관리자 권한 - 돌봄 케어 로그 삭제 API")
     @DeleteMapping("/care-logs/{careLogId}")
-    public ResponseEntity<Void> deleteForAdmin(@PathVariable("careLogId") long id, @AuthenticationPrincipal MemberDetails memberDetails) {
+    public ResponseEntity<Void> deleteForAdmin(@PathVariable("careLogId") @Parameter(required = true, description = "돌봄 케어 로그 고유 번호") long id, @AuthenticationPrincipal MemberDetails memberDetails) {
         adminCareLogService.deleteForAdmin(id, memberDetails.getMember());
 
         return ResponseEntity.noContent()
