77b323116c82cdb4c9e81a6e0f0a8bea9d053cb3
idohyeon <ehgus5026@naver.com>
2025-03-14 18:50:19 +0900

    -로컬 디렉토리 파일 경로 설정 클래스

---
+++ src/main/java/com/PetSitter/config/WebImageConfig.java
@@ -0,0 +1,21 @@
+package com.PetSitter.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebImageConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/profile/**") // 디렉토리 내 파일들을 제공
+                .addResourceLocations("file:/Users/idohyeon/petcare-uploads/profile/"); // 서버 내부 경로
+
+        registry.addResourceHandler("/uploads/pets/**")
+                .addResourceLocations("file:/Users/idohyeon/petcare-uploads/pets/");
+
+        registry.addResourceHandler("/uploads/carelogs/**")
+                .addResourceLocations("file:/Users/idohyeon/petcare-uploads/carelogs/");
+    }
+}
