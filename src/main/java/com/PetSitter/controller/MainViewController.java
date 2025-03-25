1920f314755a763b30bfb33e0dd6918e0d5c64de
idohyeon <ehgus5026@naver.com>
2025-03-25 16:15:41 +0900

    -Main VIEW Controller

--- src/main/java/com/PetSitter/controller/MainViewController.java
+++ src/main/java/com/PetSitter/controller/MainViewController.java
@@ -2,6 +2,7 @@ package com.PetSitter.controller;
 
 import com.PetSitter.domain.Member.MemberDetails;
 import com.PetSitter.dto.Review.response.ReviewResponse;
+import com.PetSitter.service.Member.MemberService;
 import com.PetSitter.service.Review.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.annotations.Comment;
@@ -18,6 +19,7 @@ import java.util.List;
 @RequestMapping("/pets-care")
 public class MainViewController {
 
+    private final MemberService memberService;
     private final ReviewService reviewService;
 
     @Comment("메인 페이지")
@@ -41,13 +43,21 @@ public class MainViewController {
 
     @Comment("펫시터 정보 페이지(펫시터 서비스)")
     @GetMapping("/pet-sitter/information")
-    public String getInfo() {
+    public String getInfo(@AuthenticationPrincipal MemberDetails memberDetails, Model model) {
+        if (memberDetails != null) {
+            model.addAttribute("currentUser", memberDetails.getMember());
+        }
+
         return "intro";
     }
 
     @Comment("메인 페이지 - 자세히 보기 1 (펫시터의 집에서 돌봄을!) 안내")
     @GetMapping("/sitter")
-    public String getSitter(Model model) {
+    public String getSitter(@AuthenticationPrincipal MemberDetails memberDetails, Model model) {
+        if (memberDetails != null) {
+            model.addAttribute("currentUser", memberDetails.getMember());
+        }
+
         List<ReviewResponse.GetDetail> reviews = reviewService.getAllReview();
 
         model.addAttribute("reviews", reviews);
@@ -57,13 +67,25 @@ public class MainViewController {
 
     @Comment("메인 페이지 - 자세히 보기 2 (고양이 돌봄 서비스) 안내")
     @GetMapping("/cat-care")
-    public String getTrainer() {
+    public String getTrainer(@AuthenticationPrincipal MemberDetails memberDetails, Model model) {
+        if (memberDetails != null) {
+            model.addAttribute("currentUser", memberDetails.getMember());
+        }
+
         return "cat-care-info";
     }
 
     @Comment("메인 페이지 - 자세히 보기 3 (어떤 분들이 활동하고 있나요?) 안내")
     @GetMapping("/sitters-information")
-    public String getSittersInfo() {
+    public String getSittersInfo(@AuthenticationPrincipal MemberDetails memberDetails, Model model) {
+        if (memberDetails != null) {
+            model.addAttribute("currentUser", memberDetails.getMember());
+        }
+
+        Object bestSitters = memberService.findBestSitters();
+
+        model.addAttribute("bestSitters", bestSitters);
+
         return "sitters-info";
     }
 
