17c5a09385d426e4ffbc2faedfef697dd4eca7cf
idohyeon <ehgus5026@naver.com>
2025-03-25 16:16:09 +0900

    -회원 Repository(+리뷰가 가장 많은 대표 돌봄사 최상위 3명 조회)

--- src/main/java/com/PetSitter/repository/Member/MemberRepository.java
+++ src/main/java/com/PetSitter/repository/Member/MemberRepository.java
@@ -3,6 +3,8 @@ package com.PetSitter.repository.Member;
 import com.PetSitter.domain.Member.Member;
 import com.PetSitter.domain.Member.Role;
 import com.PetSitter.domain.Pet.Pet;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -34,4 +36,20 @@ public interface MemberRepository extends JpaRepository<Member, Long>, MemberRep
 
     @Query("select p from Pet p where p.customer.id = :customerId")
     List<Pet> findPetsByCustomerId(@Param("customerId") long customerId);
+
+    // 리뷰가 가장 많은 대표 돌봄사 최상위 3명 조회(메인 페이지 - 자세히 보기 3 (어떤 분들이 활동하고 있나요?) 안내)
+    @Query("""
+        select m
+        from Member m
+        join fetch m.certifications
+        where m.id in (
+            select cr.sitter.id
+            from CustomerReservation cr
+            join cr.review r
+            group by cr.sitter.id
+            order by count(r.id) desc
+        )
+        """)
+    Page<Member> findBestSitters(Pageable pageable);
+
 }
