1ad0948aebdf5c801c35b9fec119b91d2e2648ac
idohyeon <ehgus5026@naver.com>
2025-03-26 19:02:10 +0900

    -Setting(+QueryDSL, 타임존 확인)

--- src/main/java/com/PetSitter/PetSitterApplication.java
+++ src/main/java/com/PetSitter/PetSitterApplication.java
@@ -7,12 +7,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.time.ZoneId;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing // created_at, updated_at 자동 업데이트
 public class PetSitterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetSitterApplication.class, args);
+
+		System.out.println("System TimeZone: " + TimeZone.getDefault().getID());
+		System.out.println("Java 17 ZoneId: " + ZoneId.systemDefault());
 	}
 
 	@Bean
