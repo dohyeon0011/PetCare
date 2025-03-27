38aa0f3b5e28eba86cecf45524716773e19dfbee
idohyeon <ehgus5026@naver.com>
2025-03-27 18:47:31 +0900

    -OAuth2 로그인 유저(카카오)

---
+++ src/main/java/com/PetSitter/domain/Member/oauth/KakaoOAuth2UserInfo.java
@@ -0,0 +1,43 @@
+package com.PetSitter.domain.Member.oauth;
+
+import java.util.Map;
+
+/**
+ * 카카오 사용자 정보 제공 데이터 스펙
+ * {
+ *   "id": 987654321,
+ *   "kakao_account": {
+ *     "profile": { "nickname": "길동이" },
+ *     "email": "gildong@kakao.com"
+ *   }
+ * }
+ */
+public class KakaoOAuth2UserInfo extends OAuth2UserInfo {
+
+    public KakaoOAuth2UserInfo(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    public String getId() {
+        return String.valueOf(attributes.get("id")); // 카카오는 "id" 필드를 ID로 사용
+    }
+
+    @Override
+    public String getName() {
+        Map<String, Object> profile = (Map<String, Object>) ((Map<String, Object>) attributes.get("kakao_account")).get("profile");
+        return profile != null ? (String) profile.get("nickname") : "사용자";
+    }
+
+    @Override
+    public String getEmail() {
+        Map<String, Object> account = (Map<String, Object>) attributes.get("kakao_account");
+        return account != null ? (String) account.get("email") : null;
+    }
+
+    @Override
+    public String getImageUrl() {
+        Map<String, Object> profile = (Map<String, Object>) ((Map<String, Object>) attributes.get("kakao_account")).get("profile");
+        return profile != null ? (String) profile.get("profile_image_url") : null;
+    }
+}
