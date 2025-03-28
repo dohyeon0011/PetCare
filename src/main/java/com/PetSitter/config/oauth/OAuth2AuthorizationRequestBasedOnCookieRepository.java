16a3986e7b1de3b2b0addee671bb66ecbb540754
idohyeon <ehgus5026@naver.com>
2025-03-28 20:23:37 +0900

    -OAuth2 인증 요청 정보 쿠키 저장 클래스

--- src/main/java/com/PetSitter/config/oauth/OAuth2AuthorizationRequestBasedOnCookieRepository.java
+++ src/main/java/com/PetSitter/config/oauth/OAuth2AuthorizationRequestBasedOnCookieRepository.java
@@ -11,7 +11,7 @@ import org.springframework.web.util.WebUtils;
 
 /**
  * OAuth2 인증 요청(필요한 정보)을 세션이 아닌 쿠키에 저장하고 로드하는 데 사용되는 구현체(인증 요청과 관련된 상태를 저장할 저장소)
- * OAuth2의 정보를 가져오고 저장하는 로직
+ * OAuth2의 정보를 가져오고 저장하는 로직(OAuth2 인증 요청 정보를 쿠키에 저장하고 관리)
  */
 @Component
 public class OAuth2AuthorizationRequestBasedOnCookieRepository implements AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
@@ -19,17 +19,31 @@ public class OAuth2AuthorizationRequestBasedOnCookieRepository implements Author
     public final static String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
     private final static int COOKIE_EXPIRE_SECONDS = 18000; // 5 hours
 
+    /**
+     * OAuth2 인증 요청 삭제
+     * 인증 요청을 제거할 때, 쿠키에서 해당 정보를 삭제
+     */
     @Override
     public OAuth2AuthorizationRequest removeAuthorizationRequest(HttpServletRequest request, HttpServletResponse response) {
         return this.loadAuthorizationRequest(request); // 쿠키에서 인증 요청을 불러옴
     }
 
+    /**
+     * OAuth2 인증 요청 불러오기
+     * 현재 요청과 관련된 인증 정보를 쿠키에서 찾아서 가져옴
+     * 로그인 성공 후, 로그인 요청 전에 사용자가 있던 위치로 리디렉트할 수 있도록 복원
+     */
     @Override
     public OAuth2AuthorizationRequest loadAuthorizationRequest(HttpServletRequest request) {
         Cookie cookie = WebUtils.getCookie(request, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
         return CookieUtil.deserialize(cookie, OAuth2AuthorizationRequest.class); // 쿠키에서 인증 요청을 디시리얼라이즈
     }
 
+    /**
+     * OAuth2 인증 요청 정보를 쿠키에 저장
+     * 사용자가 OAuth2 로그인을 시도하면, OAuth2AuthorizationRequest 객체를 쿠키에 저장
+     * 이렇게 하면 인증이 완료된 후 원래 요청을 복원 가능
+     */
     @Override
     public void saveAuthorizationRequest(OAuth2AuthorizationRequest authorizationRequest,
                                          HttpServletRequest request, HttpServletResponse response) {
