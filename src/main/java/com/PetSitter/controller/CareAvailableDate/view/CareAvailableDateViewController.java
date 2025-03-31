753cd8d63c60bbc794cd5a55ea12a762929858e3
idohyeon <ehgus5026@naver.com>
2025-03-31 21:11:06 +0900

    -돌봄사 시점: 돌봄 가능 날짜 VIEW Controller(OAuth2 로그인 유저 식별)

--- src/main/java/com/PetSitter/controller/CareAvailableDate/view/CareAvailableDateViewController.java
+++ src/main/java/com/PetSitter/controller/CareAvailableDate/view/CareAvailableDateViewController.java
@@ -1,6 +1,8 @@
 package com.PetSitter.controller.CareAvailableDate.view;
 
+import com.PetSitter.domain.Member.Member;
 import com.PetSitter.domain.Member.MemberDetails;
+import com.PetSitter.domain.Member.oauth.CustomOAuth2User;
 import com.PetSitter.dto.CareAvailableDate.response.CareAvailableDateResponse;
 import com.PetSitter.service.CareAvailableDate.CareAvailableDateService;
 import lombok.RequiredArgsConstructor;
@@ -25,9 +27,15 @@ public class CareAvailableDateViewController {
 
     @Comment("돌봄 가능 일정 등록")
     @GetMapping("/care-available-dates/new")
-    public String newCareAvailableDate(@AuthenticationPrincipal MemberDetails memberDetails, Model model) {
-        if (memberDetails != null && memberDetails.getMember() != null) {
-            model.addAttribute("currentUser", memberDetails.getMember());
+    public String newCareAvailableDate(@AuthenticationPrincipal Object principal, Model model) {
+        Member member;
+
+        if (principal instanceof MemberDetails && ((MemberDetails) principal).getMember() != null) {   // 일반 폼 로그인 사용자의 경우
+            member = ((MemberDetails) principal).getMember();
+            model.addAttribute("currentUser", member);
+        } else if (principal instanceof CustomOAuth2User && ((CustomOAuth2User) principal).getMember() != null) { // OAuth2 소셜 로그인 사용자의 경우
+            member = ((CustomOAuth2User) principal).getMember();
+            model.addAttribute("currentUser", member);
         }
 
         model.addAttribute("careAvailableDate", new CareAvailableDateResponse.GetList());
@@ -37,12 +45,19 @@ public class CareAvailableDateViewController {
 
     @Comment("회원의 등록한 모든 돌봄 일정 조회")
     @GetMapping("/members/{sitterId}/care-available-dates")
-    public String getCareAvailableDateList(@PathVariable("sitterId") long sitterId, Pageable pageable, @AuthenticationPrincipal MemberDetails memberDetails, Model model) {
-        if (memberDetails != null && memberDetails.getMember().getId() == sitterId) {
-            Page<CareAvailableDateResponse.GetList> careAvailableDates = careAvailableDateService.findAllById(sitterId, pageable);
-            model.addAttribute("careAvailableDates", careAvailableDates);
-            model.addAttribute("currentUser", memberDetails.getMember());
+    public String getCareAvailableDateList(@PathVariable("sitterId") long sitterId, @AuthenticationPrincipal Object principal, Model model, Pageable pageable) {
+        Member member;
+
+        if (principal instanceof MemberDetails && ((MemberDetails) principal).getMember() != null) {   // 일반 폼 로그인 사용자의 경우
+            member = ((MemberDetails) principal).getMember();
+            model.addAttribute("currentUser", member);
+        } else if (principal instanceof CustomOAuth2User && ((CustomOAuth2User) principal).getMember() != null) { // OAuth2 소셜 로그인 사용자의 경우
+            member = ((CustomOAuth2User) principal).getMember();
+            model.addAttribute("currentUser", member);
         }
+
+        Page<CareAvailableDateResponse.GetList> careAvailableDates = careAvailableDateService.findAllById(sitterId, pageable);
+        model.addAttribute("careAvailableDates", careAvailableDates);
 
         return "careavailabledate/care-available-date-list";
     }
@@ -51,12 +66,19 @@ public class CareAvailableDateViewController {
     @GetMapping("/members/{sitterId}/care-available-dates/{careAvailableDateId}")
     public String getCareAvailableDate(@PathVariable("sitterId") long sitterId,
                                        @PathVariable("careAvailableDateId") long careAvailableDateId,
-                                       @AuthenticationPrincipal MemberDetails memberDetails, Model model) {
-        if (memberDetails != null && memberDetails.getMember().getId() == sitterId) {
-            CareAvailableDateResponse.GetDetail careAvailableDate = careAvailableDateService.findById(sitterId, careAvailableDateId);
-            model.addAttribute("careAvailableDate", careAvailableDate);
-            model.addAttribute("currentUser", memberDetails.getMember());
+                                       @AuthenticationPrincipal Object principal, Model model) {
+        Member member;
+
+        if (principal instanceof MemberDetails && ((MemberDetails) principal).getMember() != null) {   // 일반 폼 로그인 사용자의 경우
+            member = ((MemberDetails) principal).getMember();
+            model.addAttribute("currentUser", member);
+        } else if (principal instanceof CustomOAuth2User && ((CustomOAuth2User) principal).getMember() != null) { // OAuth2 소셜 로그인 사용자의 경우
+            member = ((CustomOAuth2User) principal).getMember();
+            model.addAttribute("currentUser", member);
         }
+
+        CareAvailableDateResponse.GetDetail careAvailableDate = careAvailableDateService.findById(sitterId, careAvailableDateId);
+        model.addAttribute("careAvailableDate", careAvailableDate);
 
         return "careavailabledate/care-available-date-detail";
     }
@@ -64,14 +86,20 @@ public class CareAvailableDateViewController {
     @Comment("회원의 등록한 돌봄 가능 일정 수정")
     @GetMapping("/members/{sitterId}/care-available-dates/{careAvailableDateId}/edit")
     public String editCareAvailableDate(@PathVariable("sitterId") long sitterId, @PathVariable("careAvailableDateId") long careAvailableDateId,
-                                        @AuthenticationPrincipal MemberDetails memberDetails, Model model) {
-        if (memberDetails != null && memberDetails.getMember().getId() == sitterId) {
-            CareAvailableDateResponse.GetDetail careAvailableDate = careAvailableDateService.findById(sitterId, careAvailableDateId);
-            model.addAttribute("careAvailableDate", careAvailableDate);
-            model.addAttribute("currentUser", memberDetails.getMember());
+                                        @AuthenticationPrincipal Object principal, Model model) {
+        Member member;
+
+        if (principal instanceof MemberDetails && ((MemberDetails) principal).getMember() != null) {   // 일반 폼 로그인 사용자의 경우
+            member = ((MemberDetails) principal).getMember();
+            model.addAttribute("currentUser", member);
+        } else if (principal instanceof CustomOAuth2User && ((CustomOAuth2User) principal).getMember() != null) { // OAuth2 소셜 로그인 사용자의 경우
+            member = ((CustomOAuth2User) principal).getMember();
+            model.addAttribute("currentUser", member);
         }
+
+        CareAvailableDateResponse.GetDetail careAvailableDate = careAvailableDateService.findById(sitterId, careAvailableDateId);
+        model.addAttribute("careAvailableDate", careAvailableDate);
 
         return "careavailabledate/edit-care-available-date";
     }
-
 }
