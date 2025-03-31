ab279d13d4654cf9327ec8e1d02e0e195690b680
idohyeon <ehgus5026@naver.com>
2025-03-31 21:12:42 +0900

    -메인 VIEW Controller(OAuth2 로그인 유저 식별)

--- src/main/java/com/PetSitter/controller/MainViewController.java
+++ src/main/java/com/PetSitter/controller/MainViewController.java
@@ -1,12 +1,15 @@
 package com.PetSitter.controller;
 
+import com.PetSitter.domain.Member.Member;
 import com.PetSitter.domain.Member.MemberDetails;
+import com.PetSitter.domain.Member.oauth.CustomOAuth2User;
 import com.PetSitter.dto.Review.response.ReviewResponse;
 import com.PetSitter.service.Member.MemberService;
 import com.PetSitter.service.Review.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.annotations.Comment;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,9 +27,15 @@ public class MainViewController {
 
     @Comment("메인 페이지")
     @GetMapping("/main")
-    public String home(@AuthenticationPrincipal MemberDetails memberDetails, Model model) {
-        if (memberDetails != null) {
-            model.addAttribute("currentUser", memberDetails.getMember());
+    public String home(@AuthenticationPrincipal Object principal, Model model) {
+        Member member;
+
+        if (principal instanceof MemberDetails && ((MemberDetails) principal).getMember() != null) {   // 일반 폼 로그인 사용자의 경우
+            member = ((MemberDetails) principal).getMember();
+            model.addAttribute("currentUser", member);
+        } else if (principal instanceof CustomOAuth2User && ((CustomOAuth2User) principal).getMember() != null) { // OAuth2 소셜 로그인 사용자의 경우
+            member = ((CustomOAuth2User) principal).getMember();
+            model.addAttribute("currentUser", member);
         }
 
         List<ReviewResponse.GetDetail> reviews = reviewService.getAllReview();
@@ -43,9 +52,15 @@ public class MainViewController {
 
     @Comment("펫시터 정보 페이지(펫시터 서비스)")
     @GetMapping("/pet-sitter/information")
-    public String getInfo(@AuthenticationPrincipal MemberDetails memberDetails, Model model) {
-        if (memberDetails != null) {
-            model.addAttribute("currentUser", memberDetails.getMember());
+    public String getInfo(@AuthenticationPrincipal Object principal, Model model) {
+        Member member;
+
+        if (principal instanceof MemberDetails && ((MemberDetails) principal).getMember() != null) {   // 일반 폼 로그인 사용자의 경우
+            member = ((MemberDetails) principal).getMember();
+            model.addAttribute("currentUser", member);
+        } else if (principal instanceof CustomOAuth2User && ((CustomOAuth2User) principal).getMember() != null) { // OAuth2 소셜 로그인 사용자의 경우
+            member = ((CustomOAuth2User) principal).getMember();
+            model.addAttribute("currentUser", member);
         }
 
         return "intro";
@@ -53,9 +68,15 @@ public class MainViewController {
 
     @Comment("메인 페이지 - 자세히 보기 1 (펫시터의 집에서 돌봄을!) 안내")
     @GetMapping("/sitter")
-    public String getSitter(@AuthenticationPrincipal MemberDetails memberDetails, Model model) {
-        if (memberDetails != null) {
-            model.addAttribute("currentUser", memberDetails.getMember());
+    public String getSitter(@AuthenticationPrincipal Object principal, Model model) {
+        Member member;
+
+        if (principal instanceof MemberDetails && ((MemberDetails) principal).getMember() != null) {   // 일반 폼 로그인 사용자의 경우
+            member = ((MemberDetails) principal).getMember();
+            model.addAttribute("currentUser", member);
+        } else if (principal instanceof CustomOAuth2User && ((CustomOAuth2User) principal).getMember() != null) { // OAuth2 소셜 로그인 사용자의 경우
+            member = ((CustomOAuth2User) principal).getMember();
+            model.addAttribute("currentUser", member);
         }
 
         List<ReviewResponse.GetDetail> reviews = reviewService.getAllReview();
@@ -67,9 +88,15 @@ public class MainViewController {
 
     @Comment("메인 페이지 - 자세히 보기 2 (고양이 돌봄 서비스) 안내")
     @GetMapping("/cat-care")
-    public String getTrainer(@AuthenticationPrincipal MemberDetails memberDetails, Model model) {
-        if (memberDetails != null) {
-            model.addAttribute("currentUser", memberDetails.getMember());
+    public String getTrainer(@AuthenticationPrincipal Object principal, Model model) {
+        Member member;
+
+        if (principal instanceof MemberDetails && ((MemberDetails) principal).getMember() != null) {   // 일반 폼 로그인 사용자의 경우
+            member = ((MemberDetails) principal).getMember();
+            model.addAttribute("currentUser", member);
+        } else if (principal instanceof CustomOAuth2User && ((CustomOAuth2User) principal).getMember() != null) { // OAuth2 소셜 로그인 사용자의 경우
+            member = ((CustomOAuth2User) principal).getMember();
+            model.addAttribute("currentUser", member);
         }
 
         return "cat-care-info";
@@ -77,9 +104,15 @@ public class MainViewController {
 
     @Comment("메인 페이지 - 자세히 보기 3 (어떤 분들이 활동하고 있나요?) 안내")
     @GetMapping("/sitters-information")
-    public String getSittersInfo(@AuthenticationPrincipal MemberDetails memberDetails, Model model) {
-        if (memberDetails != null) {
-            model.addAttribute("currentUser", memberDetails.getMember());
+    public String getSittersInfo(@AuthenticationPrincipal Object principal, Model model) {
+        Member member;
+
+        if (principal instanceof MemberDetails && ((MemberDetails) principal).getMember() != null) {   // 일반 폼 로그인 사용자의 경우
+            member = ((MemberDetails) principal).getMember();
+            model.addAttribute("currentUser", member);
+        } else if (principal instanceof CustomOAuth2User && ((CustomOAuth2User) principal).getMember() != null) { // OAuth2 소셜 로그인 사용자의 경우
+            member = ((CustomOAuth2User) principal).getMember();
+            model.addAttribute("currentUser", member);
         }
 
         Object bestSitters = memberService.findBestSitters();
