1b0d10c70c9752fa5905928416c31d01cd9c8401
idohyeon <ehgus5026@naver.com>
2025-03-31 21:17:16 +0900

    -토큰 생성 및 저장 관리 커스텀 핸들러(액세스, 리프레시 토큰 쿠키 저장 방식으로 변경)

--- src/main/java/com/PetSitter/config/oauth/OAuth2SuccessHandler.java
+++ src/main/java/com/PetSitter/config/oauth/OAuth2SuccessHandler.java
@@ -9,13 +9,16 @@ import com.PetSitter.service.Member.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 import java.time.Duration;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 /**
@@ -25,12 +28,13 @@ import java.util.NoSuchElementException;
  */
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     public static final String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
     public static final String ACCESS_TOKEN_COOKIE_NAME = "access_token";
     public static final Duration REFRESH_TOKEN_DURATION = Duration.ofHours(3); // 리프레시 토큰 유효기간
     public static final Duration ACCESS_TOKEN_DURATION = Duration.ofHours(1);   // 액세스 토큰 유효기간
-//    public static final String REDIRECT_PATH = "/pets-care/main";  // 리다이렉트할 경로
+    public static final String REDIRECT_PATH = "/pets-care/main";  // 리다이렉트할 경로
 
     private final TokenProvider tokenProvider;
     private final OAuth2AuthorizationRequestBasedOnCookieRepository auth2AuthorizationRequestRepository;
@@ -47,21 +51,30 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException {
         CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
-//        Member member = (Member) memberService.findById((Long) oAuth2User.getAttributes().get("id"));
-        Member member = memberRepository.findByLoginId((String) oAuth2User.getAttributes().get("id"))
+
+        log.info("OAuth2 로그인 성공: {}", authentication.getName());
+        log.info("OAuth2 User: {}", oAuth2User);
+        log.info("OAuth2 User Attributes: {}", oAuth2User.getAttributes());
+
+        // OAuth2 공급자 확인
+        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+        String provider = oauthToken.getAuthorizedClientRegistrationId();
+
+        String loginId = getLoginIdFromOAuth2User(provider, oAuth2User); // 리소스 서버에 맞는 LoginId 추출
+        log.info("OAuth2 User LOGINID: {}", loginId);
+
+        // 회원 조회
+        Member member = memberRepository.findByLoginId(loginId)
                 .orElseThrow(() -> new NoSuchElementException("Error, onAuthenticationSuccess(): 회원 정보 조회 실패"));
 
-        // OAuth2 제공자 정보 가져오기
-        String oauthProvider = (String) oAuth2User.getAttributes().get("provider");  // google, kakao, naver
-
         // 리프레시 토큰 생성
-        String refreshToken = tokenProvider.generateToken(member, REFRESH_TOKEN_DURATION, oauthProvider);
+        String refreshToken = tokenProvider.generateToken(member, REFRESH_TOKEN_DURATION, provider);
 
         // 리프레시 토큰을 쿠키에 저장
         addRefreshTokenToCookie(request, response, refreshToken);
 
         // 액세스 토큰 생성
-        String accessToken = tokenProvider.generateToken(member, ACCESS_TOKEN_DURATION, oauthProvider);
+        String accessToken = tokenProvider.generateToken(member, ACCESS_TOKEN_DURATION, provider);
 
         // 액세스 토큰을 쿠키에 저장
         addAccessTokenToCookie(request, response, accessToken);
@@ -71,7 +84,34 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         clearAuthenticationAttributes(request, response);
 
         // 리다이렉트
-//        getRedirectStrategy().sendRedirect(request, response, targetUrl); // 액세스 토큰을 URL 쿼리 파라미터에 추가(이 방식은 SSR에서 로컬 혹은 세션 스토리지에 저장할 때 클라이언트에서 JS로 저장할 때 사용)
+        getRedirectStrategy().sendRedirect(request, response, REDIRECT_PATH);
+    }
+
+    // 리소스 서버에 맞는 LoginId 추출
+    private static String getLoginIdFromOAuth2User(String provider, CustomOAuth2User oAuth2User) {
+        // provider별로 LoginID 필드 결정
+        String loginId;
+
+        switch (provider.toLowerCase()) {
+            case "google":
+                loginId = (String) oAuth2User.getAttributes().get("sub");
+                break;
+            case "kakao":
+                loginId = String.valueOf(oAuth2User.getAttributes().get("id"));
+                break;
+            case "naver":
+                Map<String, Object> naverResponse = (Map<String, Object>) oAuth2User.getAttributes().get("response");
+                loginId = naverResponse != null ? (String) naverResponse.get("id") : null;
+                break;
+            default:
+                throw new IllegalArgumentException("지원되지 않는 OAuth2 공급자입니다: " + provider);
+        }
+
+        // ID가 정상적으로 가져와졌는지 확인
+        if (loginId == null) {
+            throw new NoSuchElementException("Error, onAuthenticationSuccess(): OAuth2 LoginID 조회 실패");
+        }
+        return loginId;
     }
 
     // 리프레시 토큰을 쿠키에 저장하는 메서드
