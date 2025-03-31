9a2ffe4a78b87c8f5c351fd675b3be657356a397
idohyeon <ehgus5026@naver.com>
2025-03-31 21:10:51 +0900

    -돌봄사 시점: 돌봄 가능 날짜 API Controller(OAuth2 로그인 유저 식별)

--- src/main/java/com/PetSitter/controller/CareAvailableDate/api/CareAvailableDateApiController.java
+++ src/main/java/com/PetSitter/controller/CareAvailableDate/api/CareAvailableDateApiController.java
@@ -1,7 +1,9 @@
 package com.PetSitter.controller.CareAvailableDate.api;
 
 import com.PetSitter.config.exception.BadRequestCustom;
+import com.PetSitter.domain.Member.Member;
 import com.PetSitter.domain.Member.MemberDetails;
+import com.PetSitter.domain.Member.oauth.CustomOAuth2User;
 import com.PetSitter.dto.CareAvailableDate.request.AddCareAvailableDateRequest;
 import com.PetSitter.dto.CareAvailableDate.request.UpdateCareAvailableDateRequest;
 import com.PetSitter.dto.CareAvailableDate.response.CareAvailableDateResponse;
@@ -31,7 +33,7 @@ public class CareAvailableDateApiController {
     @Operation(summary = "돌봄사 - 돌봄 가능 일정 등록", description = "돌봄 가능 일정 등록 API")
     @PostMapping("/{sitterId}/care-available-dates/new")
     public ResponseEntity<?> saveCareAvailability(@PathVariable("sitterId") @Parameter(required = true, description = "회원(돌봄사) 고유 번호") long id,
-                                                  @AuthenticationPrincipal MemberDetails memberDetails,
+                                                  @AuthenticationPrincipal Object principal,
                                                   @RequestBody @Valid AddCareAvailableDateRequest request,
                                                   BindingResult result) {
         // 검증 오류가 있을 경우 에러 메시지를 리턴
@@ -46,8 +48,19 @@ public class CareAvailableDateApiController {
                     .body(errors);
         }
 
-        if (memberDetails != null && memberDetails.getMember().getId() != id) {
-            throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
+        // principal이 MemberDetails 타입인지 또는 CustomOAuth2User 타입인지 확인
+        if (principal instanceof MemberDetails) {
+            Member member = ((MemberDetails) principal).getMember();
+
+            if (member.getId() != id) {
+                throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
+            }
+        } else if (principal instanceof CustomOAuth2User) {
+            Member member = ((CustomOAuth2User) principal).getMember();
+
+            if (member.getId() != id) {
+                throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
+            }
         }
 
         CareAvailableDateResponse.GetDetail careAvailableDate = careAvailableDateService.save(id, request);
@@ -68,9 +81,19 @@ public class CareAvailableDateApiController {
     @Operation(summary = "돌봄사 - 등록한 모든 돌봄 일정 조회", description = "등록한 모든 돌봄 일정 조회 API")
     @GetMapping("/{sitterId}/care-available-dates")
     public ResponseEntity<Page<CareAvailableDateResponse.GetList>> findCareAvailableDateList(@PathVariable("sitterId") @Parameter(required = true, description = "회원(돌봄사) 고유 번호") long id,
-                                                                                             @Parameter(description = "페이징 파라미터, page: 페이지 번호 - 0부터 시작, size: 한 페이지의 데이터 개수") Pageable pageable, @AuthenticationPrincipal MemberDetails memberDetails) {
-        if (memberDetails != null && memberDetails.getMember().getId() != id) {
-            throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
+                                                                                             @Parameter(description = "페이징 파라미터, page: 페이지 번호 - 0부터 시작, size: 한 페이지의 데이터 개수") Pageable pageable, @AuthenticationPrincipal Object principal) {
+        if (principal instanceof MemberDetails) {
+            Member member = ((MemberDetails) principal).getMember();
+
+            if (member.getId() != id) {
+                throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
+            }
+        } else if (principal instanceof CustomOAuth2User) {
+            Member member = ((CustomOAuth2User) principal).getMember();
+
+            if (member.getId() != id) {
+                throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
+            }
         }
 
         Page<CareAvailableDateResponse.GetList> sitterAvailableDateList = careAvailableDateService.findAllById(id, pageable);
@@ -83,9 +106,19 @@ public class CareAvailableDateApiController {
     @GetMapping("/{sitterId}/care-available-dates/{careAvailableDateId}")
     public ResponseEntity<CareAvailableDateResponse.GetDetail> findCareAvailableDateOne(@PathVariable("sitterId") @Parameter(required = true, description = "회원(돌봄사) 고유 번호") long id,
                                                                                         @PathVariable("careAvailableDateId") @Parameter(required = true, description = "돌봄 일정 고유 번호") long careAvailableDateId,
-                                                                                        @AuthenticationPrincipal MemberDetails memberDetails) {
-        if (memberDetails != null && memberDetails.getMember().getId() != id) {
-            throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
+                                                                                        @AuthenticationPrincipal Object principal) {
+        if (principal instanceof MemberDetails) {
+            Member member = ((MemberDetails) principal).getMember();
+
+            if (member.getId() != id) {
+                throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
+            }
+        } else if (principal instanceof CustomOAuth2User) {
+            Member member = ((CustomOAuth2User) principal).getMember();
+
+            if (member.getId() != id) {
+                throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
+            }
         }
 
         CareAvailableDateResponse.GetDetail sitterAvailableDate = careAvailableDateService.findById(id, careAvailableDateId);
@@ -98,9 +131,19 @@ public class CareAvailableDateApiController {
     @DeleteMapping("/{sitterId}/care-available-dates/{careAvailableDateId}")
     public ResponseEntity<Void> deleteCareAvailableDate(@PathVariable("sitterId") @Parameter(required = true, description = "회원(돌봄사) 고유 번호") long id,
                                                        @PathVariable("careAvailableDateId") @Parameter(required = true, description = "돌봄 일정 고유 번호") long careAvailableDateId,
-                                                        @AuthenticationPrincipal MemberDetails memberDetails) {
-        if (memberDetails != null && memberDetails.getMember().getId() != id) {
-            throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
+                                                        @AuthenticationPrincipal Object principal) {
+        if (principal instanceof MemberDetails) {
+            Member member = ((MemberDetails) principal).getMember();
+
+            if (member.getId() != id) {
+                throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
+            }
+        } else if (principal instanceof CustomOAuth2User) {
+            Member member = ((CustomOAuth2User) principal).getMember();
+
+            if (member.getId() != id) {
+                throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
+            }
         }
 
         careAvailableDateService.delete(id, careAvailableDateId);
@@ -113,7 +156,7 @@ public class CareAvailableDateApiController {
     @PutMapping("/{sitterId}/care-available-dates/{careAvailableDateId}")
     public ResponseEntity<?> updateCareAvailableDate(@PathVariable("sitterId") @Parameter(required = true, description = "회원(돌봄사) 고유 번호") long id,
                                                      @PathVariable("careAvailableDateId") @Parameter(required = true, description = "돌봄 일정 고유 번호") long careAvailableDateId,
-                                                     @AuthenticationPrincipal MemberDetails memberDetails,
+                                                     @AuthenticationPrincipal Object principal,
                                                      @RequestBody @Valid UpdateCareAvailableDateRequest request, BindingResult result) {
         if (result.hasErrors()) {
             Map<String, String> errorMessages = new HashMap<>();
@@ -126,8 +169,18 @@ public class CareAvailableDateApiController {
                     .body(errorMessages);
         }
 
-        if (memberDetails != null && memberDetails.getMember().getId() != id) {
-            throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
+        if (principal instanceof MemberDetails) {
+            Member member = ((MemberDetails) principal).getMember();
+
+            if (member.getId() != id) {
+                throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
+            }
+        } else if (principal instanceof CustomOAuth2User) {
+            Member member = ((CustomOAuth2User) principal).getMember();
+
+            if (member.getId() != id) {
+                throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
+            }
         }
 
         CareAvailableDateResponse.GetDetail updateSitterAvailableDate = careAvailableDateService.update(id, careAvailableDateId, request);
