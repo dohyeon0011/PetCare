8caa079bab91d5651296331afa6ac2c3ce175c23
idohyeon <ehgus5026@naver.com>
2025-04-01 16:55:37 +0900

    -OAuth2 소셜 로그인 Service(OAuth2UserService 구현체)

--- src/main/java/com/PetSitter/config/oauth/CustomOAuth2UserService.java
+++ src/main/java/com/PetSitter/config/oauth/CustomOAuth2UserService.java
@@ -15,6 +15,10 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -28,6 +32,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
      * OAuth2UserRequest: 소셜 로그인 요청을 담고 있는 객체 (구글, 카카오, 네이버 로그인 후 받은 정보 포함)
      * OAuth2User: 소셜 로그인 후 가져온 사용자 정보 객체
      */
+    @Transactional
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException { // OAuth2 제공자로부터 사용자 정보를 불러오는 메서드
         OAuth2User oauth2User = new DefaultOAuth2UserService().loadUser(userRequest);
@@ -37,8 +42,28 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 
         OAuth2UserInfo userInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(registrationId, oauth2User.getAttributes());
 
-        Member member = memberRepository.findByLoginId(userInfo.getId())    // 신규 사용자라면 자동 회원가입을 진행하고, 기존 회원이라면 기존 계정과 매칭해서 로그인
-                .orElseGet(() -> registerNewMember(userInfo, registrationId));
+        /*Member member = memberRepository.findByLoginIdAndIsDeletedFalse(userInfo.getId())    // 신규 사용자라면 자동 회원가입을 진행하고, 기존 회원이라면 기존 계정과 매칭해서 로그인
+                .orElseGet(() -> registerNewMember(userInfo, registrationId));*/
+
+        Optional<Member> existingMember = memberRepository.findByLoginId(userInfo.getId()); // 탈퇴 여부 상관없이 조회
+
+        // 기존 회원일 때
+        if (existingMember.isPresent()) {
+            Member member = existingMember.get();
+
+            // 기존에 탈퇴했던 회원일 때
+            if (member.isDeleted()) {
+                String newLoginId = member.getLoginId() + "_" + UUID.randomUUID().toString().substring(0, 8); // 기존 아이디의 로그인 아이디 임의 변경(새롭게 회원가입 할 로그인 아이디와 충돌이 나지 않게(유니크 키 제약 조건))
+                memberRepository.updateLoginId(member.getId(), newLoginId);
+
+                member = registerNewMember(userInfo, registrationId); // 회원 정보 다시 생성
+            }
+
+            return new CustomOAuth2User(member, oauth2User.getAttributes());
+        }
+
+        // 만약 기존 회원이 아니었다면, 새로운 회원으로 등록
+        Member member = existingMember.orElseGet(() -> registerNewMember(userInfo, registrationId));
 
         return new CustomOAuth2User(member, oauth2User.getAttributes());
     }
@@ -48,8 +73,9 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
                 .loginId(userInfo.getId())  // OAuth2 로그인 ID
                 .password("")
                 .name(userInfo.getName())   // 사용자 이름
-                .nickName(userInfo.getName()) // 사용자 닉네임
+                .nickName(userInfo.getName() + "_" + UUID.randomUUID().toString().substring(0, 10)) // 사용자 닉네임
                 .email(userInfo.getEmail()) // 이메일
+                .phoneNumber(userInfo.getPhoneNumber())
                 .zipcode("")
                 .address("")
                 .socialProvider(SocialProvider.valueOf(provider.toUpperCase())) // 제공자 (GOOGLE, NAVER, KAKAO)
