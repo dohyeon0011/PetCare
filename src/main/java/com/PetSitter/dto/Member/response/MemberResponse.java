83dcf0bd14e823d190c4a1668316e0a9f112bfcb
idohyeon <ehgus5026@naver.com>
2025-04-02 19:23:21 +0900

    -회원 정보 Response DTO(회원 정보 수정 후 DTO 변환 시 회원 역할(ROLE)변경 여부 필드 추가)

--- src/main/java/com/PetSitter/dto/Member/response/MemberResponse.java
+++ src/main/java/com/PetSitter/dto/Member/response/MemberResponse.java
@@ -95,4 +95,85 @@ public class MemberResponse {
         }
     }
 
+    @NoArgsConstructor
+    @Getter
+    public static class getUpdateCustomer {
+        private long id;
+        private String name;
+        private String nickName;
+        private String email;
+        private String phoneNumber;
+        private String zipcode;
+        private String address;
+        private String profileImage;
+        private Role role;
+        private SocialProvider socialProvider;
+        private String introduction;
+        private Integer careerYear;
+        private int amount;
+        private List<PetResponse.GetList> pets = new ArrayList<>();
+        private boolean roleChanged; // 회원 역할 변경 여부('CUSTOMER' or 'PET_SITTER')
+
+        public getUpdateCustomer(Member member, List<Pet> pets, boolean roleChanged) {
+            this.id = member.getId();
+            this.name = member.getName();
+            this.nickName = member.getNickName();
+            this.email = member.getEmail();
+            this.phoneNumber = member.getPhoneNumber();
+            this.zipcode = member.getZipcode();
+            this.address = member.getAddress();
+            this.profileImage = member.getProfileImage();
+            this.role = member.getRole();
+            this.socialProvider = member.getSocialProvider();
+            this.introduction = (member.getIntroduction() != null) ?
+                    member.getIntroduction().replace("\n", "<br>")
+                    : "";
+            this.careerYear = member.getCareerYear();
+            this.amount = member.getAmount();
+            this.pets = pets.stream()
+                    .map(PetResponse.GetList::new)
+                    .collect(Collectors.toList());
+            this.roleChanged = roleChanged;
+        }
+    }
+
+    @NoArgsConstructor
+    @Getter
+    public static class getUpdateSitter {
+        private long id;
+        private String name;
+        private String nickName;
+        private String email;
+        private String phoneNumber;
+        private String zipcode;
+        private String address;
+        private String profileImage;
+        private Role role;
+        private SocialProvider socialProvider;
+        private String introduction;
+        private Integer careerYear;
+        private List<CertificationResponse.GetList> certifications;
+        private boolean roleChanged; // 회원 역할 변경 여부('CUSTOMER' or 'PET_SITTER')
+
+        public getUpdateSitter(Member member, List<Certification> certifications, boolean roleChanged) {
+            this.id = member.getId();
+            this.name = member.getName();
+            this.nickName = member.getNickName();
+            this.email = member.getEmail();
+            this.phoneNumber = member.getPhoneNumber();
+            this.zipcode = member.getZipcode();
+            this.address = member.getAddress();
+            this.profileImage = member.getProfileImage();
+            this.role = member.getRole();
+            this.socialProvider = member.getSocialProvider();
+            this.introduction = (member.getIntroduction() != null) ?
+                    member.getIntroduction().replace("\n", "<br>")
+                    : "";
+            this.careerYear = member.getCareerYear();
+            this.certifications = certifications.stream()
+                    .map(CertificationResponse.GetList::new)
+                    .collect(Collectors.toList());
+            this.roleChanged = roleChanged;
+        }
+    }
 }
