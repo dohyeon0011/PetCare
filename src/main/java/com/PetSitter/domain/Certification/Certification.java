fb11f760470fcec7d7183cdaac37d20926413269
idohyeon <ehgus5026@naver.com>
2025-04-02 13:56:28 +0900

    -돌봄사 자격증 엔티티(기본키 생성 전략 수정(IDENTITY -> 시퀀스 전략))

--- src/main/java/com/PetSitter/domain/Certification/Certification.java
+++ src/main/java/com/PetSitter/domain/Certification/Certification.java
@@ -15,7 +15,7 @@ import org.hibernate.annotations.Comment;
 public class Certification {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     @Column(name = "certification_id", updatable = false)
     private long id;
 
