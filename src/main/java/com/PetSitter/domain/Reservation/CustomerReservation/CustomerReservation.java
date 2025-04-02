b3bd91505e3010e5ea3a337d86a646c89aab721c
idohyeon <ehgus5026@naver.com>
2025-04-02 13:56:36 +0900

    -고객 시점: 돌봄 예약 엔티티(기본키 생성 전략 수정(IDENTITY -> 시퀀스 전략))

--- src/main/java/com/PetSitter/domain/Reservation/CustomerReservation/CustomerReservation.java
+++ src/main/java/com/PetSitter/domain/Reservation/CustomerReservation/CustomerReservation.java
@@ -26,7 +26,7 @@ import java.util.List;
 public class CustomerReservation { // 돌봄 예약(고객 시점)
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     @Column(name = "customer_reservation_id", updatable = false)
     private long id;
 
