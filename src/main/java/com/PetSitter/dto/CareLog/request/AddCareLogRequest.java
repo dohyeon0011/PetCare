90f8e740651c31404f789dba81c5a2d830f148f0
idohyeon <ehgus5026@naver.com>
2025-04-21 14:03:31 +0900

    -돌봄사: 돌봄 케어 로그 Request DTO

--- src/main/java/com/PetSitter/dto/CareLog/request/AddCareLogRequest.java
+++ src/main/java/com/PetSitter/dto/CareLog/request/AddCareLogRequest.java
@@ -18,8 +18,6 @@ public class AddCareLogRequest {
     @NotBlank(message = "해당 돌봄 케어에 대한 상세 설명을 필수로 적어야 합니다.")
     private String description;
 
-    private String image;
-
     public CareLog toEntity(SitterSchedule sitterSchedule, String image) {
         return CareLog.builder()
                 .sitterSchedule(sitterSchedule)
