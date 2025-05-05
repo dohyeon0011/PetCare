f54801237676b599db74b3bd01d4e799cf872780
idohyeon <ehgus5026@naver.com>
2025-05-05 18:09:44 +0900

    -돌봄사: 돌봄 가능 날짜 조회 Response DTO(스웨거 @Schema 필드별 추가)

--- src/main/java/com/PetSitter/dto/CareAvailableDate/response/CareAvailableDateResponse.java
+++ src/main/java/com/PetSitter/dto/CareAvailableDate/response/CareAvailableDateResponse.java
@@ -2,19 +2,29 @@ package com.PetSitter.dto.CareAvailableDate.response;
 
 import com.PetSitter.domain.CareAvailableDate.CareAvailableDate;
 import com.PetSitter.domain.CareAvailableDate.CareAvailableDateStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Schema(description = "등록한 돌봄 일정 조회 Response DTO")
 public class CareAvailableDateResponse {
 
     @NoArgsConstructor
     @Getter
+    @Schema(description = "등록한 돌봄 일정 리스트 조회 Response DTO")
     public static class GetList {
+        @Schema(description = "돌봄 가능 날짜 id")
         private long id;
+
+        @Schema(description = "등록한 돌봄 일정", pattern = "yyyy-MM-dd")
         private LocalDate availableAt;
+
+        @Schema(description = "돌봄 비용")
         private int price;
+
+        @Schema(description = "돌봄 가능 상태", pattern = "POSSIBILITY(예약 가능), IMPOSSIBILITY(예약 불가능)")
         private CareAvailableDateStatus status;
 
         public GetList(CareAvailableDate careAvailableDate) {
@@ -34,12 +44,24 @@ public class CareAvailableDateResponse {
 
     @NoArgsConstructor
     @Getter
+    @Schema(description = "등록한 돌봄 일정 상세 조회 Response DTO")
     public static class GetDetail {
+        @Schema(description = "돌봄 가능 날짜 id")
         private long id;
+
+        @Schema(description = "등록한 돌봄 일정", pattern = "yyyy-MM-dd")
         private LocalDate availableAt;
+
+        @Schema(description = "돌봄 비용")
         private int price;
+
+        @Schema(description = "돌봄 주소(우편번호)", pattern = "12345")
         private String zipcode;
+
+        @Schema(description = "돌봄 주소(상세주소)", pattern = "서울특별시 용산구 한남동")
         private String address;
+
+        @Schema(description = "돌봄 가능 상태", pattern = "POSSIBILITY(예약 가능), IMPOSSIBILITY(예약 불가능)")
         private CareAvailableDateStatus status;
 
         public GetDetail(long id, LocalDate availableAt, int price, String zipcode, String address, CareAvailableDateStatus status) {
@@ -54,9 +76,15 @@ public class CareAvailableDateResponse {
 
     @NoArgsConstructor
     @Getter
+    @Schema(description = "고객이 예약 시 보여질 돌봄 날짜 관련 데이터 Response DTO")
     public static class GetReservation { // 고객이 예약 시 보여질 데이터
+        @Schema(description = "돌봄 가능 날짜 id")
         private long id;
+
+        @Schema(description = "등록한 돌봄 일정", pattern = "yyyy-MM-dd")
         private LocalDate availableAt;
+
+        @Schema(description = "돌봄 비용")
         private int price;
 
         public GetReservation(CareAvailableDate careAvailableDate) {
