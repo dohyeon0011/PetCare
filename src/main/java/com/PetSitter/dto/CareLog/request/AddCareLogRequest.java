2686c4bce01acfefb4cf1de6ced0ab4f88859265
idohyeon <ehgus5026@naver.com>
2025-05-05 18:10:00 +0900

    -돌봄사: 돌봄 케어 로그 Req DTO(스웨거 @Schema 필드별 추가)

--- src/main/java/com/PetSitter/dto/CareLog/request/AddCareLogRequest.java
+++ src/main/java/com/PetSitter/dto/CareLog/request/AddCareLogRequest.java
@@ -2,19 +2,23 @@ package com.PetSitter.dto.CareLog.request;
 
 import com.PetSitter.domain.CareLog.CareLog;
 import com.PetSitter.domain.Reservation.SitterSchedule.SitterSchedule;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Getter
+@Schema(description = "돌봄 케어 로그 작성 Req DTO")
 public class AddCareLogRequest {
 
 //    @NotBlank(message = "케어 로그를 작성할 돌봄을 선택해야 합니다.")
 //    private long sitterScheduleId;
 
+    @Schema(description = "돌봄 유형", pattern = "ex) 산책, 간식, 낮잠, 훈련")
     private String careType;
 
+    @Schema(description = "돌봄 내용 상세 설명")
     @NotBlank(message = "해당 돌봄 케어에 대한 상세 설명을 필수로 적어야 합니다.")
     private String description;
 
@@ -26,5 +30,4 @@ public class AddCareLogRequest {
                 .image(image)
                 .build();
     }
-
 }
