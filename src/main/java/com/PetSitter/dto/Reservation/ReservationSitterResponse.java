436431e0e4147ba05aef5afe045200e195f37175
idohyeon <ehgus5026@naver.com>
2025-05-05 18:20:45 +0900

    -예약 전 돌봄사 정보 조회 Response DTO(스웨거 @Schema 필드별 추가)

--- src/main/java/com/PetSitter/dto/Reservation/ReservationSitterResponse.java
+++ src/main/java/com/PetSitter/dto/Reservation/ReservationSitterResponse.java
@@ -5,21 +5,33 @@ import com.PetSitter.domain.Review.Review;
 import com.PetSitter.dto.Certification.response.CertificationResponse;
 import com.PetSitter.dto.Review.response.ReviewResponse;
 import com.querydsl.core.annotations.QueryProjection;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.parameters.P;
 
 import java.util.List;
 
+@Schema(description = "예약 전 예약 가능 페이지 - 돌봄사 정보 조회 Response DTO")
 public class ReservationSitterResponse { // 고객이 예약하기 전 보여줄 정보
 
     @NoArgsConstructor
     @Getter
+    @Schema(description = "예약 전 예약 가능 페이지 - 돌봄사 리스트")
     public static class GetList { // 예약 가능 페이지에 있는 돌봄사들의 정보
+        @Schema(description = "돌봄사 id")
         private long sitterId;
+
+        @Schema(description = "돌봄사 이름(실명)")
         private String sitterName;
+
+        @Schema(description = "돌봄사 자기소개")
         private String introduction;
+
+        @Schema(description = "돌봄사 경력연차")
         private Integer careerYear;
+
+        @Schema(description = "돌봄사 프로필 이미지")
         private String profileImage;
 
         /*public GetList(Member sitter) {
@@ -40,15 +52,33 @@ public class ReservationSitterResponse { // 고객이 예약하기 전 보여줄
 
     @NoArgsConstructor
     @Getter
+    @Schema(description = "예약 전 예약 가능 페이지 - 돌봄사 상세 정보 조회 Response DTO")
     public static class GetDetail { // 예약 가능 페이지 목록 중 특정 돌봄사의 자세한 정보 + 해당 돌봄사에 대한 리뷰도 보여줄 것
+        @Schema(description = "돌봄사 id")
         private long sitterId;
+
+        @Schema(description = "돌봄사 이름(실명)")
         private String sitterName;
+
+        @Schema(description = "돌봄사 자기소개")
         private String introduction;
+
+        @Schema(description = "돌봄사 경력연차")
         private Integer careerYear;
+
+        @Schema(description = "돌봄사 보유 자격증")
         private List<CertificationResponse.GetReservation> certifications;
+
+        @Schema(description = "돌봄 주소(우편번호)", pattern = "11111")
         private String zipcode;
+
+        @Schema(description = "돌봄 주소(상세주소)")
         private String address;
+
+        @Schema(description = "돌봄사 프로필 이미지")
         private String profileImage;
+
+        @Schema(description = "작성된 돌봄 리뷰")
         private List<ReviewResponse.GetDetail> reviews;
 
         public GetDetail(Member sitter, List<Review> reviews) {
@@ -78,5 +108,4 @@ public class ReservationSitterResponse { // 고객이 예약하기 전 보여줄
                     .toList();
         }
     }
-
 }
