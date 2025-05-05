6bac2a5717fc2fe42397c74c0cd4918f498bb147
idohyeon <ehgus5026@naver.com>
2025-05-05 18:18:17 +0900

    -관리자 페이지 - 회원 포인트 내역 Response DTO(스웨거 @Schema 필드별 추가)

--- src/main/java/com/PetSitter/dto/PointHistory/response/AdminPointHistoryResponse.java
+++ src/main/java/com/PetSitter/dto/PointHistory/response/AdminPointHistoryResponse.java
@@ -1,20 +1,32 @@
 package com.PetSitter.dto.PointHistory.response;
 
 import com.PetSitter.domain.PointHistory.PointsStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Schema(description = "관리자 페이지 - 회원 포인트 내역 조회 Response DTO")
 public class AdminPointHistoryResponse {
 
     @NoArgsConstructor
     @Getter
+    @Schema(description = "관리자 페이지 - 회원 포인트 내역 리스트 조회 Response DTO")
     public static class PointListResponse {
+        @Schema(description = "포인트 내역 id")
         private long id;
+
+        @Schema(description = "고객 이름")
         private String customerName;
+
+        @Schema(description = "포인트")
         private int point;
+
+        @Schema(description = "발생일자")
         private LocalDateTime createdAt;
+
+        @Schema(description = "적립 or 사용 여부", pattern = "SAVING(적립), USING(사용)")
         private PointsStatus pointsStatus;
 
         public PointListResponse(long id, String customerName, int point, LocalDateTime createdAt, PointsStatus pointsStatus) {
