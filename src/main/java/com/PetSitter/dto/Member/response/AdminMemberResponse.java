e77e78e7e6a7042add42affeaa9a34a046409e97
idohyeon <ehgus5026@naver.com>
2025-05-05 18:16:24 +0900

    -관리자 페이지 - 회원 관리 Response DTO(스웨거 @Schema 필드별 추가)

--- src/main/java/com/PetSitter/dto/Member/response/AdminMemberResponse.java
+++ src/main/java/com/PetSitter/dto/Member/response/AdminMemberResponse.java
@@ -8,6 +8,7 @@ import com.PetSitter.domain.Pet.Pet;
 import com.PetSitter.dto.Certification.response.CertificationResponse;
 import com.PetSitter.dto.Pet.response.PetResponse;
 import com.querydsl.core.annotations.QueryProjection;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,17 +18,32 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 // 관리자 페이지(회원 조회)
+@Schema(description = "관리자 페이지(회원 조회) Response DTO")
 public class AdminMemberResponse {
 
     @NoArgsConstructor
     @Getter
+    @Schema(description = "관리자 페이지(회원) 리스트 조회 Response DTO")
     public static class MemberListResponse {
+        @Schema(description = "회원 id")
         private long id;
+
+        @Schema(description = "회원 이름(실명)")
         private String name;
+
+        @Schema(description = "회원 활동 닉네임")
         private String nickName;
+
+        @Schema(description = "이메일")
         private String email;
+
+        @Schema(description = "회원 역할", defaultValue = "CUSTOMER", pattern = "ex) CUSTOMER(고객), PET_SITTER(돌봄사)")
         private Role role;
+
+        @Schema(description = "회원가입일자")
         private LocalDateTime createdAt;
+
+        @Schema(description = "회원 탈퇴 여부", defaultValue = "false", pattern = "ex) false(활동중), true(탈퇴)")
         private boolean isDeleted;
 
         @QueryProjection
@@ -44,20 +60,48 @@ public class AdminMemberResponse {
 
     @NoArgsConstructor
     @Getter
+    @Schema(description = "관리자 페이지(회원) 고객 정보 조회 Response DTO")
     public static class CustomerDetailResponse {
+        @Schema(description = "회원 id")
         private long id;
+
+        @Schema(description = "회원 이름(실명)")
         private String name;
+
+        @Schema(description = "회원 활동 닉네임")
         private String nickName;
+
+        @Schema(description = "이메일")
         private String email;
+
+        @Schema(description = "휴대폰 번호", pattern = "010-xxxx-xxxx")
         private String phoneNumber;
+
+        @Schema(description = "주소(우편번호)", minLength = 5, maxLength = 5)
         private String zipcode;
+
+        @Schema(description = "주소(상세주소)")
         private String address;
+
+        @Schema(description = "프로필 이미지")
         private String profileImage;
+
+        @Schema(description = "회원 역할", defaultValue = "CUSTOMER", pattern = "ex) CUSTOMER(고객), PET_SITTER(돌봄사)")
         private Role role;
+
+        @Schema(description = "소셜 제공자", defaultValue = "NULL", pattern = "ex) KAKAO, NAVER, GOOGLE")
         private SocialProvider socialProvider;
+
+        @Schema(description = "자기소개")
         private String introduction;
+
+        @Schema(description = "보유 적립금")
         private int amount;
+
+        @Schema(description = "보유 반려견")
         private List<PetResponse.GetList> pets = new ArrayList<>();
+
+        @Schema(description = "회원 탈퇴 여부", defaultValue = "false", pattern = "ex) false(활동중), true(탈퇴)")
         private boolean isDeleted;
 
         public CustomerDetailResponse(Member member, List<Pet> pets) {
@@ -84,20 +128,48 @@ public class AdminMemberResponse {
 
     @NoArgsConstructor
     @Getter
+    @Schema(description = "관리자 페이지(회원) 돌봄사 정보 조회 Response DTO")
     public static class SitterDetailResponse {
+        @Schema(description = "회원 id")
         private long id;
+
+        @Schema(description = "회원 이름(실명)")
         private String name;
+
+        @Schema(description = "회원 활동 닉네임")
         private String nickName;
+
+        @Schema(description = "이메일")
         private String email;
+
+        @Schema(description = "휴대폰 번호", pattern = "010-xxxx-xxxx")
         private String phoneNumber;
+
+        @Schema(description = "주소(우편번호)", minLength = 5, maxLength = 5)
         private String zipcode;
+
+        @Schema(description = "주소(상세주소)")
         private String address;
+
+        @Schema(description = "프로필 이미지")
         private String profileImage;
+
+        @Schema(description = "회원 역할", defaultValue = "CUSTOMER", pattern = "ex) CUSTOMER(고객), PET_SITTER(돌봄사)")
         private Role role;
+
+        @Schema(description = "소셜 제공자", defaultValue = "NULL", pattern = "ex) KAKAO, NAVER, GOOGLE")
         private SocialProvider socialProvider;
+
+        @Schema(description = "자기소개")
         private String introduction;
+
+        @Schema(description = "돌봄사 경력연차", defaultValue = "NULL")
         private Integer careerYear;
+
+        @Schema(description = "보유 자격증")
         private List<CertificationResponse.GetList> certifications;
+
+        @Schema(description = "회원 탈퇴 여부", defaultValue = "false", pattern = "ex) false(활동중), true(탈퇴)")
         private boolean isDeleted;
 
         public SitterDetailResponse(Member member, List<Certification> certifications) {
@@ -121,5 +193,4 @@ public class AdminMemberResponse {
             this.isDeleted = member.isDeleted();
         }
     }
-
 }
