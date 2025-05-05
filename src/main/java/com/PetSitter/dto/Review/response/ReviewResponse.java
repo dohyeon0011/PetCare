4f3c4a3a924ec145e455fca5478fef0fee8ca3ee
idohyeon <ehgus5026@naver.com>
2025-05-05 18:21:41 +0900

    -돌봄 리뷰 Response DTO(스웨거 @Schema 필드별 추가)

--- src/main/java/com/PetSitter/dto/Review/response/ReviewResponse.java
+++ src/main/java/com/PetSitter/dto/Review/response/ReviewResponse.java
@@ -1,23 +1,39 @@
 package com.PetSitter.dto.Review.response;
 
 import com.PetSitter.domain.Member.Role;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Schema(description = "돌봄 리뷰 조회 Response DTO")
 public class ReviewResponse {
 
     @NoArgsConstructor
     @Getter
+    @Schema(description = "돌봄 리뷰 리스트 조회 Response DTO")
     public static class GetList { // 모든 리뷰 조회할 때
+        @Schema(description = "리뷰 id")
         private long id;
+
+        @Schema(description = "예약 id")
         private long customerReservationId;
+
+        @Schema(description = "고객 닉네임(활동명)")
         private String customerNickName;
+
+        @Schema(description = "돌봄사 이름(실명)")
         private String sitterName;
+
+        @Schema(description = "돌봄 평점", minLength = 0, maxLength = 5, defaultValue = "0")
         private Double rating;
+
+        @Schema(description = "작성일자", pattern = "yyyy-MM-dd:HH:mm:ss")
         private LocalDateTime createdAt;
+
+        @Schema(description = "회원 유형", pattern = "CUSTOMER(고객), PET_SITTER(돌봄사)")
         private Role role;
 
         public GetList(long reviewId, long customerReservationId, String nickName, String name, Double rating, LocalDateTime createdAt, Role role) {
@@ -33,14 +49,30 @@ public class ReviewResponse {
 
     @NoArgsConstructor
     @Getter
+    @Schema(description = "돌봄 리뷰 상세 조회 Response DTO")
     public static class GetDetail { // 리뷰의 자세한 정보 조회
+        @Schema(description = "리뷰 id")
         private long id;
+
+        @Schema(description = "예약 id")
         private long customerReservationId;
+
+        @Schema(description = "고객 닉네임(활동명)")
         private String customerNickName;
+
+        @Schema(description = "돌봄사 이름(실명)")
         private String sitterName;
+
+        @Schema(description = "돌봄 예약 날짜", pattern = "yyyy-MM-dd")
         private LocalDate reservationAt;
+
+        @Schema(description = "돌봄 평점", minLength = 0, maxLength = 5, defaultValue = "0")
         private Double rating;
+
+        @Schema(description = "돌봄 리뷰 내용")
         private String comment;
+
+        @Schema(description = "작성일자", pattern = "yyyy-MM-dd:HH:mm:ss")
         private LocalDateTime createdAt;
 
         /*public GetDetail(Review review) {
@@ -66,11 +98,21 @@ public class ReviewResponse {
 
     @NoArgsConstructor
     @Getter
+    @Schema(description = "돌봄 리뷰 작성 시 보여질 폼 데이터 Response DTO")
     public static class GetNewReview { // 리뷰 작성 시 보여질 폼 데이터
+        @Schema(description = "예약 id")
         private long customerReservationId;
+
+        @Schema(description = "고객 닉네임(활동명)")
         private String customerNickName;
+
+        @Schema(description = "돌봄사 이름(실명)")
         private String sitterName;
+
+        @Schema(description = "돌봄 평점", minLength = 0, maxLength = 5, defaultValue = "0")
         private Double rating;
+
+        @Schema(description = "돌봄 리뷰 내용")
         private String comment;
 
         /*public GetNewReview(CustomerReservation customerReservation) {
