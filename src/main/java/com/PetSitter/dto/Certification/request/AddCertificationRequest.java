97afdf3fde6f1923a30fe1d17a5b9bef37ffe59f
idohyeon <ehgus5026@naver.com>
2025-05-05 18:14:49 +0900

    -돌봄사 자격증 Req DTO(스웨거 @Schema 필드별 추가)

--- src/main/java/com/PetSitter/dto/Certification/request/AddCertificationRequest.java
+++ src/main/java/com/PetSitter/dto/Certification/request/AddCertificationRequest.java
@@ -1,6 +1,7 @@
 package com.PetSitter.dto.Certification.request;
 
 import com.PetSitter.domain.Certification.Certification;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,8 +10,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Schema(description = "돌봄사 자격증 등록 Req DTO")
 public class AddCertificationRequest {
 
+    @Schema(description = "자격증 이름")
     @NotBlank(message = "자격증 이름 입력은 필수입니다.")
     private String name;
 
@@ -19,5 +22,4 @@ public class AddCertificationRequest {
                 .name(name)
                 .build();
     }
-
 }
