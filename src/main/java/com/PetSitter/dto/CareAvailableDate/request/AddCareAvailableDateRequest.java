a3ff930814c91561fc2bfaf541bd1c50e7f49b3e
idohyeon <ehgus5026@naver.com>
2025-05-05 18:08:49 +0900

    -돌봄사: 돌봄 가능 날짜 등록 Req DTO(스웨거 @Schema 필드별 추가)

--- src/main/java/com/PetSitter/dto/CareAvailableDate/request/AddCareAvailableDateRequest.java
+++ src/main/java/com/PetSitter/dto/CareAvailableDate/request/AddCareAvailableDateRequest.java
@@ -1,6 +1,7 @@
 package com.PetSitter.dto.CareAvailableDate.request;
 
 import com.PetSitter.domain.CareAvailableDate.CareAvailableDate;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -14,12 +15,15 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Schema(description = "돌봄 가능 날짜 등록 Req DTO")
 public class AddCareAvailableDateRequest {
 
+    @Schema(description = "등록할 돌봄 날짜", pattern = "yyyy-MM-dd")
     @NotNull(message = "날짜 입력은 필수입니다.")
     @DateTimeFormat(pattern = "yyyy-MM-dd") // DTO는 뷰와 컨트롤러 간 데이터 처리에 집중
     private LocalDate availableAt;
 
+    @Schema(description = "돌봄 비용")
     @NotNull
     @Min(value = 1, message = "돌봄 비용 입력은 필수입니다.")
     private int price;
