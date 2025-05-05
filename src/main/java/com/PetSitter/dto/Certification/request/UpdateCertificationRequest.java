112052bd234813d93f154a99ed0862a92ecd37d3
idohyeon <ehgus5026@naver.com>
2025-05-05 18:14:59 +0900

    -돌봄사 자격증 수정 Req DTO(스웨거 @Schema 필드별 추가)

--- src/main/java/com/PetSitter/dto/Certification/request/UpdateCertificationRequest.java
+++ src/main/java/com/PetSitter/dto/Certification/request/UpdateCertificationRequest.java
@@ -1,5 +1,6 @@
 package com.PetSitter.dto.Certification.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,10 +9,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Schema(description = "자격증 정보 수정 Req DTO")
 public class UpdateCertificationRequest {
 
+    @Schema(description = "자격증 id")
     private long id;
 
+    @Schema(description = "수정할 자격증 이름")
     @NotBlank(message = "자격증 이름 입력은 필수입니다.")
     private String name;
 
