a655f38371574efd9ffc4cb60ad6a9a66aa3ab32
idohyeon <ehgus5026@naver.com>
2025-05-05 18:15:13 +0900

    -돌봄사 자격증 Response DTO(스웨거 @Schema 필드별 추가)

--- src/main/java/com/PetSitter/dto/Certification/response/CertificationResponse.java
+++ src/main/java/com/PetSitter/dto/Certification/response/CertificationResponse.java
@@ -1,16 +1,22 @@
 package com.PetSitter.dto.Certification.response;
 
 import com.PetSitter.domain.Certification.Certification;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 
+@Schema(description = "자격증 조회 Response DTO")
 public class CertificationResponse {
 
     @NoArgsConstructor
     @Getter
+    @Schema(description = "자격증 조회 리스트 Response DTO")
     public static class GetList {
+        @Schema(description = "자격증 id")
         private long id;
+
+        @Schema(description = "자격증 이름")
         private String name;
 
         public GetList(Certification certification) {
@@ -21,12 +27,13 @@ public class CertificationResponse {
 
     @NoArgsConstructor
     @Getter
+    @Schema(description = "예약 전 고객이 조회할 돌봄사 자격증 정보 Response DTO")
     public static class GetReservation {
+        @Schema(description = "자격증 이름")
         private String name;
 
         public GetReservation(Certification certification) {
             this.name = certification.getName();
         }
     }
-
 }
