8debb82cf9a60a1e71b196307ce053ea7ca40d2a
idohyeon <ehgus5026@naver.com>
2025-05-05 18:10:31 +0900

    -돌봄사: 돌봄 케어 로그 Response DTO(스웨거 @Schema 필드별 추가)

--- src/main/java/com/PetSitter/dto/CareLog/response/CareLogResponse.java
+++ src/main/java/com/PetSitter/dto/CareLog/response/CareLogResponse.java
@@ -1,21 +1,37 @@
 package com.PetSitter.dto.CareLog.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Schema(description = "돌봄 케어 로그 조회 Response DTO")
 public class CareLogResponse {
 
     @NoArgsConstructor
     @Getter
+    @Schema(description = "돌봄 케어 로그 리스트 조회 Response DTO")
     public static class GetList {
+        @Schema(description = "돌봄 케어 로그 id")
         private long id;
+
+        @Schema(description = "돌봄 예약 id")
         private long reservationId;
+
+        @Schema(description = "케어 로그 작성 돌봄사 이름")
         private String sitterName; // 케어 로그 작성 돌봄자
+
+        @Schema(description = "돌봄 예약 고객 닉네임")
         private String customerNickName;
+
+        @Schema(description = "돌봄 유형")
         private String careType;
+
+        @Schema(description = "돌봄 설명")
         private String description;
+
+        @Schema(description = "작성일자")
         private LocalDateTime createdAt;
 
         public GetList(long id, long reservationId, String sitterName, String customerNickName, String careType, String description, LocalDateTime createdAt) {
@@ -31,12 +47,24 @@ public class CareLogResponse {
 
     @NoArgsConstructor
     @Getter
+    @Schema(description = "돌봄 케어 로그 상세 조회 Response DTO")
     public static class GetDetail {
+        @Schema(description = "돌봄 케어 로그 id")
         private long id;
+
+        @Schema(description = "케어 로그 작성 돌봄사 이름")
         private String sitterName; // 케어 로그 작성 돌봄자
+
+        @Schema(description = "돌봄 유형")
         private String careType;
+
+        @Schema(description = "돌봄 설명")
         private String description;
+
+        @Schema(description = "돌봄 케어 사진")
         private String image;
+
+        @Schema(description = "작성일자")
         private LocalDateTime createdAt;
 
         /*public GetDetail(CareLog careLog) {
@@ -60,11 +88,21 @@ public class CareLogResponse {
 
     @NoArgsConstructor
     @Getter
+    @Schema(description = "돌봄 케어 로그 작성 폼 데이터 Response DTO")
     public static class GetNewCareLog {
+
+        @Schema(description = "케어 로그 작성 돌봄사 이름")
         private String sitterName;
+
+        @Schema(description = "돌봄 예약 고객 닉네임")
         private String customerNickName;
+
+        @Schema(description = "돌봄 유형")
         private String careType;
+
+        @Schema(description = "돌봄 설명")
         private String description;
+        
         private String imgPath;
 
         /*public GetNewCareLog(SitterSchedule sitterSchedule) {
