a98702ce856c60b44a0356619bfe94920ed6c60b
idohyeon <ehgus5026@naver.com>
2025-05-05 18:17:39 +0900

    -반려견 정보 조회 Response DTO(스웨거 @Schema 필드별 추가)

--- src/main/java/com/PetSitter/dto/Pet/response/PetResponse.java
+++ src/main/java/com/PetSitter/dto/Pet/response/PetResponse.java
@@ -1,20 +1,34 @@
 package com.PetSitter.dto.Pet.response;
 
 import com.PetSitter.domain.Pet.Pet;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 
+@Schema(description = "반려견 정보 조회 Response DTO")
 public class PetResponse {
 
     @NoArgsConstructor
     @Getter
+    @Schema(description = "반려견 정보 리스트 조회 Response DTO")
     public static class GetList {
+        @Schema(description = "반려견 id")
         private long id;
+
+        @Schema(description = "반려견 이름")
         private String name;
+
+        @Schema(description = "반려견 나이")
         private int age;
+
+        @Schema(description = "반려견 품종")
         private String breed;
+
+        @Schema(description = "반려견 특이사항", pattern = "건강 상태 및 특이사항")
         private String medicalConditions;
+
+        @Schema(description = "반려견 프로필 이미지")
         private String profileImage;
 
         public GetList(Pet pet) {
@@ -29,10 +43,18 @@ public class PetResponse {
 
     @NoArgsConstructor
     @Getter
+    @Schema(description = "고객 예약 시 보여질 반려견 정보 조회 Response DTO")
     public static class GetReservation { // 고객이 예약 시 보여질 반려견 정보
+        @Schema(description = "반려견 id")
         private long id;
+
+        @Schema(description = "반려견 이름")
         private String name;
+
+        @Schema(description = "반려견 나이")
         private int age;
+
+        @Schema(description = "반려견 품종")
         private String breed;
 
         public GetReservation(Pet pet) {
@@ -42,5 +64,4 @@ public class PetResponse {
             this.breed = pet.getBreed();
         }
     }
-
 }
