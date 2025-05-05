1221660da7ec1f3bd93ae2651ebba5c3a6a034d2
idohyeon <ehgus5026@naver.com>
2025-05-05 18:17:07 +0900

    -반려견 정보 수정 Req DTO(스웨거 @Schema 필드별 추가)

--- src/main/java/com/PetSitter/dto/Pet/request/UpdatePetRequest.java
+++ src/main/java/com/PetSitter/dto/Pet/request/UpdatePetRequest.java
@@ -1,5 +1,6 @@
 package com.PetSitter.dto.Pet.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -11,18 +12,25 @@ import org.springframework.web.multipart.MultipartFile;
 @NoArgsConstructor
 @Getter
 @Setter
+@Schema(description = "반려견 수정 Req DTO")
 public class UpdatePetRequest {
+    @Schema(description = "반려견 id")
     private long id;
 
+    @Schema(description = "수정할 반려견 이름")
     private String name;
 
+    @Schema(description = "수정할 반려견 나이")
     @NotNull(message = "반려견 나이 입력은 필수입니다.")
     private int age;
 
+    @Schema(description = "수정할 반려견 품종")
     @NotBlank(message = "반려견 품종 입력은 필수입니다.")
     private String breed;
 
+    @Schema(description = "수정할 반려견 특이 사항", pattern = "건강 상태 및 특이사항")
     private String medicalConditions;
 
+    @Schema(description = "수정할 반려견 프로필 이미지")
     private MultipartFile profileImage;
 }
