19c7a7488085c13a24e6e77af178b0db2ca5ba21
idohyeon <ehgus5026@naver.com>
2025-05-13 19:53:39 +0900

    -돌봄사 시점: 돌봄 예약 일정 VIEW Controller(챗봇 기능 로그인 회원 id model 추가)

--- src/main/java/com/PetSitter/controller/Reservation/SitterSchedule/view/SitterScheduleViewController.java
+++ src/main/java/com/PetSitter/controller/Reservation/SitterSchedule/view/SitterScheduleViewController.java
@@ -33,10 +33,12 @@ public class SitterScheduleViewController {
         if (principal instanceof MemberDetails && ((MemberDetails) principal).getMember() != null) {   // 일반 폼 로그인 사용자의 경우
             member = ((MemberDetails) principal).getMember();
             model.addAttribute("currentUser", member);
+            model.addAttribute("isLogin", member.getId());
         } else if (principal instanceof CustomOAuth2User && ((CustomOAuth2User) principal).getMember() != null) { // OAuth2 소셜 로그인 사용자의 경우
             member = ((CustomOAuth2User) principal).getMember();
             model.addAttribute("currentUser", member);
             model.addAttribute("isOAuthUser", true);
+            model.addAttribute("isLogin", member.getId());
         }
 
         Page<SitterScheduleResponse.GetList> schedules = sitterScheduleService.findAllById(sitterId, pageable);
