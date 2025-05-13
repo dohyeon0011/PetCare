34fd9cb8bad1298453f3e8f4dacc0b8ddff22927
idohyeon <ehgus5026@naver.com>
2025-05-13 19:51:31 +0900

    -돌봄사: 돌봄 케어 로그 VIEW Controller(챗봇 기능 로그인 회원 id model 추가)

--- src/main/java/com/PetSitter/controller/CareLog/view/CareLogViewController.java
+++ src/main/java/com/PetSitter/controller/CareLog/view/CareLogViewController.java
@@ -51,10 +51,12 @@ public class CareLogViewController {
         if (principal instanceof MemberDetails && ((MemberDetails) principal).getMember() != null) {   // 일반 폼 로그인 사용자의 경우
             member = ((MemberDetails) principal).getMember();
             model.addAttribute("currentUser", member);
+            model.addAttribute("isLogin", member.getId());
         } else if (principal instanceof CustomOAuth2User && ((CustomOAuth2User) principal).getMember() != null) { // OAuth2 소셜 로그인 사용자의 경우
             member = ((CustomOAuth2User) principal).getMember();
             model.addAttribute("currentUser", member);
             model.addAttribute("isOAuthUser", true);
+            model.addAttribute("isLogin", member.getId());
         }
 
         Page<CareLogResponse.GetList> careLogs = careLogService.findAll(sitterId, pageable);
