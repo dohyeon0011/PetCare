d80391fda0089809a157df6f1507782a4d21db2c
idohyeon <ehgus5026@naver.com>
2025-05-15 14:42:58 +0900

    -Redis 설정 클래스 파일

--- src/main/java/com/PetSitter/config/redis/RedisConfig.java
+++ src/main/java/com/PetSitter/config/redis/RedisConfig.java
@@ -10,14 +10,16 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 // Redis 기반 챗봇 AI 설정 Config 클래스
 @Configuration
 public class RedisConfig {
+    // jedis: cpu 효율성 측면에서 별로 좋지 않음. 극한으로 끌어 올리려면 적절한 커넥션 풀을 찾아서 정해야 함.(동기적으로 처리)
+    // Lettuce: 비동기로 처리하기 때문에 성능적인 측면에서 좋음.
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
+    public RedisConnectionFactory redisConnectionFactory() {    // RedisConnectionFactory: Redis 데이터베이스와의 연결을 설정하는 역할
         return new LettuceConnectionFactory(); // 기본 host: localhost, port: 6379
     }
 
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
+        RedisTemplate<String, String> template = new RedisTemplate<>(); // RedisTemplate: Redis 데이터베이스와 상호작용하기 위한 설정을 담은 클래스
         template.setConnectionFactory(factory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
