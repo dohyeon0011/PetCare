c8626d7e558daccaf6f081bd9f6d3f6534bc4b1e
idohyeon <ehgus5026@naver.com>
2025-05-28 19:57:30 +0900

    -채팅방 메시지 Service(채팅 메시지 생성 로직 수정)

--- src/main/java/com/PetSitter/service/chat/chatmessage/ChatMessageService.java
+++ src/main/java/com/PetSitter/service/chat/chatmessage/ChatMessageService.java
@@ -1,6 +1,7 @@
 package com.PetSitter.service.chat.chatmessage;
 
 import com.PetSitter.domain.Member.Member;
+import com.PetSitter.domain.Member.Role;
 import com.PetSitter.domain.chat.ChatMessage;
 import com.PetSitter.domain.chat.ChatRoom;
 import com.PetSitter.repository.Member.MemberRepository;
@@ -21,6 +22,7 @@ public class ChatMessageService {
 
     private final ChatMessageRepository chatMessageRepository;
     private final ChatRoomRepository chatRoomRepository;
+    private final ChatRoomService chatRoomService;
     private final MemberRepository memberRepository;
 
     /**
@@ -28,8 +30,15 @@ public class ChatMessageService {
      */
     public ChatMessage saveMessage(String roomId, Long senderId, Long receiverId, String message) {
         // 1. 채팅방 조회
-        ChatRoom chatRoom = chatRoomRepository.findByRoomId(roomId)
+        ChatRoom chatRoom = null;
+        if (roomId != null && !roomId.isBlank()) {   // 기존 채팅방에서 메시지를 전송할 경우
+            log.info("ChatMessageService - saveMessage(): 기존 채팅방 메시지 저장: roomId={}", roomId);
+            chatRoom = chatRoomRepository.findByRoomId(roomId)
                 .orElseThrow(() -> new EntityNotFoundException("채팅방 엔티티 조회 오류. [ChatRoom id=" + roomId + "]"));
+        } else {    // 새로운 대화를 시작할 경우
+            log.info("ChatMessageService - saveMessage(): 새 채팅방 생성 후 메시지 저장: senderId={}, receiverId={}", senderId, receiverId);
+            chatRoom = chatRoomService.createOrGetChatRoom(senderId, receiverId);
+        }
 
         // 2. 발신자, 수신자 조회
         Member sender = memberRepository.findById(senderId)
@@ -46,6 +55,7 @@ public class ChatMessageService {
                 .build();
         log.info("ChatMessageService: saveMessage() - ChatMessage Entity Create Success. id={}", chatMessage.getId());
 
-        return chatMessage;
+        return chatMessageRepository.save(chatMessage);
     }
+
 }
