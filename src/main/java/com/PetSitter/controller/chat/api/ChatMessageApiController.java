66da0e48d6b12240964fcc3e5e1b04c79536dba7
idohyeon <ehgus5026@naver.com>
2025-05-29 20:23:59 +0900

    -채팅방 메시지 API Controller(STOMP 기반 수정)

--- src/main/java/com/PetSitter/controller/chat/api/ChatMessageApiController.java
+++ src/main/java/com/PetSitter/controller/chat/api/ChatMessageApiController.java
@@ -37,6 +37,7 @@ public class ChatMessageApiController {
     @Operation(summary = "채팅방 채팅 메시지 전송", description = "채팅방 채팅 메시지 전송 API")
     @MessageMapping("/chat-room/{roomId}/message")    // prefix, endpoint 설정을 포함한 입력한 url로 발행된 메세지를 뿌림.(Websocket으로 STOMP 통신)
 //    @SendToUser("/queue/chat-message")   // 해당 메서드의 return 값을 url을 구독하는 클라이언트에게 메세지 발행.(1:1 관계, 1:N일 경우 @SendTo, 경로 파라미터를 허용 X(이유: WebSocket 메시지를 사용자에게 보낼 때는 해당 유저가 고정된 경로(/user/queue/xxx)를 구독하고 있어야 하기 때문. PathVariable이 있으면 경로가 유동적이라 클라이언트가 정해진 경로를 구독할 수 없음.))
+                                         // /user/A/queue/chat-message 이런 식으로 Spring 내부에서 로그인 사용자 ID로 자동 분리해서 처리함.(자기 자신한테 보내고 받기. 상대에게 보내려면 convertAndSendToUser()로 명시적으로 보내야 함.)
     @PreAuthorize("isAuthenticated()")  //  WebSocket 세션 인증 정보(accessor.setUser())
     public ResponseEntity<?> saveChatMessage(@DestinationVariable("roomId") @Parameter(required = true, description = "채팅창 고유 번호") String roomId, // 웹소켓 메세징의 경우 @PathVariable이 아닌 @DestinationVariable(구독 및 발행 url의 path parameter)을 사용해야 함.
                                              @Payload @Valid ChatMessageRequest chatMessageRequest, BindingResult result,   // 웹소켓 메시지는 @Payload로 ChatMessageRequest 객체에 매핑
