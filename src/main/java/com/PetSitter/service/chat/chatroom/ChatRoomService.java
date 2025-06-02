dbbc10889eb5215cb4feee5f8ef649f444b241cc
idohyeon <ehgus5026@naver.com>
2025-06-02 20:24:40 +0900

    -채팅방 Service(참여 채팅방 전체 조회 메서드 추가)

--- src/main/java/com/PetSitter/service/chat/chatroom/ChatRoomService.java
+++ src/main/java/com/PetSitter/service/chat/chatroom/ChatRoomService.java
@@ -2,14 +2,20 @@ package com.PetSitter.service.chat.chatroom;
 
 import com.PetSitter.domain.Member.Member;
 import com.PetSitter.domain.chat.ChatRoom;
+import com.PetSitter.dto.chat.chatroom.response.ChatRoomResponse;
 import com.PetSitter.repository.Member.MemberRepository;
 import com.PetSitter.repository.chat.chatroom.ChatRoomRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.annotations.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -21,14 +27,14 @@ public class ChatRoomService {
     private final ChatRoomRepository chatRoomRepository;
     private final MemberRepository memberRepository;
 
-    /**
-     * 채팅방 생성 Or 불러오기
-     */
+    @Comment("채팅방 생성 Or 불러오기")
     public ChatRoom createOrGetChatRoom(Long senderId, Long receiverId) {
+        log.info("ChatRoomService - createOrGetChatRoom() 호출 성공.");
         ChatRoom chatRoom = chatRoomRepository.findBySenderIdAndReceiverId(senderId, receiverId)
                 .orElseGet(() -> {  // 발신자 - 수신자가 최초 대화할 경우, 새로운 채팅방 엔티티 생성
                     Member sender = memberRepository.findById(senderId)
                             .orElseThrow(() -> new EntityNotFoundException("발신자 엔티티 조회 오류: [senderId=" + senderId + "]"));
+                    authorizationMember(sender);
                     Member receiver = memberRepository.findById(receiverId)
                             .orElseThrow(() -> new EntityNotFoundException("수신자 엔티티 조회 오류: [receiverId=" + receiverId + "]"));
 
@@ -44,5 +50,23 @@ public class ChatRoomService {
 
         log.info("ChatRoomService: createOrGetChatRoom() - ChatRoom Entity Loading Success. id={}", chatRoom.getRoomId());
         return chatRoom;
+    }
+
+    @Comment("참여중인 채팅방 목록 전체 조회")
+    public List<ChatRoomResponse.getChatRoomList> getAllChatRooms(Long memberId) {
+        log.info("ChatRoomService - getAllChatRooms() 호출 성공.");
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new EntityNotFoundException("ChatRoomService: getAllChatRooms() - 회원 엔티티 조회 실패. id=" + memberId));
+        authorizationMember(member);
+
+        return chatRoomRepository.findAllByMemberId(member.getId());
+    }
+
+    private static void authorizationMember(Member member) {
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName(); // 로그인에 사용된 아이디 값 반환
+
+        if(!member.getLoginId().equals(userName)) {
+            throw new IllegalArgumentException("회원 본인만 가능합니다.");
+        }
     }
 }
