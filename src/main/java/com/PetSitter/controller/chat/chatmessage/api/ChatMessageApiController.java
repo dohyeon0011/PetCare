2cf469b0da6849c7fdfcab0efcffda39de104bd5
idohyeon <ehgus5026@naver.com>
2025-06-02 20:23:09 +0900

    -채팅방 메시지 API Controller(Mapping 경로 수정 및 패키지 이동)

--- src/main/java/com/PetSitter/controller/chat/chatmessage/api/ChatMessageApiController.java
+++ src/main/java/com/PetSitter/controller/chat/chatmessage/api/ChatMessageApiController.java
@@ -1,4 +1,4 @@
-package com.PetSitter.controller.chat.api;
+package com.PetSitter.controller.chat.chatmessage.api;
 
 import com.PetSitter.domain.Member.Member;
 import com.PetSitter.domain.Member.MemberDetails;
@@ -34,7 +34,7 @@ public class ChatMessageApiController {
     private final ChatMessageService chatMessageService;
 
     @Operation(summary = "채팅방 채팅 메시지 전송", description = "채팅방 채팅 메시지 전송 API")
-    @MessageMapping("/chat-room/{roomId}/message")    // prefix, endpoint 설정을 포함한 입력한 url로 발행된 메세지를 뿌림.(Websocket으로 STOMP 통신)
+    @MessageMapping("/chat-rooms/{roomId}/message")    // prefix, endpoint 설정을 포함한 입력한 url로 발행된 메세지를 뿌림.(Websocket으로 STOMP 통신)
 //    @SendToUser("/queue/chat-message")   // 해당 메서드의 return 값을 url을 구독하는 클라이언트에게 메세지 발행.(1:1 관계, 1:N일 경우 @SendTo, 경로 파라미터를 허용 X(이유: WebSocket 메시지를 사용자에게 보낼 때는 해당 유저가 고정된 경로(/user/queue/xxx)를 구독하고 있어야 하기 때문. PathVariable이 있으면 경로가 유동적이라 클라이언트가 정해진 경로를 구독할 수 없음.))
                                          // /user/A/queue/chat-message 이런 식으로 Spring 내부에서 로그인 사용자 ID로 자동 분리해서 처리함.(자기 자신한테 보내고 받기. 상대에게 보내려면 convertAndSendToUser()로 명시적으로 보내야 함.)
     @PreAuthorize("isAuthenticated()")  //  WebSocket 세션 인증 정보(accessor.setUser(), 해당 메서드나 클래스가 호출되기 전에 인증 정보를 먼저 파악.)
