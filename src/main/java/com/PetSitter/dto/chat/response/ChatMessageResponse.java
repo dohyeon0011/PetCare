32fcc145b17ad343705dd52554083f71b176d932
idohyeon <ehgus5026@naver.com>
2025-06-03 19:17:37 +0900

    -채팅방 메시지 조회 Response DTO(특정 채팅방 메시지 조회 내부 클래스 추가)

--- src/main/java/com/PetSitter/dto/chat/response/ChatMessageResponse.java
+++ src/main/java/com/PetSitter/dto/chat/response/ChatMessageResponse.java
@@ -1,5 +1,6 @@
 package com.PetSitter.dto.chat.response;
 
+import com.PetSitter.domain.chat.ChatMessage;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,30 +8,73 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Schema(description = "채팅방 메시지 조회 Response DTO")
-@NoArgsConstructor
-@Getter
 public class ChatMessageResponse {
 
-    @Schema(description = "채팅방 id")
-    private Long roomId;
+    @Schema(description = "전송한 메시지 DTO")
+    @NoArgsConstructor
+    @Getter
+    public static class messageDto {
+        @Schema(description = "채팅 메시지 id")
+        private Long id;
 
-    @Schema(description = "발신자 id")
-    private Long senderId;
+        @Schema(description = "채팅방 id")
+        private Long roomId;
 
-    @Schema(description = "수신자 id")
-    private Long receiverId;
+        @Schema(description = "발신자 id")
+        private Long senderId;
 
-    @Schema(description = "전송된 채팅방 메시지")
-    private String message;
+        @Schema(description = "수신자 id")
+        private Long receiverId;
 
-    @Schema(description = "전송 시간")
-    private LocalDateTime sentAt;
+        @Schema(description = "전송된 채팅방 메시지")
+        private String message;
 
-    public ChatMessageResponse(Long roomId, Long senderId, Long receiverId, String message, LocalDateTime sentAt) {
-        this.roomId = roomId;
-        this.senderId = senderId;
-        this.receiverId = receiverId;
-        this.message = message;
-        this.sentAt = sentAt;
+        @Schema(description = "전송 시간")
+        private LocalDateTime sentAt;
+
+        public messageDto(Long id, Long roomId, Long senderId, Long receiverId, String message, LocalDateTime sentAt) {
+            this.id = id;
+            this.roomId = roomId;
+            this.senderId = senderId;
+            this.receiverId = receiverId;
+            this.message = message;
+            this.sentAt = sentAt;
+        }
+    }
+
+    @Schema(description = "특정 채팅방 메시지 조회 DTO")
+    @NoArgsConstructor
+    @Getter
+    public static class chatMessageDto {
+        @Schema(description = "채팅 메시지 id")
+        private Long id;
+
+        @Schema(description = "발신자 id")
+        private Long senderId;
+
+        @Schema(description = "발신자 이름")
+        private String senderName;
+
+        @Schema(description = "수신자 id")
+        private Long receiverId;
+
+        @Schema(description = "수신자 이름")
+        private String receiverName;
+
+        @Schema(description = "전송된 채팅방 메시지")
+        private String message;
+
+        @Schema(description = "전송 시간")
+        private LocalDateTime sentAt;
+
+        public chatMessageDto(ChatMessage chatMessage) {
+            this.id = chatMessage.getId();
+            this.senderId = chatMessage.getSender().getId();
+            this.senderName = chatMessage.getSender().getName();
+            this.receiverId = chatMessage.getReceiver().getId();
+            this.receiverName = chatMessage.getReceiver().getName();
+            this.message = chatMessage.getMessage();
+            this.sentAt = chatMessage.getSentAt();
+        }
     }
 }
