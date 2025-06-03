923d0f9841da20a46c15dd8bf0229744333aa982
idohyeon <ehgus5026@naver.com>
2025-06-03 19:20:20 +0900

    -채팅방 조회 Response DTO(참여중인 특정 채팅방 조회 내부 클래스 추가)

--- src/main/java/com/PetSitter/dto/chat/chatroom/response/ChatRoomResponse.java
+++ src/main/java/com/PetSitter/dto/chat/chatroom/response/ChatRoomResponse.java
@@ -1,15 +1,19 @@
 package com.PetSitter.dto.chat.chatroom.response;
 
+import com.PetSitter.domain.chat.ChatMessage;
+import com.PetSitter.dto.chat.response.ChatMessageResponse;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-@Schema(description = "채팅방 조회 DTO Response")
+@Schema(description = "채팅방 조회 Response DTO")
 public class ChatRoomResponse {
 
-    @Schema(description = "채팅방 리스트 조회 DTO Response")
+    @Schema(description = "채팅방 리스트 조회 Response DTO")
     @NoArgsConstructor
     @Getter
     public static class getChatRoomList {
@@ -34,6 +38,36 @@ public class ChatRoomResponse {
             this.receiverName = receiverName;
             this.latestMessage = latestMessage;
             this.latestAt = latestAt;
+        }
+    }
+
+    @Schema(description = "참여중인 특정 채팅방 조회 Response DTO")
+    @NoArgsConstructor
+    @Getter
+    public static class getChatRoomDetail {
+        @Schema(description = "채팅방 고유 id")
+        private Long id;
+
+        @Schema(description = "채팅방 번호")
+        private String roomId;
+
+        @Schema(description = "발신자 id")
+        private Long senderId;
+
+        @Schema(description = "수신자 id")
+        private Long receiverId;
+
+        @Schema(description = "채팅 메시지 내역")
+        List<ChatMessageResponse.chatMessageDto> chatMessages = new ArrayList<>();
+
+        public getChatRoomDetail(Long id, String roomId, Long senderId, Long receiverId, List<ChatMessage> chatMessages) {
+            this.id = id;
+            this.roomId = roomId;
+            this.senderId = senderId;
+            this.receiverId = receiverId;
+            this.chatMessages = chatMessages.stream()
+                    .map(ChatMessageResponse.chatMessageDto::new)
+                    .toList();
         }
     }
 }
