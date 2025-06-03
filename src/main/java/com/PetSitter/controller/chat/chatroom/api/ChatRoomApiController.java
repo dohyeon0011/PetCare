89fc7f713e1083b3245f340b2b73709cbea1fde3
idohyeon <ehgus5026@naver.com>
2025-06-03 19:18:38 +0900

    -채팅방 API Controller(참여중인 특정 채팅방 조회 메서드 추가)

--- src/main/java/com/PetSitter/controller/chat/chatroom/api/ChatRoomApiController.java
+++ src/main/java/com/PetSitter/controller/chat/chatroom/api/ChatRoomApiController.java
@@ -2,6 +2,7 @@ package com.PetSitter.controller.chat.chatroom.api;
 
 import com.PetSitter.dto.chat.chatroom.response.ChatRoomResponse;
 import com.PetSitter.service.chat.chatroom.ChatRoomService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +22,7 @@ public class ChatRoomApiController {
 
     private final ChatRoomService chatRoomService;
 
+    @Operation(summary = "채팅방 전체 조회", description = "채팅방 전체 조회 API")
     @GetMapping("/members/{memberId}/chat/chat-rooms")
     public ResponseEntity<List<ChatRoomResponse.getChatRoomList>> findAllChatRooms(@PathVariable("memberId") @Parameter(required = true, description = "회원 고유 id") Long memberId) {
         log.info("ChatRoomApiController - findAllChatRooms() 호출 성공. memberId={}", memberId);
@@ -29,5 +31,16 @@ public class ChatRoomApiController {
 
         return ResponseEntity.ok()
                 .body(chatRooms);
+    }
+
+    @Operation(summary = "참여중인 특정 채팅방 조회", description = "참여중인 특정 채팅방 조회 API")
+    @GetMapping("/members/{memberId}/chat/chat-rooms/{roomId}")
+    public ResponseEntity<ChatRoomResponse.getChatRoomDetail> findChatRooms(@PathVariable("memberId") @Parameter(required = true, description = "회원 고유 id") Long memberId,
+                                                                            @PathVariable("roomId") @Parameter(required = true, description = "채팅방 번호") String roomId) {
+        log.info("ChatRoomApiController - findChatRooms() 호출 성공. memberId={}, roomId={}", memberId, roomId);
+        ChatRoomResponse.getChatRoomDetail chatRoom = chatRoomService.getChatRoom(memberId, roomId);
+
+        return ResponseEntity.ok()
+                .body(chatRoom);
     }
 }
