bb7e6a06a53849287ff987bd45c8df456ca6fb73
idohyeon <ehgus5026@naver.com>
2025-06-04 21:10:08 +0900

    -채팅방 API Controller(특정 돌봄사와의 진행중인 채팅방 존재 여부 확인 메서드 추가)

--- src/main/java/com/PetSitter/controller/chat/chatroom/api/ChatRoomApiController.java
+++ src/main/java/com/PetSitter/controller/chat/chatroom/api/ChatRoomApiController.java
@@ -7,12 +7,10 @@ import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -34,11 +32,27 @@ public class ChatRoomApiController {
     }
 
     @Operation(summary = "참여중인 특정 채팅방 조회", description = "참여중인 특정 채팅방 조회 API")
-    @GetMapping("/members/{memberId}/chat/chat-rooms/{roomId}")
+    @GetMapping("/members/{memberId}/chat/chat-rooms/{roomId}/messages")
     public ResponseEntity<ChatRoomResponse.getChatRoomDetail> findChatRooms(@PathVariable("memberId") @Parameter(required = true, description = "회원 고유 id") Long memberId,
                                                                             @PathVariable("roomId") @Parameter(required = true, description = "채팅방 번호") String roomId) {
         log.info("ChatRoomApiController - findChatRooms() 호출 성공. memberId={}, roomId={}", memberId, roomId);
         ChatRoomResponse.getChatRoomDetail chatRoom = chatRoomService.getChatRoom(memberId, roomId);
+
+        return ResponseEntity.ok()
+                .body(chatRoom);
+    }
+
+    @Operation(summary = "특정 돌봄사와의 진행중인 채팅방 존재 여부 확인", description = "특정 돌봄사와의 진행중인 채팅방 존재 여부 확인 API")
+    @GetMapping("/members/{memberId}/chat/chat-rooms-exists")
+    public ResponseEntity<Optional<ChatRoomResponse.getExistsChatRoomDetail>> existsChatRooms(@PathVariable("memberId") @Parameter(required = true, description = "회원 고유 id") Long senderId,
+                                             @RequestParam("sitterId") @Parameter(description = "수신자(돌봄사) 고유 id") Long receiverId) {
+        log.info("ChatRoomApiController - existsChatRooms() 호출 성공. senderId={}, receiverId={}", senderId, receiverId);
+        Optional<ChatRoomResponse.getExistsChatRoomDetail> chatRoom = chatRoomService.existsChatRooms(senderId, receiverId);
+
+        chatRoom.ifPresentOrElse(
+                cr -> log.info("Existing ChatRoom found. roomId={}, receiverId={}", cr.getRoomId(), cr.getReceiverId()),
+                () -> log.info("None Existing ChatRoom found for senderId={}, receiverId={}", senderId, receiverId)
+        );
 
         return ResponseEntity.ok()
                 .body(chatRoom);
