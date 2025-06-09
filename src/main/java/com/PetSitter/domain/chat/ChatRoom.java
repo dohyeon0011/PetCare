a164e6c58a09246065d2ca0df147b77379d957cc
idohyeon <ehgus5026@naver.com>
2025-06-09 20:19:34 +0900

    -채팅방 Entity(id 생성 전략 수정)

--- src/main/java/com/PetSitter/domain/chat/ChatRoom.java
+++ src/main/java/com/PetSitter/domain/chat/ChatRoom.java
@@ -20,7 +20,7 @@ import java.time.LocalDateTime;
 public class ChatRoom { // 채팅방 엔티티
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Comment("대화방 번호")
