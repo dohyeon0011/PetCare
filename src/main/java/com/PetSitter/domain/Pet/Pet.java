fd6a5ea9f579a6b51cb4f775c72957b553393669
idohyeon <ehgus5026@naver.com>
2025-06-10 17:46:27 +0900

    -반려견 Entity(id 생성 전략 수정)

--- src/main/java/com/PetSitter/domain/Pet/Pet.java
+++ src/main/java/com/PetSitter/domain/Pet/Pet.java
@@ -14,7 +14,7 @@ import org.hibernate.annotations.Comment;
 public class Pet {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "pet_id", updatable = false)
     private long id;
 
