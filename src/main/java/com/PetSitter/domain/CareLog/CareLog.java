b2940a5bafb8161f8d0cd9337ffb0288269b33a4
idohyeon <ehgus5026@naver.com>
2025-06-10 17:45:59 +0900

    -돌봄 케어 로그 Entity(id 생성 전략 수정)

--- src/main/java/com/PetSitter/domain/CareLog/CareLog.java
+++ src/main/java/com/PetSitter/domain/CareLog/CareLog.java
@@ -19,7 +19,7 @@ import java.time.LocalDateTime;
 public class CareLog { // 돌봄 케어 로그
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "care_log_id")
     private long id;
 
