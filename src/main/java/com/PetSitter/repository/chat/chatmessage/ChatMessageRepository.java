a6b23ac4de67ae528c953299c3f048e88d217c2b
idohyeon <ehgus5026@naver.com>
2025-06-13 21:34:48 +0900

    -채팅방 메시지 Repository(사용자가 읽지 않은 메시지만 조회 로직 메서드 추가)

--- src/main/java/com/PetSitter/repository/chat/chatmessage/ChatMessageRepository.java
+++ src/main/java/com/PetSitter/repository/chat/chatmessage/ChatMessageRepository.java
@@ -2,7 +2,10 @@ package com.PetSitter.repository.chat.chatmessage;
 
 import com.PetSitter.domain.chat.ChatMessage;
 import com.PetSitter.domain.chat.ChatRoom;
+import com.PetSitter.dto.chat.response.UnreadMessageRes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +14,20 @@ import java.util.List;
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
     // 채팅방 id로 채팅 메시지 조회(채팅 메시지 전송 시간 기준 오름차순 조회)
     List<ChatMessage> findAllByChatRoomOrderBySentAtAsc(ChatRoom chatRoom);
+
+    // 사용자가 읽지 않은 메시지만 조회(읽지 않은 메시지 알림 탭)
+    @Query("""
+                select new com.PetSitter.dto.chat.response.UnreadMessageRes(
+                    cm.chatRoom.id,
+                    cm.chatRoom.roomId,
+                    cm.sender.id,
+                    cm.sender.name,
+                    cm.message,
+                    cm.sentAt
+                )
+                from ChatMessage cm
+                where cm.receiver.id = :memberId and cm.isRead = false
+                order by sentAt desc
+            """)
+    List<UnreadMessageRes> findUnreadMessagesByMemberId(@Param("memberId") Long memberId);
 }
