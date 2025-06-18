a6f44768571e73069458157a20c5c780449530ea
idohyeon <ehgus5026@naver.com>
2025-06-18 20:05:01 +0900

    -관리자 돌봄 케어 로그 Service(단건 작업 트랜잭션 제거)

--- src/main/java/com/PetSitter/service/Admin/CareLog/AdminCareLogService.java
+++ src/main/java/com/PetSitter/service/Admin/CareLog/AdminCareLogService.java
@@ -5,9 +5,7 @@ import com.PetSitter.domain.Member.Member;
 import com.PetSitter.domain.Member.Role;
 import com.PetSitter.repository.CareLog.CareLogRepository;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.annotations.Comment;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.NoSuchElementException;
 
@@ -17,15 +15,16 @@ public class AdminCareLogService {
 
     private final CareLogRepository careLogRepository;
 
-    @Comment("관리자 권한 돌봄 케어 로그 삭제")
-    @Transactional
+    /**
+     * 관리자 권한 돌봄 케어 로그 삭제
+     */
     public void deleteForAdmin(long id, Member member) {
         verifyingPermissionsAdmin(member);
 
         CareLog careLog = careLogRepository.findById(id)
                 .orElseThrow(() -> new NoSuchElementException("존재하지 않는 돌봄 케어 로그입니다."));
-
         careLog.changeIsDeleted(true);
+        careLogRepository.saveAndFlush(careLog); // 단건 update는 @Transactional 제거 후 직접 db에 flush가 비용이 쌈. 대신, 영속성 컨텍스트 관리 비용이 들어감.(jpql or native query로도 가능.)
     }
 
     public static void verifyingPermissionsAdmin(Member member) {
@@ -33,5 +32,4 @@ public class AdminCareLogService {
             throw new IllegalArgumentException("관리자 인증이 되지 않은 사용자입니다.");
         }
     }
-
 }
