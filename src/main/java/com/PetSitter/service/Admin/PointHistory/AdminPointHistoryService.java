78c40a0bf9f0147424858603536e7c979dc684ed
idohyeon <ehgus5026@naver.com>
2025-06-18 20:06:03 +0900

    -관리자: 회원 포인트 내역 Service(읽기 전용 커스텀 어노테이션 적용)

--- src/main/java/com/PetSitter/service/Admin/PointHistory/AdminPointHistoryService.java
+++ src/main/java/com/PetSitter/service/Admin/PointHistory/AdminPointHistoryService.java
@@ -1,16 +1,15 @@
 package com.PetSitter.service.Admin.PointHistory;
 
+import com.PetSitter.config.annotation.ReadOnlyTransactional;
 import com.PetSitter.domain.Member.Member;
 import com.PetSitter.domain.Member.Role;
 import com.PetSitter.domain.PointHistory.PointSearch;
 import com.PetSitter.dto.PointHistory.response.AdminPointHistoryResponse;
 import com.PetSitter.repository.PointHistory.PointHistoryRepository;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.annotations.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,16 +17,18 @@ public class AdminPointHistoryService {
 
     private final PointHistoryRepository pointHistoryRepository;
 
-    @Comment("관리자 페이지 회원 포인트 내역 전체 조회")
-    @Transactional(readOnly = true)
-    public Page<AdminPointHistoryResponse.PointListResponse> findAllForAdmin(PointSearch pointSearch, Member member, Pageable pageable) {
-        verifyingPermissionsAdmin(member);
-
+    /**
+     * 관리자 페이지 회원 포인트 내역 전체 조회
+     * @ReadOnlyTransactional: 커스텀 읽기 전용 어노테이션
+     */
+    @ReadOnlyTransactional
+    public Page<AdminPointHistoryResponse.PointListResponse> findAllForAdmin(PointSearch pointSearch, Member admin, Pageable pageable) {
+        verifyingPermissionsAdmin(admin);
         return pointHistoryRepository.findAll(pointSearch, pageable);
     }
 
-    public static void verifyingPermissionsAdmin(Member member) {
-        if (!member.getRole().equals(Role.ADMIN)) {
+    public static void verifyingPermissionsAdmin(Member admin) {
+        if (!admin.getRole().equals(Role.ADMIN)) {
             throw new IllegalArgumentException("관리자 인증이 되지 않은 사용자입니다.");
         }
     }
