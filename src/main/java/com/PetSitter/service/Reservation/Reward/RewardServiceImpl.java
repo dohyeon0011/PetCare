bf68f7d12795477b4674d7ce77b777162a5fe214
idohyeon <ehgus5026@naver.com>
2025-07-01 21:27:58 +0900

    -돌봄 예약금 적립 구현체(addReward() 인자 수정 및 memberRepository 의존 제거)

--- src/main/java/com/PetSitter/service/Reservation/Reward/RewardServiceImpl.java
+++ src/main/java/com/PetSitter/service/Reservation/Reward/RewardServiceImpl.java
@@ -1,25 +1,14 @@
 package com.PetSitter.service.Reservation.Reward;
 
 import com.PetSitter.domain.Member.Member;
-import com.PetSitter.repository.Member.MemberRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.NoSuchElementException;
-
 @Service
-@RequiredArgsConstructor
 public class RewardServiceImpl implements RewardService {
 
-    private final MemberRepository memberRepository;
-
     @Override
-    public void addReward(long customerId, int amount) {
-        Member customer = memberRepository.findById(customerId)
-                .orElseThrow(() -> new NoSuchElementException("고객 정보 조회에 실패했습니다."));
-
+    public void addReward(Member customer, int amount) {
         int reservationAmount = calculatorReward(amount);
-
         customer.addRewardPoints(reservationAmount);
     }
 
@@ -27,5 +16,4 @@ public class RewardServiceImpl implements RewardService {
     public int calculatorReward(int reservationAmount) { // 적립금 계산 로직 (예약금의 2% 적립)
         return (int) (reservationAmount * 0.02);
     }
-
 }
