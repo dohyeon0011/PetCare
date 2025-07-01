8ffe103ce9975b690c7baf6a453acf7958e670a6
idohyeon <ehgus5026@naver.com>
2025-07-01 21:26:57 +0900

    -동물 병원 Repository(모든 병원 데이터 조회 쿼리 메서드, 카운트 쿼리 메서드 로직 키워드 조건문 추가)

--- src/main/java/com/PetSitter/repository/hospital/PetHospitalRepository.java
+++ src/main/java/com/PetSitter/repository/hospital/PetHospitalRepository.java
@@ -12,35 +12,41 @@ public interface PetHospitalRepository extends JpaRepository<PetHospital, Long> 
     // 병원 이름 + 주소 조합으로 조회
     Optional<PetHospital> findByNameAndAddress(String name, String address);
 
-    // 모든 병원 조회(병원마다 최신화된 1건만)
+    // 모든 병원 조회(병원마다 최신화된 1건만) + 지역 키워드 조건 검색
     @Query(value = """
             select name, zipcode, address, street_zipcode, street_address, tel, lat, lng, latest_at
-            from (
-                select *, row_number() over (partition by name, address order by latest_at desc) as rn
-                from pet_hospital
-            ) p
-            where p.rn = 1
-            limit :limit offset :offset
+                from (
+                    select *, row_number() over (partition by name, address order by latest_at desc) as rn
+                        from pet_hospital
+                        where (:sido is null or address like concat('%', :sido, '%'))
+                            and (:sigungu is null or address like concat('%', :sigungu, '%'))
+                ) p
+                where p.rn = 1
+                limit :limit offset :offset
     """,
     countQuery = """
             select count(*)
-            from (
-                select 1
-                from pet_hospital
-                group by name, address
-            ) count_table
+                from (
+                    select 1
+                        from pet_hospital
+                        where (:sido is null or address like concat('%', :sido, '%'))
+                            and (:sigungu is null or address like concat('%', :sigungu, '%'))
+                        group by name, address
+                ) count_table
             """,
     nativeQuery = true)
-    List<Object[]> findAllLatestPetHospitalsByNameAndAddress(@Param("limit") int limit, @Param("offset") int offset);
+    List<Object[]> findPageLatestPetHospitalsByNameAndAddressUsingNativeQuery(@Param("limit") int limit, @Param("offset") int offset, @Param("sido") String sido, @Param("sigungu") String sigungu);
 
-    // 병원 이름 + 주소별 카운트 쿼리(페이징 시 이용)
+    // 병원 이름 + 주소별 카운트 쿼리(페이징 시 이용) + 지역 키워드 조건 검색
     @Query(value = """
             select count(*)
-            from (
-                select 1
-                from pet_hospital
-                group by name, address
-            ) count_table
+                from (
+                    select 1
+                        from pet_hospital
+                        where (:sido is null or address like concat('%', :sido, '%'))
+                            and (:sigungu is null or address like concat('%', :sigungu, '%'))
+                        group by name, address
+                ) count_table
             """, nativeQuery = true)
-    int countGroupedHospitals();
+    int countGroupedHospitals(@Param("sido") String sido, @Param("sigungu") String sigungu);
 }
