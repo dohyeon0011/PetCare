25a111fea47988568bcc1d6568916165ddb3d361
idohyeon <ehgus5026@naver.com>
2025-07-01 21:27:09 +0900

    -동물 병원 검색 조건 키워드 클래스

---
+++ src/main/java/com/PetSitter/domain/hospital/PetHospitalSearch.java
@@ -0,0 +1,11 @@
+package com.PetSitter.domain.hospital;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class PetHospitalSearch {
+    private String sido;
+    private String sigungu;
+}
