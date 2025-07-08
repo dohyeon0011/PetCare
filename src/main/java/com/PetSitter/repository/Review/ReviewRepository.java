0ed27c818f802ab347c99622a1282baf5cb80df0
idohyeon <ehgus5026@naver.com>
2025-07-08 18:56:54 +0900

    -돌봄 리뷰 Repository(특정 돌봄사의 평균 리뷰 점수, 총 리뷰 개수 조회 로직 메서드 추가)

--- src/main/java/com/PetSitter/repository/Review/ReviewRepository.java
+++ src/main/java/com/PetSitter/repository/Review/ReviewRepository.java
@@ -2,6 +2,7 @@ package com.PetSitter.repository.Review;
 
 import com.PetSitter.domain.Reservation.CustomerReservation.CustomerReservation;
 import com.PetSitter.domain.Review.Review;
+import com.PetSitter.dto.Reservation.ReservationSitterResponse;
 import com.PetSitter.dto.Review.response.ReviewResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,15 +10,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositoryCustom {
 
     Optional<Review> findByCustomerReservation(CustomerReservation customerReservation);
-
-    // 고객 시점 예약 엔티티의 customerId(고객)를 기준으로 모든 리뷰 조회
-    List<Review> findByCustomerReservationCustomerId(long customerId);
 
     // 고객 시점 예약 엔티티의 customerId(고객)를 기준으로 모든 리뷰 조회(+페이징)
     @Query("select new com.PetSitter.dto.Review.response.ReviewResponse$GetList(r.id, cr.id, c.nickName, s.name, r.rating, r.createdAt, c.role) " +
@@ -51,12 +48,19 @@ public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRep
             "and r.isDeleted = false")
     Optional<ReviewResponse.GetDetail> findReviewDetail(@Param("id") long id);
 
-    // 고객 시점 예약 엔티티의 sitterId를 기준으로 모든 리뷰 조회
-    List<Review> findByCustomerReservationSitterId(long sitterId);
-
     // 고객 시점 예약 엔티티의 customerId를 기준으로 특정 리뷰 조회
     Optional<Review> findByCustomerReservationCustomerIdAndIdAndIsDeletedFalse(long customerId, long reviewId);
 
-    // 고객 시점 예약 엔티티의 sitterId를 기준으로 특정 리뷰 조회
-    Optional<Review> findByCustomerReservationSitterIdAndId(long sitterId, long reviewId);
+    // 특정 돌봄사의 평균 리뷰 점수와 총 리뷰 개수 조회
+    @Query("""
+            select
+                new com.PetSitter.dto.Reservation.ReservationSitterResponse$avgRatingAndTotalReviewsDTO(
+                    avg(r.rating), count(r)
+                )
+                from Review r
+                where r.customerReservation.sitter.id = :sitterId
+                and r.isDeleted = false
+                group by r.customerReservation.sitter.id
+            """)
+    Optional<ReservationSitterResponse.avgRatingAndTotalReviewsDTO> findAvgRatingAndTotalReviewsBySitterId(@Param("sitterId") long sitterId);
 }
