32459870b967e900e541420dbe8fec7072900c50
idohyeon <ehgus5026@naver.com>
2025-07-10 18:24:30 +0900

    -커스텀 401 에러 유틸 클래스

---
+++ src/main/java/com/PetSitter/config/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.PetSitter.config.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
