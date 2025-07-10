f40074050306fc1af92f7b1665bed7c152357f8e
idohyeon <ehgus5026@naver.com>
2025-07-10 18:27:11 +0900

    -돌봄사 시점: 돌봄 일정 API Controller(반환 데이터 스펙 수정 및 로직 및 검증문 수정)

--- src/main/java/com/PetSitter/controller/Reservation/SitterSchedule/api/SitterScheduleApiController.java
+++ src/main/java/com/PetSitter/controller/Reservation/SitterSchedule/api/SitterScheduleApiController.java
@@ -1,6 +1,7 @@
 package com.PetSitter.controller.Reservation.SitterSchedule.api;
 
 import com.PetSitter.config.exception.BadRequestCustom;
+import com.PetSitter.config.exception.UnauthorizedException;
 import com.PetSitter.domain.Member.Member;
 import com.PetSitter.domain.Member.MemberDetails;
 import com.PetSitter.domain.Member.oauth.CustomOAuth2User;
@@ -15,6 +16,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/pets-care")
@@ -24,29 +27,34 @@ public class SitterScheduleApiController {
 
     @Operation(summary = "돌봄사 - 전체 돌봄 예약 목록 조회", description = "전체 돌봄 예약 목록 조회 API")
     @GetMapping("/members/{sitterId}/schedules")
-    public ResponseEntity<Page<SitterScheduleResponse.GetList>> findAllSitterSchedule(@PathVariable("sitterId") @Parameter(required = true, description = "회원(돌봄사) 고유 번호") long id,
+    public ResponseEntity<?> findAllSitterSchedule(@PathVariable("sitterId") @Parameter(required = true, description = "회원(돌봄사) 고유 번호") long id,
                                                                                       @Parameter(description = "페이징 파라미터, page: 페이지 번호 - 0부터 시작, size: 한 페이지의 데이터 개수") Pageable pageable,
                                                                                       @AuthenticationPrincipal Object principal) {
-//        List<SitterScheduleResponse.GetList> sitterScheduleList = sitterScheduleService.findAllById(id);
-
+        Member member = null;
         if (principal instanceof MemberDetails) {
-            Member member = ((MemberDetails) principal).getMember();
-
+            member = ((MemberDetails) principal).getMember();
             if (member.getId() != id) {
                 throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
             }
         } else if (principal instanceof CustomOAuth2User) {
-            Member member = ((CustomOAuth2User) principal).getMember();
-
+            member = ((CustomOAuth2User) principal).getMember();
             if (member.getId() != id) {
                 throw new BadRequestCustom("잘못된 요청입니다. 유효한 ID가 아닙니다.");
             }
         }
+        if (member == null) {
+            throw new UnauthorizedException("Member Entity is null. id={" + id + "}");
+        }
 
-        Page<SitterScheduleResponse.GetList> sitterScheduleList = sitterScheduleService.findAllById(id, pageable);
+        Page<SitterScheduleResponse.GetList> sitterScheduleList = sitterScheduleService.findAllById(member.getId(), pageable);
+        Long totalReservationAmount = sitterScheduleService.getTotalReservationAmount(member.getId());
+        Map<String, Object> response = Map.of(
+                "schedules", sitterScheduleList,
+                "totalReservationAmount", totalReservationAmount
+        );
 
         return ResponseEntity.ok()
-                .body(sitterScheduleList);
+                .body(response);
     }
 
     @Operation(summary = "돌봄사 - 특정 돌봄 예약 정보 조회", description = "특정 돌봄 예약 정보 조회 API")
