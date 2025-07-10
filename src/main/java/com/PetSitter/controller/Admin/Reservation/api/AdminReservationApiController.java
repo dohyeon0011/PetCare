e7dce5dd2d2f5a41cda8a859fc380aa200a99a00
idohyeon <ehgus5026@naver.com>
2025-07-10 18:19:48 +0900

    -반환 데이터 스펙 수정(관리자 페이지 - 돌봄 예약 관리)

--- src/main/java/com/PetSitter/controller/Admin/Reservation/api/AdminReservationApiController.java
+++ src/main/java/com/PetSitter/controller/Admin/Reservation/api/AdminReservationApiController.java
@@ -1,6 +1,5 @@
 package com.PetSitter.controller.Admin.Reservation.api;
 
-import com.PetSitter.domain.Member.Member;
 import com.PetSitter.domain.Member.MemberDetails;
 import com.PetSitter.domain.Reservation.ReservationSearch;
 import com.PetSitter.dto.Reservation.CustomerReservation.response.AdminReservationResponse;
@@ -11,9 +10,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,13 +26,25 @@ public class AdminReservationApiController {
 
     @Operation(summary = "관리자 페이지 - 모든 예약 목록 조회", description = "관리자 권한 - 모든 예약 목록 조회 API")
     @GetMapping("/reservations")
-    public ResponseEntity<Page<AdminReservationResponse.ReservationListResponse>> findAllForAdmin(@ModelAttribute @Parameter(description = "검색 파라미터: 회원(고객) 이름") ReservationSearch reservationSearch,
+    public ResponseEntity<Map<String, Object>> findAllForAdmin(@ModelAttribute @Parameter(description = "검색 파라미터: 회원(고객) 이름") ReservationSearch reservationSearch,
                                                                                                   @AuthenticationPrincipal MemberDetails memberDetails,
                                                                                                   @PageableDefault @Parameter(description = "페이징 파라미터, page: 페이지 번호 - 0부터 시작, size: 한 페이지의 데이터 개수") Pageable pageable) {
+        if (memberDetails == null || memberDetails.getMember() == null) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                    .body(Map.of(
+                            "error", "해당 기능은 관리자 기능으로 권한이 존재하지 않습니다."
+                    ));
+        }
         Page<AdminReservationResponse.ReservationListResponse> reservations = adminReservationService.findAllForAdmin(reservationSearch, memberDetails.getMember(), pageable);
+        Long totalReservationAmount = adminReservationService.getTotalReservationAmount(memberDetails.getMember());
+
+        Map<String, Object> response = Map.of(
+                "reservations", reservations,
+                "totalReservationAmount", totalReservationAmount
+        );
 
         return ResponseEntity.ok()
-                .body(reservations);
+                .body(response);
     }
 
     @Operation(summary = "관리자 페이지 - 예약 상세 정보 조회", description = "관리자 페이지 예약 상세 정보 조회 API")
