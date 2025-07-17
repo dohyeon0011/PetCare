f3475c32c00b0ab95e9570da50643ee6332b8243
idohyeon <ehgus5026@naver.com>
2025-07-17 19:12:49 +0900

    -회원 신고 등록 Req DTO(제목 필드 추가)

--- src/main/java/com/PetSitter/dto/report/user/request/AddUserReportRequest.java
+++ src/main/java/com/PetSitter/dto/report/user/request/AddUserReportRequest.java
@@ -3,6 +3,8 @@ package com.PetSitter.dto.report.user.request;
 import com.PetSitter.domain.Member.Member;
 import com.PetSitter.domain.report.user.UserReport;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,15 +14,22 @@ import lombok.NoArgsConstructor;
 public class AddUserReportRequest {
 
     @Schema(description = "신고 당한 회원 id")
+    @NotNull(message = "신고 대상 회원 id 값은 필수입니다.")
     private Long reportedUserId;
 
+    @Schema(description = "신고 문의 제목")
+    @NotBlank(message = "신고 문의 제목 필드 입력은 필수입니다.")
+    private String title;
+
     @Schema(description = "신고 내용")
+    @NotBlank(message = "신고 내용 필드 입력은 필수입니다.")
     private String content;
 
     public UserReport toEntity(Member reporter, Member reportedUser) {
         return UserReport.builder()
                 .reporter(reporter)
                 .reportedUser(reportedUser)
+                .title(title)
                 .content(content)
                 .build();
     }
