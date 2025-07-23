75b6575cc610925e4c2b947bd494333cd5e4bdc0
idohyeon <ehgus5026@naver.com>
2025-07-23 18:46:07 +0900

    -신고 문의 정보 Repository(문의 내역 조회 반환 타입 Page<>로 수정, 관리자 전용 조회 메서드 추가)

--- src/main/java/com/PetSitter/repository/report/user/UserReportRepository.java
+++ src/main/java/com/PetSitter/repository/report/user/UserReportRepository.java
@@ -1,12 +1,14 @@
 package com.PetSitter.repository.report.user;
 
 import com.PetSitter.domain.report.user.UserReport;
+import com.PetSitter.dto.report.user.response.AdminUserReportResponse;
 import com.PetSitter.dto.report.user.response.UserReportResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserReportRepository extends JpaRepository<UserReport, Long> {
@@ -32,7 +34,7 @@ public interface UserReportRepository extends JpaRepository<UserReport, Long> {
             join ur.reportedUser reportedUser
             where reporter.id = :memberId
             """)
-    List<UserReportResponse.UserReportListDTO> findAllUserReportDTOByMemberId(@Param("memberId") Long memberId);
+    Page<UserReportResponse.UserReportListDTO> findAllUserReportDTOByMemberId(@Param("memberId") Long memberId, Pageable pageable);
 
     @Query("""
             select new com.PetSitter.dto.report.user.response.UserReportResponse$UserReportDetailDTO(
@@ -44,4 +46,27 @@ public interface UserReportRepository extends JpaRepository<UserReport, Long> {
             where ur.id = :id and reporter.id = :memberId
             """)
     UserReportResponse.UserReportDetailDTO findUserReportDetailDTOByIdAndMemberId(@Param("id") Long id, @Param("memberId") Long memberId);
+
+    // 관리자 - 유저 신고 문의 내역 조회
+    @Query("""
+            select new com.PetSitter.dto.report.user.response.AdminUserReportResponse$UserReportListDTO(
+                ur.id, reporter.name, reportedUser.nickName, ur.title, ur.status, ur.createdAd
+            )
+            from UserReport ur
+            join ur.reporter reporter
+            join ur.reportedUser reportedUser
+            """)
+    Page<AdminUserReportResponse.UserReportListDTO> findAllUserReportDTOForAdmin(Pageable pageable);
+
+    // 관리자 - 유저 신고 문의 상세 조회
+    @Query("""
+            select new com.PetSitter.dto.report.user.response.AdminUserReportResponse$UserReportDetailDTO(
+                ur.id, reporter.name, reported.nickName, ur.title, ur.content, ur.status, ur.createdAt
+            )
+            from UserReport ur
+            join ur.report report
+            join ur.reportedUser reportedUser
+            where ur.id = :id
+            """)
+    AdminUserReportResponse.UserReportDetailDTO findUserReportDetailDTOByIdForAdmin(@Param("id") Long id);
 }
