b1585ad572d19a3687e784613ebf1b8e4b860d32
idohyeon <ehgus5026@naver.com>
2025-07-23 18:45:19 +0900

    -신고 문의 등록 VIEW Controller(문의 내역 조회 반환 타입 Page<>로 수정)

--- src/main/java/com/PetSitter/controller/report/user/view/UserReportViewController.java
+++ src/main/java/com/PetSitter/controller/report/user/view/UserReportViewController.java
@@ -7,14 +7,14 @@ import com.PetSitter.dto.report.user.response.UserReportResponse;
 import com.PetSitter.service.Member.MemberService;
 import com.PetSitter.service.report.user.UserReportService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -48,7 +48,7 @@ public class UserReportViewController {
      * 나의 유저 신고 문의 내역 조회 페이지 반환
      */
     @GetMapping("/users")
-    public String indexUserReport(@AuthenticationPrincipal Object principal, Model model) {
+    public String indexUserReport(@AuthenticationPrincipal Object principal, Pageable pageable, Model model) {
         Member member = null;
         if (principal instanceof MemberDetails && ((MemberDetails) principal).getMember() != null) {
             member = ((MemberDetails) principal).getMember();
@@ -58,7 +58,7 @@ public class UserReportViewController {
             model.addAttribute("currentUser", member);
             model.addAttribute("isOAuthUser", true);
         }
-        List<UserReportResponse.UserReportListDTO> findUserReportList = userReportService.indexUserReport(member);
+        Page<UserReportResponse.UserReportListDTO> findUserReportList = userReportService.indexUserReport(member, pageable);
         model.addAttribute("myUserReports", findUserReportList);
 
         return "report/user/user-report-list";
