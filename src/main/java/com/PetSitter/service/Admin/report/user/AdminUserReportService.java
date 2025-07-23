8bbc92b2b21f334f32fe782becb987682d6a395b
idohyeon <ehgus5026@naver.com>
2025-07-23 18:48:11 +0900

    -관리자 - 유저 신고 문의 내역 관리 Service

---
+++ src/main/java/com/PetSitter/service/Admin/report/user/AdminUserReportService.java
@@ -0,0 +1,63 @@
+package com.PetSitter.service.Admin.report.user;
+
+import com.PetSitter.config.annotation.ReadOnlyTransactional;
+import com.PetSitter.domain.Member.Member;
+import com.PetSitter.domain.Member.Role;
+import com.PetSitter.domain.report.user.UserReport;
+import com.PetSitter.dto.report.user.request.AdminUpdateUserReportRequest;
+import com.PetSitter.dto.report.user.response.AdminUserReportResponse;
+import com.PetSitter.repository.report.user.UserReportRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AdminUserReportService {
+
+    private final UserReportRepository userReportRepository;
+
+    /**
+     * 관리자: 유저 신고 문의 내역 조회 메서드
+     */
+    @ReadOnlyTransactional
+    public Page<AdminUserReportResponse.UserReportListDTO> indexUserReport(Member admin, Pageable pageable) {
+        log.info("AdminUserReportService - indexUserReport(): Call Success.");
+        verifyingPermissionsAdmin(admin);
+        return userReportRepository.findAllUserReportDTOForAdmin(pageable);
+    }
+
+    /**
+     * 관리자: 유저 신고 문의 상세 조회 메서드
+     */
+    @ReadOnlyTransactional
+    public AdminUserReportResponse.UserReportDetailDTO showUserReport(Member admin, Long id) {
+        log.info("AdminUserReportService - showUserReport(): Call Success.");
+        verifyingPermissionsAdmin(admin);
+        return userReportRepository.findUserReportDetailDTOByIdForAdmin(id);
+    }
+
+    /**
+     * 관리자: 유저 신고 문의 처리 상태 수정 메서드
+     */
+    public void updateUserReport(Member admin, AdminUpdateUserReportRequest request) {
+        log.info("AdminUserReportService - updateUserReport(): Call Success.");
+        verifyingPermissionsAdmin(admin);
+        UserReport userReport = userReportRepository.findById(request.getId())
+                .orElseThrow(() -> new EntityNotFoundException("유저 신고 문의 엔티티 조회에 실패했습니다. id={" + request.getId() + "}"));
+
+        userReport.changeReportStatus(request.getUpdateStatus());
+        userReportRepository.saveAndFlush(userReport);
+    }
+
+    private static void verifyingPermissionsAdmin(Member admin) {
+        if (!admin.getRole().equals(Role.ADMIN)) {
+            throw new AccessDeniedException("관리자 인증이 되지 않은 사용자입니다.");
+        }
+    }
+}
