ab12184c3989763f732566e493a315a4623e791b
idohyeon <ehgus5026@naver.com>
2025-07-24 17:38:31 +0900

    -관리자 - 유저 신고 문의 조회 Res DTO(삭제 여부 필드 추가)

--- src/main/java/com/PetSitter/dto/report/user/response/AdminUserReportResponse.java
+++ src/main/java/com/PetSitter/dto/report/user/response/AdminUserReportResponse.java
@@ -1,6 +1,7 @@
 package com.PetSitter.dto.report.user.response;
 
 import com.PetSitter.domain.report.user.ReportStatus;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,15 +31,20 @@ public class AdminUserReportResponse {
         @Schema(description = "신고 문의 처리 상태")
         private ReportStatus status;
 
+        @Schema(description = "삭제 여부", pattern = "True = 삭제, False = 존재")
+        @JsonProperty("isDeleted")
+        private boolean isDeleted;
+
         @Schema(description = "작성 시간")
         private LocalDateTime createdAt;
 
-        public UserReportListDTO(Long id, String reporterName, String reportedUserName, String title, ReportStatus status, LocalDateTime createdAt) {
+        public UserReportListDTO(Long id, String reporterName, String reportedUserName, String title, ReportStatus status, boolean isDeleted, LocalDateTime createdAt) {
             this.id = id;
             this.reporterName = reporterName;
             this.reportedUserName = reportedUserName;
             this.title = title;
             this.status = status;
+            this.isDeleted = isDeleted;
             this.createdAt = createdAt;
         }
     }
@@ -66,16 +72,21 @@ public class AdminUserReportResponse {
         @Schema(description = "신고 문의 처리 상태")
         private ReportStatus status;
 
+        @Schema(description = "삭제 여부", pattern = "True = 삭제, False = 존재")
+        @JsonProperty("isDeleted")
+        private boolean isDeleted;
+
         @Schema(description = "작성 시간")
         private LocalDateTime createdAt;
 
-        public UserReportDetailDTO(Long id, String reporterName, String reportedUserName, String title, String content, ReportStatus status, LocalDateTime createdAt) {
+        public UserReportDetailDTO(Long id, String reporterName, String reportedUserName, String title, String content, ReportStatus status, boolean isDeleted, LocalDateTime createdAt) {
             this.id = id;
             this.reporterName = reporterName;
             this.reportedUserName = reportedUserName;
             this.title = title;
             this.content = content;
             this.status = status;
+            this.isDeleted = isDeleted;
             this.createdAt = createdAt;
         }
     }
