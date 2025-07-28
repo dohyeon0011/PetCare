7c06e72e670d6d529d3b341d2f39307a94654769
idohyeon <ehgus5026@naver.com>
2025-07-28 14:10:27 +0900

    -관리자 페이지: 유저 신고 문의 내역 관리 VIEW Controller(관리자 페이지 - 유저 신고 문의 답변 페이지 반환 메서드 추가)

--- src/main/java/com/PetSitter/controller/Admin/report/user/view/AdminUserReportViewController.java
+++ src/main/java/com/PetSitter/controller/Admin/report/user/view/AdminUserReportViewController.java
@@ -48,4 +48,18 @@ public class AdminUserReportViewController {
         }
         return "admin/report/user/user-report-detail";
     }
+
+    /**
+     * 관리자 페이지 - 유저 신고 문의 답변 페이지 반환
+     */
+    @GetMapping("/{userReportId}/reply")
+    public String newUserReportReply(@PathVariable("userReportId") Long userReportId, @AuthenticationPrincipal MemberDetails memberDetails, Model model) {
+        if (memberDetails != null && memberDetails.getMember() != null) {
+            AdminUserReportResponse.AdminUserReportReplyResDTO userReportReplyInfo = adminUserReportService.getNewUserReportReplyInfo(memberDetails.getMember(), userReportId);
+            model.addAttribute("userReportReplyInfo", userReportReplyInfo);
+            model.addAttribute("currentUser", memberDetails.getMember());
+            model.addAttribute("isLogin", memberDetails.getMember().getId());
+        }
+        return "admin/report/user/user-report-new-reply";
+    }
 }
