276be7af3ac6df5cc771cef2b6f3e4b7855ebbc9
idohyeon <ehgus5026@naver.com>
2025-07-31 18:14:03 +0900

    관리자 - 유저 신고 문의 처리 상태 수정 Req DTO

--- src/main/java/com/PetSitter/dto/report/user/request/AdminUpdateUserReportRequest.java
+++ src/main/java/com/PetSitter/dto/report/user/request/AdminUpdateUserReportRequest.java
@@ -1,15 +1,16 @@
 package com.PetSitter.dto.report.user.request;
 
-import com.PetSitter.domain.report.user.ReportStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Getter
-@Schema(description = "관리자 - 유저 신고 문의 처리 상태 수정 Req DTO")
+@Schema(description = "관리자 - 유저 신고 문의 답변 등록 Req DTO")
 public class AdminUpdateUserReportRequest {
 
-    @Schema(description = "수정할 처리 상태")
-    private ReportStatus updateStatus;
+    @Schema(description = "관리자 문의 답변 내용")
+    @NotBlank(message = "답변 내용을 필수로 입력해야 합니다.")
+    private String replyContent;
 }
