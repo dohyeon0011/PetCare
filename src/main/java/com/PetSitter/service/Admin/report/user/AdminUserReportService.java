9c668122142d85d636feb6721b3567f1b1b932e7
idohyeon <ehgus5026@naver.com>
2025-07-31 18:14:31 +0900

    -관리자 - 유저 신고 문의 내역 관리 Service(관리자: 유저 신고 문의 처리 상태 수정 메서드 작성)

--- src/main/java/com/PetSitter/service/Admin/report/user/AdminUserReportService.java
+++ src/main/java/com/PetSitter/service/Admin/report/user/AdminUserReportService.java
@@ -3,6 +3,7 @@ package com.PetSitter.service.Admin.report.user;
 import com.PetSitter.config.annotation.ReadOnlyTransactional;
 import com.PetSitter.domain.Member.Member;
 import com.PetSitter.domain.Member.Role;
+import com.PetSitter.domain.report.user.ReportStatus;
 import com.PetSitter.domain.report.user.UserReport;
 import com.PetSitter.dto.report.user.request.AdminUpdateUserReportRequest;
 import com.PetSitter.dto.report.user.response.AdminUserReportResponse;
@@ -14,6 +15,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -45,14 +47,30 @@ public class AdminUserReportService {
     /**
      * 관리자: 유저 신고 문의 처리 상태 수정 메서드
      */
+    @Transactional
     public void updateUserReport(Member admin, Long userReportId, AdminUpdateUserReportRequest request) {
         log.info("AdminUserReportService - updateUserReport(): Call Success.");
         verifyingPermissionsAdmin(admin);
         UserReport userReport = userReportRepository.findById(userReportId)
                 .orElseThrow(() -> new EntityNotFoundException("유저 신고 문의 엔티티 조회에 실패했습니다. reportId={" + userReportId + "}"));
+        replyToUserReport(request, userReport); // UserReport 엔티티 관리자 답변 처리
+    }
 
-        userReport.changeReportStatus(request.getUpdateStatus());
-        userReportRepository.saveAndFlush(userReport);
+    /**
+     * UserReport 관리자 답변 시 처리 메서드
+     */
+    private static void replyToUserReport(AdminUpdateUserReportRequest request, UserReport userReport) {
+        userReport.changeReplyContent(request.getReplyContent());
+        userReport.changeReportStatus(ReportStatus.RESOLVE);
+        userReport.changeReplyAt();
+    }
+
+    /**
+     * 관리자: 유저 신고 문의 답변 페이지 데이터 반환 메서드
+     */
+    public AdminUserReportResponse.AdminUserReportReplyResDTO getNewUserReportReplyInfo(Member admin, Long userReportId) {
+        verifyingPermissionsAdmin(admin);
+         return userReportRepository.findUserReportReplyResDTOForAdmin(userReportId);
     }
 
     private static void verifyingPermissionsAdmin(Member admin) {
