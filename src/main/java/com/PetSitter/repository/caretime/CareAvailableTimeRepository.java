5d06097c5f9b538e202adc148d3dfc3e40ebc429
idohyeon <ehgus5026@naver.com>
2025-08-06 15:24:48 +0900

    -돌봄사: 예약 가능 시간 Repository

---
+++ src/main/java/com/PetSitter/repository/caretime/CareAvailableTimeRepository.java
@@ -0,0 +1,8 @@
+package com.PetSitter.repository.caretime;
+
+import com.PetSitter.domain.caretime.CareAvailableTime;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CareAvailableTimeRepository extends JpaRepository<CareAvailableTime, Long> {
+
+}
